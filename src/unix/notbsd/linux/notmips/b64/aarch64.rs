27f7a6c1b27a9b3e899b9553d2eee72adea1795a
Alex Crichton <alex@alexcrichton.com>
2015-09-15 14:32:14 -0700

    Consolidate common linux defn's upwards

--- src/unix/notbsd/linux/notmips/b64/aarch64.rs
+++ src/unix/notbsd/linux/notmips/b64/aarch64.rs
@@ -1,31 +1,31 @@
 //! AArch64-specific definitions for 64-bit linux-like values
 
+pub type c_char = u8;
 pub type nlink_t = u32;
 pub type blksize_t = i32;
-pub type c_char = u8;
 pub type wchar_t = u32;
 
 s! {
     pub struct stat {
-        pub st_dev: dev_t,
-        pub st_ino: ino_t,
-        pub st_mode: mode_t,
-        pub st_nlink: nlink_t,
-        pub st_uid: uid_t,
-        pub st_gid: gid_t,
-        pub st_rdev: dev_t,
-        __pad1: dev_t,
-        pub st_size: off_t,
-        pub st_blksize: blksize_t,
-        __pad2: c_int,
-        pub st_blocks: blkcnt_t,
-        pub st_atime: time_t,
-        pub st_atime_nsec: c_long,
-        pub st_mtime: time_t,
-        pub st_mtime_nsec: c_long,
-        pub st_ctime: time_t,
-        pub st_ctime_nsec: c_long,
-        __unused: [c_int; 2],
+        pub st_dev: ::dev_t,
+        pub st_ino: ::ino_t,
+        pub st_mode: ::mode_t,
+        pub st_nlink: ::nlink_t,
+        pub st_uid: ::uid_t,
+        pub st_gid: ::gid_t,
+        pub st_rdev: ::dev_t,
+        __pad1: ::dev_t,
+        pub st_size: ::off_t,
+        pub st_blksize: ::blksize_t,
+        __pad2: ::c_int,
+        pub st_blocks: ::blkcnt_t,
+        pub st_atime: ::time_t,
+        pub st_atime_nsec: ::c_long,
+        pub st_mtime: ::time_t,
+        pub st_mtime_nsec: ::c_long,
+        pub st_ctime: ::time_t,
+        pub st_ctime_nsec: ::c_long,
+        __unused: [::c_int; 2],
     }
 
     pub struct pthread_attr_t {
