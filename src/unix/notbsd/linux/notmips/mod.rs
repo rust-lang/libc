67e3388d19631c010f66e161adbe62a7f3669130
Alex Crichton <alex@alexcrichton.com>
2015-09-22 21:09:40 -0700

    Add FIONBIO

--- src/unix/notbsd/linux/notmips/mod.rs
+++ src/unix/notbsd/linux/notmips/mod.rs
@@ -167,6 +167,7 @@ pub const TCP_TIMESTAMP: ::c_int = 24;
 pub const SO_REUSEPORT: ::c_int = 15;
 
 pub const FIOCLEX: ::c_ulong = 0x5451;
+pub const FIONBIO: ::c_int = 0x5421;
 
 pub const SA_ONSTACK: ::c_ulong = 0x08000000;
 pub const SA_SIGINFO: ::c_ulong = 0x00000004;
