9e1a38ee2cec9c9151d3750d027ae5a839f4b113
Anton Blanchard <anton@samba.org>
2016-01-21 15:47:02 +0000

    wchar_t is signed on powerpc64

--- src/unix/notbsd/linux/other/b64/powerpc64.rs
+++ src/unix/notbsd/linux/other/b64/powerpc64.rs
@@ -1,7 +1,7 @@
 //! PowerPC64-specific definitions for 64-bit linux-like values
 
 pub type c_char = u8;
-pub type wchar_t = u32;
+pub type wchar_t = i32;
 pub type nlink_t = u64;
 pub type blksize_t = i64;
 
