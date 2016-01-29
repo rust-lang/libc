9c8643fb4ef00c586468174fe8e6f1736530ca2c
Alex Crichton <alex@alexcrichton.com>
2016-01-28 17:39:16 -0800

    Add pthread_setname_np for NetBSD

--- src/unix/bsd/openbsdlike/netbsd.rs
+++ src/unix/bsd/openbsdlike/netbsd.rs
@@ -366,4 +366,7 @@ extern {
                   addr: *mut ::c_void,
                   data: ::c_int) -> ::c_int;
     pub fn sethostname(name: *const ::c_char, len: ::size_t) -> ::c_int;
+    pub fn pthread_setname_np(t: ::pthread_t,
+                              name: *const ::c_char,
+                              arg: *mut ::c_void) -> ::c_int;
 }
