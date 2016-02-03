c97d2174c4ed8ea0f01ec4cf08b66dfe06c23d1d
Dave Huseby <dhuseby@mozilla.com>
2016-02-03 13:03:45 -0800

    fixes the bitrig d_namlen compile error

--- src/unix/bsd/openbsdlike/bitrig.rs
+++ src/unix/bsd/openbsdlike/bitrig.rs
@@ -17,7 +17,7 @@ s! {
         pub d_off: ::off_t,
         pub d_reclen: u16,
         pub d_type: u8,
-        pub d_namelen: u8,
+        pub d_namlen: u8,
         __d_padding: [u8; 4],
         pub d_name: [::c_char; 256],
     }
