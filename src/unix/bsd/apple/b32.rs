f500051a99db01976b5b1c429fc86c522ec7cf40
Martin Wernstål <m4rw3r@gmail.com>
2016-03-13 20:35:56 +0100

    apple: TIOCTIMESTAMP and TIOCDCDTIMESTAMP differ on 32 bit

--- src/unix/bsd/apple/b32.rs
+++ src/unix/bsd/apple/b32.rs
@@ -13,3 +13,6 @@ s! {
 pub const __PTHREAD_MUTEX_SIZE__: usize = 40;
 pub const __PTHREAD_COND_SIZE__: usize = 24;
 pub const __PTHREAD_RWLOCK_SIZE__: usize = 124;
+
+pub const TIOCTIMESTAMP: ::c_ulong = 0x40087459;
+pub const TIOCDCDTIMESTAMP: ::c_ulong = 0x40087458;
--- src/unix/bsd/apple/b64.rs
+++ src/unix/bsd/apple/b64.rs
@@ -13,3 +13,6 @@ s! {
 pub const __PTHREAD_MUTEX_SIZE__: usize = 56;
 pub const __PTHREAD_COND_SIZE__: usize = 40;
 pub const __PTHREAD_RWLOCK_SIZE__: usize = 192;
+
+pub const TIOCTIMESTAMP: ::c_ulong = 0x40107459;
+pub const TIOCDCDTIMESTAMP: ::c_ulong = 0x40107458;
