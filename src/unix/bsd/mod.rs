0dc717626e32fc8303ec8f62ec48656d7e54b1e2
A.J. Gardner <aaron.j.gardner@gmail.com>
2016-03-30 22:50:10 -0500

    Define struct and consts for bsds
    The BSDs seem to have more peculiarities than the notbsds

--- src/unix/bsd/mod.rs
+++ src/unix/bsd/mod.rs
@@ -122,6 +122,14 @@ s! {
     }
 }
 
+pub const LC_ALL: ::c_int = 0;
+pub const LC_COLLATE: ::c_int = 1;
+pub const LC_CTYPE: ::c_int = 2;
+pub const LC_MONETARY: ::c_int = 3;
+pub const LC_NUMERIC: ::c_int = 4;
+pub const LC_TIME: ::c_int = 5;
+pub const LC_MESSAGES: ::c_int = 6;
+
 pub const FIOCLEX: ::c_ulong = 0x20006601;
 pub const FIONBIO: ::c_ulong = 0x8004667e;
 
