ae664ff911cff82bf19240a69a2fbcd1a6d83ae7
Raphael Cohn <raphael.cohn@stormmq.com>
2016-06-01 10:24:34 +0100

    Removing sched_getcpu for musl as no current releases support it

--- src/unix/notbsd/mod.rs
+++ src/unix/notbsd/mod.rs
@@ -804,7 +804,6 @@ extern {
     pub fn stat64(path: *const c_char, buf: *mut stat64) -> ::c_int;
     pub fn eventfd(init: ::c_uint, flags: ::c_int) -> ::c_int;
     pub fn sysinfo (info: *mut ::sysinfo) -> ::c_int;
-    pub fn sched_getcpu() -> ::c_int;
 }
 
 cfg_if! {
