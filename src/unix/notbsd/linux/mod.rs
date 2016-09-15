c1da83215bb0ad5e8b76673dd0aba8cca7c381f7
Philipp Matthias Schaefer <philipp.matthias.schaefer@posteo.de>
2016-09-15 22:12:37 +0200

    Add EPOLLEXCLUSIVE for linux.

--- src/unix/notbsd/linux/mod.rs
+++ src/unix/notbsd/linux/mod.rs
@@ -454,6 +454,7 @@ pub const SHM_HUGETLB: ::c_int = 0o4000;
 pub const SHM_NORESERVE: ::c_int = 0o10000;
 
 pub const EPOLLRDHUP: ::c_int = 0x2000;
+pub const EPOLLEXCLUSIVE: ::c_int = 0x10000000;
 pub const EPOLLONESHOT: ::c_int = 0x40000000;
 
 pub const QFMT_VFS_OLD: ::c_int = 1;
