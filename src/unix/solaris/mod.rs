7d2aea94a10967096a3f450c4cb7e878cfde473e
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-12-04 10:32:42 -0800

    Add needed structs for fd passing

--- src/unix/solaris/mod.rs
+++ src/unix/solaris/mod.rs
@@ -114,6 +114,12 @@ s! {
         pub msg_flags: ::c_int,
     }
 
+    pub struct cmsghdr {
+        pub cmsg_len: ::size_t,
+        pub cmsg_level: ::c_int,
+        pub cmsg_type: ::c_int,
+    }
+
     pub struct fd_set {
         fds_bits: [i32; FD_SETSIZE / 32],
     }
