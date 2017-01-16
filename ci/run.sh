c8a4d214eb071627526896921e86aa87441cc2d0
Kevin Brothaler <admin@digipom.com>
2017-01-16 13:06:25 -0400

    Run the unit tests on the iOS simulator for x86_64-apple-ios

--- ci/run.sh
+++ ci/run.sh
@@ -113,6 +113,10 @@ case "$TARGET" in
     grep "^PASSED .* tests" /tmp/out
     ;;
 
+  x86_64-apple-ios)
+    python ./ci/ios/deploy_and_run_x86_64_binary_on_ios_simulator.py $CARGO_TARGET_DIR/$TARGET/debug/libc-test
+    ;;
+
   arm-unknown-linux-gnueabihf)
     qemu-arm -L /usr/arm-linux-gnueabihf $CARGO_TARGET_DIR/$TARGET/debug/libc-test
     ;;
