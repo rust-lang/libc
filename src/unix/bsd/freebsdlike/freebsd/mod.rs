1363fe4a03b7e0c9f239ce47414b09de8532c4a0
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2017-01-26 18:36:02 +0100

    Add types for `aarch64-unknown-freebsd`
    `aarch64.rs` was copied and adapted from `x86_64.rs`.

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -391,6 +391,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
+    } else if #[cfg(target_arch = "aarch64")] {
+        mod aarch64;
+        pub use self::aarch64::*;
     } else {
         // Unknown target_arch
     }
