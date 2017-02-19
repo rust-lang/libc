010a8275db6c1410cec1e19d6fe2a757ea49b5a3
Philipp Keller <philipp.keller@gmail.com>
2017-02-19 13:30:54 +0100

    Add pthread_exit

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -549,6 +549,7 @@ extern {
                link_name = "pthread_join$UNIX2003")]
     pub fn pthread_join(native: ::pthread_t,
                         value: *mut *mut ::c_void) -> ::c_int;
+    pub fn pthread_exit(value: *mut c_void);
     pub fn pthread_attr_init(attr: *mut ::pthread_attr_t) -> ::c_int;
     pub fn pthread_attr_destroy(attr: *mut ::pthread_attr_t) -> ::c_int;
     pub fn pthread_attr_setstacksize(attr: *mut ::pthread_attr_t,
