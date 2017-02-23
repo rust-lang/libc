cbdf43b7f4da6914c8e92e12f3b3cfff3584abc1
Philipp Keller <philipp.keller@gmail.com>
2017-02-23 06:20:23 +0100

    Remove duplicate readdir definition

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -358,9 +358,6 @@ extern {
     #[cfg_attr(target_os = "solaris", link_name = "__posix_readdir_r")]
     pub fn readdir_r(dirp: *mut ::DIR, entry: *mut ::dirent,
                      result: *mut *mut ::dirent) -> ::c_int;
-    #[cfg_attr(target_os = "macos", link_name = "readdir$INODE64")]
-    #[cfg_attr(target_os = "netbsd", link_name = "__readdir30")]
-    pub fn readdir(arg1: *mut ::DIR) -> *mut ::dirent;
     #[cfg_attr(all(target_os = "macos", target_arch = "x86"),
                link_name = "closedir$UNIX2003")]
     pub fn closedir(dirp: *mut ::DIR) -> ::c_int;
