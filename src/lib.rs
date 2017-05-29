1e7a8d1997b2a59d4a39bc18d1dab226e67d135f
Jorge Aparicio <jorge@japaric.io>
2017-05-29 14:35:44 -0500

    add more types

--- src/lib.rs
+++ src/lib.rs
@@ -83,12 +83,23 @@ mod pwd {}
 #[cfg(target_pointer_width = "64")]
 mod pwd {}
 
-pub type c_longlong = i64;
+pub type int8_t = i8;
+pub type int16_t = i16;
+pub type int32_t = i32;
+pub type int64_t = i64;
+
+pub type uint8_t = u8;
+pub type uint16_t = u16;
+pub type uint32_t = u32;
+pub type uint64_t = u64;
+
 pub type c_schar = i8;
 pub type c_short = i16;
+pub type c_longlong = i64;
+
 pub type c_uchar = u8;
-pub type c_ulonglong = u64;
 pub type c_ushort = u16;
+pub type c_ulonglong = u64;
 
 // NOTE from libc v0.2.23
 // Use repr(u8) as LLVM expects `void*` to be the same as `i8*` to help enable
