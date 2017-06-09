8f88b6ddcc8f093243b3e5650448dae014ab6c00
Marco A L Barbosa <malbarbo@gmail.com>
2017-06-08 21:05:36 -0300

    Hide output of emsdk installation

--- ci/emscripten.sh
+++ ci/emscripten.sh
@@ -10,13 +10,30 @@
 
 set -ex
 
+hide_output() {
+  set +x
+  on_err="
+echo ERROR: An error was encountered with the build.
+cat /tmp/build.log
+exit 1
+"
+  trap "$on_err" ERR
+  bash -c "while true; do sleep 30; echo \$(date) - building ...; done" &
+  PING_LOOP_PID=$!
+  $@ &> /tmp/build.log
+  trap - ERR
+  kill $PING_LOOP_PID
+  rm -f /tmp/build.log
+  set -x
+}
+
 cd /
 curl -L https://s3.amazonaws.com/mozilla-games/emscripten/releases/emsdk-portable.tar.gz | \
     tar -xz
 
 cd /emsdk-portable
 ./emsdk update
-./emsdk install sdk-1.37.13-64bit
+hide_output ./emsdk install sdk-1.37.13-64bit
 ./emsdk activate sdk-1.37.13-64bit
 
 # Compile and cache libc
