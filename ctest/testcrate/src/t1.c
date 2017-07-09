9f540b4dc56b95c74abf0b1646818414f59fb56a
Alex Crichton <alex@alexcrichton.com>
2017-07-09 08:47:00 -0700

    Add support for fixed-size array arguments

--- ctest/testcrate/src/t1.c
+++ ctest/testcrate/src/t1.c
@@ -7,3 +7,7 @@ void* T1c(void* a) { return NULL; }
 int32_t T1d(unsigned a ) { return 0; }
 void T1e(unsigned a, const struct T1Bar* b) { }
 void T1f(void) {}
+void T1g(const int32_t a[4]) {}
+void T1h(const int32_t a[4]) {}
+void T1i(int32_t a[4]) {}
+void T1j(int32_t a[4]) {}
