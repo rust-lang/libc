086d575925d663f0cf08d1b0ffdebc2589d3a435
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2017-09-26 06:00:45 +0000

    Revert "POLL*/RTLD_LAZY: definitions differ on Haiku."
    This reverts commit f816434f9b12016c4b83477f3d548de27cbabd50.

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -163,22 +163,15 @@ pub const S_ISUID: ::c_int = 0x800;
 pub const S_ISGID: ::c_int = 0x400;
 pub const S_ISVTX: ::c_int = 0x200;
 
-#[cfg(not(target_os = "haiku"))]
 pub const POLLIN: ::c_short = 0x1;
-#[cfg(not(target_os = "haiku"))]
 pub const POLLPRI: ::c_short = 0x2;
-#[cfg(not(target_os = "haiku"))]
 pub const POLLOUT: ::c_short = 0x4;
-#[cfg(not(target_os = "haiku"))]
 pub const POLLERR: ::c_short = 0x8;
-#[cfg(not(target_os = "haiku"))]
 pub const POLLHUP: ::c_short = 0x10;
-#[cfg(not(target_os = "haiku"))]
 pub const POLLNVAL: ::c_short = 0x20;
 
 pub const IF_NAMESIZE: ::size_t = 16;
 
-#[cfg(not(target_os = "haiku"))]
 pub const RTLD_LAZY: ::c_int = 0x1;
 
 pub const LOG_EMERG: ::c_int = 0;
