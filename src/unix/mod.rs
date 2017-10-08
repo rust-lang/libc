59904c6efe05c7c22ca070563b217c7dd1a7762b
Clar Charr <clar@charr.xyz>
2017-10-08 18:54:39 -0400

    Fix freebsd.

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -239,7 +239,8 @@ cfg_if! {
     } else if #[cfg(target_os = "emscripten")] {
         #[link(name = "c")]
         extern {}
-    } else if #[cfg(all(target_os = "netbsd", target_vendor = "rumprun"))] {
+    } else if #[cfg(all(stdbuild,
+                        target_os = "netbsd", target_vendor = "rumprun"))] {
         // Since we don't use -nodefaultlibs on Rumprun, libc is always pulled
         // in automatically by the linker. We avoid passing it explicitly, as it
         // causes some versions of binutils to crash with an assertion failure.
