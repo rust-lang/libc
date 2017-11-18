892115ad83c1050c2cc1a8d459f31a03d2be5c9c
bgermann <bgermann@users.noreply.github.com>
2017-11-18 14:50:40 +0100

    Change futimes to futimesat on Solaris
    futimes is not commonly defined on Solaris, but futimesat is.

--- src/unix/solaris/mod.rs
+++ src/unix/solaris/mod.rs
@@ -1307,7 +1307,7 @@ extern {
                     flags: ::c_int, addr: *mut ::sockaddr,
                     addrlen: *mut ::socklen_t) -> ::ssize_t;
     pub fn mkstemps(template: *mut ::c_char, suffixlen: ::c_int) -> ::c_int;
-    pub fn futimes(fd: ::c_int, times: *const ::timeval) -> ::c_int;
+    pub fn futimesat(fd: ::c_int, path: *const ::c_char, times: *const ::timeval) -> ::c_int;
     pub fn utimensat(dirfd: ::c_int, path: *const ::c_char,
                      times: *const ::timespec, flag: ::c_int) -> ::c_int;
     pub fn nl_langinfo(item: ::nl_item) -> *mut ::c_char;
