629908abe33f4594a04dd4b4091e6eaca4056302
luozijun <gnulinux@126.com>
2017-12-26 15:57:11 +0800

    Add struct `bpf_hdr` on macOS

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -170,6 +170,7 @@ fn main() {
         cfg.header("sys/xattr.h");
         cfg.header("sys/sys_domain.h");
         cfg.header("net/if_utun.h");
+        cfg.header("net/bpf.h");
         if target.starts_with("x86") {
             cfg.header("crt_externs.h");
         }
