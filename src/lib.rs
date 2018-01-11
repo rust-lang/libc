02b5a926d6a9426bf5a1abb2abe24b99c9eec416
neirac <cneirabustos@gmail.com>
2018-01-11 20:38:30 +0000

    solaris mod

--- src/lib.rs
+++ src/lib.rs
@@ -293,6 +293,9 @@ cfg_if! {
     } else if #[cfg(unix)] {
         mod unix;
         pub use unix::*;
+    }  else if #[cfg(target_os = "solaris")] {
+        mod unix;
+        pub use unix::solaris;
     } else {
         // Unknown target_family
     }
