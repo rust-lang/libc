030e503f819fc25576f40ee19e1088e2767bbcae
jD91mZM2 <me@krake.one>
2018-05-25 08:49:53 +0200

    Add SIG* on redox

--- src/redox/mod.rs
+++ src/redox/mod.rs
@@ -85,6 +85,38 @@ pub const O_SYMLINK: ::c_int =    0x4000_0000;
 pub const O_NOFOLLOW: ::c_int =   0x8000_0000;
 pub const O_ACCMODE: ::c_int =    O_RDONLY | O_WRONLY | O_RDWR;
 
+pub const SIGHUP:    usize = 1;
+pub const SIGINT:    usize = 2;
+pub const SIGQUIT:   usize = 3;
+pub const SIGILL:    usize = 4;
+pub const SIGTRAP:   usize = 5;
+pub const SIGABRT:   usize = 6;
+pub const SIGBUS:    usize = 7;
+pub const SIGFPE:    usize = 8;
+pub const SIGKILL:   usize = 9;
+pub const SIGUSR1:   usize = 10;
+pub const SIGSEGV:   usize = 11;
+pub const SIGUSR2:   usize = 12;
+pub const SIGPIPE:   usize = 13;
+pub const SIGALRM:   usize = 14;
+pub const SIGTERM:   usize = 15;
+pub const SIGSTKFLT: usize = 16;
+pub const SIGCHLD:   usize = 17;
+pub const SIGCONT:   usize = 18;
+pub const SIGSTOP:   usize = 19;
+pub const SIGTSTP:   usize = 20;
+pub const SIGTTIN:   usize = 21;
+pub const SIGTTOU:   usize = 22;
+pub const SIGURG:    usize = 23;
+pub const SIGXCPU:   usize = 24;
+pub const SIGXFSZ:   usize = 25;
+pub const SIGVTALRM: usize = 26;
+pub const SIGPROF:   usize = 27;
+pub const SIGWINCH:  usize = 28;
+pub const SIGIO:     usize = 29;
+pub const SIGPWR:    usize = 30;
+pub const SIGSYS:    usize = 31;
+
 extern {
     pub fn memalign(align: ::size_t, size: ::size_t) -> *mut ::c_void;
     pub fn read(fd: ::c_int, buf: *mut ::c_void, count: ::size_t)
