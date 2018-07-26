41ca29f709edf9c7460fe9fc8951116c118e1b99
Linus Färnstrand <faern@faern.net>
2018-07-26 20:56:56 +0200

    Add align feature to sem_t struct

--- src/unix/notbsd/linux/mips/mod.rs
+++ src/unix/notbsd/linux/mips/mod.rs
@@ -21,11 +21,16 @@ s! {
     }
 
     // FIXME this is actually a union
+    #[cfg_attr(all(feature = "align", target_pointer_width = "32"),
+               repr(align(4)))]
+    #[cfg_attr(all(feature = "align", target_pointer_width = "64"),
+               repr(align(8)))]
     pub struct sem_t {
         #[cfg(target_pointer_width = "32")]
         __size: [::c_char; 16],
         #[cfg(target_pointer_width = "64")]
         __size: [::c_char; 32],
+        #[cfg(not(feature = "align"))]
         __align: [::c_long; 0],
     }
 
--- src/unix/uclibc/x86_64/mod.rs
+++ src/unix/uclibc/x86_64/mod.rs
@@ -205,11 +205,16 @@ s! {
         pub c_cc: [::cc_t; ::NCCS],
     }
 
+    #[cfg_attr(all(feature = "align", target_pointer_width = "32"),
+               repr(align(4)))]
+    #[cfg_attr(all(feature = "align", target_pointer_width = "64"),
+               repr(align(8)))]
     pub struct sem_t { // ToDo
         #[cfg(target_pointer_width = "32")]
         __size: [::c_char; 16],
         #[cfg(target_pointer_width = "64")]
         __size: [::c_char; 32],
+        #[cfg(not(feature = "align"))]
         __align: [::c_long; 0],
     }
 
