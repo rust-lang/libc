e6d8c8d5ea4bef86ab5410ba71759564b6df114b
xd009642 <danielmckenna93@gmail.com>
2018-10-08 19:06:21 +0100

    Added openbsdlike and fixed netbsd/x86_64.rs

--- src/unix/bsd/netbsdlike/netbsd/x86_64.rs
+++ src/unix/bsd/netbsdlike/netbsd/x86_64.rs
@@ -9,7 +9,3 @@ pub const PT_GETREGS: ::c_int = PT_FIRSTMACH + 1;
 pub const PT_SETREGS: ::c_int = PT_FIRSTMACH + 2;
 pub const PT_GETFPREGS: ::c_int = PT_FIRSTMACH + 3;
 pub const PT_SETFPREGS: ::c_int = PT_FIRSTMACH + 4;
-pub const PT_GETDBREGS: ::c_int = PT_FIRSTMACH + 5;
-pub const PT_SETDBREGS: ::c_int = PT_FIRSTMACH + 6;
-pub const PT_SETSTEP: ::c_int = PT_FIRSTMACH + 7;
-pub const PT_CLEARSTEP: ::c_int = PT_FIRSTMACH + 8;
