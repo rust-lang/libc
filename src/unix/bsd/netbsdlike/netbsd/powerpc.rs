08e34d7c2a5598e7da107ca43fb9068426af4ed8
xd009642 <danielmckenna93@gmail.com>
2018-10-08 18:48:03 +0100

    Arch specific ptrace commands for netbsd

--- src/unix/bsd/netbsdlike/netbsd/powerpc.rs
+++ src/unix/bsd/netbsdlike/netbsd/powerpc.rs
@@ -1,3 +1,9 @@
+use self::*;
+
 pub type c_long = i32;
 pub type c_ulong = u32;
 pub type c_char = u8;
+
+pub const PT_STEP: ::c_int = PT_FIRSTMACH + 0;
+pub const PT_GETREGS: ::c_int = PT_FIRSTMACH + 1;
+pub const PT_SETREGS: ::c_int = PT_FIRSTMACH + 2;
