648589cf1e281d19bf49e13d846a05405fda86b0
John Baublitz <john.m.baublitz@gmail.com>
2018-12-21 18:51:40 -0500

    Add if_link.h consts for rtnetlink attribute rta_type field

--- src/unix/notbsd/linux/mod.rs
+++ src/unix/notbsd/linux/mod.rs
@@ -938,6 +938,16 @@ pub const IFA_F_DEPRECATED: u32 = 0x20;
 pub const IFA_F_TENTATIVE: u32 = 0x40;
 pub const IFA_F_PERMANENT: u32 = 0x80;
 
+// linux/if_link.h
+pub const IFLA_UNSPEC: ::c_ushort = 0;
+pub const IFLA_ADDRESS: ::c_ushort = 1;
+pub const IFLA_BROADCAST: ::c_ushort = 2;
+pub const IFLA_IFNAME: ::c_ushort = 3;
+pub const IFLA_MTU: ::c_ushort = 4;
+pub const IFLA_LINK: ::c_ushort = 5;
+pub const IFLA_QDISK: ::c_ushort = 6;
+pub const IFLA_STATS: ::c_ushort = 7;
+
 // linux/if_tun.h
 pub const IFF_TUN: ::c_short = 0x0001;
 pub const IFF_TAP: ::c_short = 0x0002;
