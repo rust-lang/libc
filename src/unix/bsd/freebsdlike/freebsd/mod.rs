3e96dbee3bdbeff75cabb8d25ded0b9c620ba03a
lenoil98 <hamiltcl@verizon.net>
2019-01-17 16:07:30 -0500

    Update mod.rs
    Add support for powerpc64 on freebsd

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -1213,6 +1213,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub use self::aarch64::*;
+    } else if #[cfg(target_arch = "powerpc64")] {
+       mod powerpc64;
+       pub use self::powerpc64::*;
     } else {
         // Unknown target_arch
     }
