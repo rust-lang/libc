e628de2d7d40425af7d7a29a6064f356193a1df2
MikaelUrankar <mikael.urankar@gmail.com>
2019-02-02 16:19:08 +0100

    Add armv6 and armv7 FreeBSD targets

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -1213,6 +1213,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub use self::aarch64::*;
+    } else if #[cfg(target_arch = "arm")] {
+        mod arm;
+        pub use self::arm::*;
     } else {
         // Unknown target_arch
     }
