55749a05d5fe50b37cb74104b2cf356fc815c830
gnzlbg <gonzalobg88@gmail.com>
2019-02-07 00:54:20 +0100

    Fix typo

--- src/unix/notbsd/linux/no_align.rs
+++ src/unix/notbsd/linux/no_align.rs
@@ -4,25 +4,25 @@ s! {
                   target_arch = "mips64", target_arch = "s390x",
                   target_arch = "sparc64",
                   all(target_arch = "aarch64", target_env = "musl")))]
-        __align: [::c_int; 0],
+        pub __align: [::c_int; 0],
         #[cfg(not(any(target_arch = "x86_64", target_arch = "powerpc64",
                       target_arch = "mips64", target_arch = "s390x",
                       target_arch = "sparc64",
                       all(target_arch = "aarch64", target_env = "musl"))))]
-        __align: [::c_long; 0],
+        pub __align: [::c_long; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_MUTEXATTR_T],
     }
 
     pub struct pthread_rwlockattr_t {
         #[cfg(target_env = "musl")]
-        __align: [::c_int; 0],
+        pub __align: [::c_int; 0],
         #[cfg(not(target_env = "musl"))]
-        __align: [::c_long; 0],
+        pub __align: [::c_long; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_RWLOCKATTR_T],
     }
 
     pub struct pthread_condattr_t {
-        __align: [::c_int; 0],
+        pub __align: [::c_int; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_CONDATTR_T],
     }
 }
@@ -30,9 +30,9 @@ s! {
 s_no_extra_traits! {
     pub struct pthread_cond_t {
         #[cfg(target_env = "musl")]
-        __align: [*const ::c_void; 0],
+        pub __align: [*const ::c_void; 0],
         #[cfg(not(target_env = "musl"))]
-        __align: [::c_longlong; 0],
+        pub __align: [::c_longlong; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_COND_T],
     }
 
@@ -42,13 +42,13 @@ s_no_extra_traits! {
                   target_arch = "powerpc",
                   all(target_arch = "x86_64",
                       target_pointer_width = "32")))]
-        __align: [::c_long; 0],
+        pub __align: [::c_long; 0],
         #[cfg(not(any(target_arch = "mips",
                       target_arch = "arm",
                       target_arch = "powerpc",
                       all(target_arch = "x86_64",
                           target_pointer_width = "32"))))]
-        __align: [::c_longlong; 0],
+        pub __align: [::c_longlong; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_MUTEX_T],
     }
 
@@ -58,13 +58,13 @@ s_no_extra_traits! {
                   target_arch = "powerpc",
                   all(target_arch = "x86_64",
                       target_pointer_width = "32")))]
-        __align: [::c_long; 0],
+        pub __align: [::c_long; 0],
         #[cfg(not(any(target_arch = "mips",
                       target_arch = "arm",
                       target_arch = "powerpc",
                       all(target_arch = "x86_64",
                           target_pointer_width = "32"))))]
-        __align: [::c_longlong; 0],
+        pub __align: [::c_longlong; 0],
         pub size: [u8; ::__SIZEOF_PTHREAD_RWLOCK_T],
     }
 }
