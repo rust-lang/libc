c4e185555a2d0f04a59a941ab9698d97982c1a18
gnzlbg <gonzalobg88@gmail.com>
2019-02-06 19:07:08 +0100

    enable more build targets on all build jobs

--- ci/build.sh
+++ ci/build.sh
@@ -58,21 +58,16 @@ test_target() {
 TARGETS="wasm32-unknown-unknown"
 case "${OS}" in
     linux*)
-        TARGETS="i686-unknown-linux-gnu x86_64-unknown-linux-gnu x86_64-unknown-freebsd x86_64-unknown-netbsd "
+        TARGETS="i686-unknown-linux-gnu x86_64-unknown-linux-gnu x86_64-unknown-freebsd x86_64-unknown-netbsd i586-unknown-linux-gnu i586-unknown-linux-musl i686-linux-android i686-unknown-freebsd mipsel-unknown-linux-gnu armv7-linux-androideabi armv7-unknown-linux-gnueabihf armv7-unknown-linux-musleabihf x86_64-sun-solaris "
 
         if [ "${RUST}" = "nightly" ]; then
             # aarch64-unknown-cloudabi armv7-unknown-cloudabi-eabihf i686-unknown-cloudabi powerpc-unknown-linux-gnuspe sparc-unknown-linux-gnu mips-unknown-unknown-linux-uclib i686-unknown-haiku i686-unknown-netbsd mipsel-unknown-unknown-linux-uclib sparc64-unknown-netbsd x86_64-unknown-bitrig x86_64-unknown-haiku x86_64-unknown-openbsd
-            TARGETS="${TARGETS} aarch64-fuchsia armv7-linux-androideabi armv7-unknown-linux-gnueabihf armv7-unknown-linux-musleabihf i586-unknown-linux-gnu i586-unknown-linux-musl i686-linux-android i686-unknown-freebsd mipsel-unknown-linux-gnu x86_64-fuchsia x86_64-rumprun-netbsd x86_64-sun-solaris x86_64-unknown-cloudabi x86_64-unknown-redox thumbv6m-none-eabi thumbv7em-none-eabi thumbv7em-none-eabihf thumbv7m-none-eabi x86_64-fortanix-unknown-sgx"
+            TARGETS="${TARGETS} aarch64-fuchsia  x86_64-fuchsia x86_64-rumprun-netbsd x86_64-unknown-cloudabi x86_64-unknown-redox thumbv6m-none-eabi thumbv7em-none-eabi thumbv7em-none-eabihf thumbv7m-none-eabi x86_64-fortanix-unknown-sgx"
         fi
 
         ;;
     osx*)
-        TARGETS="i686-apple-darwin x86_64-apple-darwin"
-
-        if [ "${RUST}" = "nightly" ]; then
-            TARGETS="${TARGETS} aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios"
-        fi
-
+        TARGETS="i686-apple-darwin x86_64-apple-darwin aarch64-apple-ios armv7-apple-ios armv7s-apple-ios x86_64-apple-ios"
         ;;
     *)
         ;;
