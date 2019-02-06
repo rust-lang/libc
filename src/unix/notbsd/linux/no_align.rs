a23301b343bcba7d60026a83ab895de8560e4f6c
gnzlbg <gonzalobg88@gmail.com>
2019-02-07 00:51:05 +0100

    Fix align on Rust 1.24

--- src/unix/notbsd/linux/no_align.rs
+++ src/unix/notbsd/linux/no_align.rs
@@ -10,7 +10,7 @@ s! {
                       target_arch = "sparc64",
                       all(target_arch = "aarch64", target_env = "musl"))))]
         __align: [::c_long; 0],
-        size: [u8; __SIZEOF_PTHREAD_MUTEXATTR_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_MUTEXATTR_T],
     }
 
     pub struct pthread_rwlockattr_t {
@@ -18,12 +18,12 @@ s! {
         __align: [::c_int; 0],
         #[not(target_env = "musl")]
         __align: [::c_long; 0],
-        size: [u8; __SIZEOF_PTHREAD_RWLOCKATTR_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_RWLOCKATTR_T],
     }
 
     pub struct pthread_condattr_t {
         __align: [::c_int; 0],
-        size: [u8; __SIZEOF_PTHREAD_CONDATTR_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_CONDATTR_T],
     }
 }
 
@@ -33,7 +33,7 @@ s_no_extra_traits! {
         __align: [*const ::c_void; 0],
         #[cfg(not(target_env = "musl"))]
         __align: [::c_longlong; 0],
-        size: [u8; __SIZEOF_PTHREAD_COND_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_COND_T],
     }
 
     pub struct pthread_mutex_t {
@@ -49,7 +49,7 @@ s_no_extra_traits! {
                       all(target_arch = "x86_64",
                           target_pointer_width = "32"))))]
         __align: [::c_longlong; 0],
-        size: [u8; __SIZEOF_PTHREAD_MUTEX_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_MUTEX_T],
     }
 
     pub struct pthread_rwlock_t {
@@ -65,6 +65,6 @@ s_no_extra_traits! {
                       all(target_arch = "x86_64",
                           target_pointer_width = "32"))))]
         __align: [::c_longlong; 0],
-        size: [u8; __SIZEOF_PTHREAD_RWLOCK_T],
+        pub size: [u8; ::__SIZEOF_PTHREAD_RWLOCK_T],
     }
 }
