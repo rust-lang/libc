2d686747b0e22dc55328704ec90953c80c3e1edb
gnzlbg <gonzalobg88@gmail.com>
2019-02-07 10:33:19 +0100

    test more platforms

--- ci/build.sh
+++ ci/build.sh
@@ -54,7 +54,7 @@ test_target() {
 TARGETS=""
 case "${OS}" in
     linux*)
-        TARGETS=" aarch64-apple-ios i686-apple-darwin x86_64-apple-darwin  armv7-apple-ios armv7s-apple-ios x86_64-apple-ios i686-unknown-linux-gnu x86_64-unknown-linux-gnu x86_64-unknown-freebsd x86_64-unknown-netbsd i586-unknown-linux-gnu i686-linux-android i686-unknown-freebsd mipsel-unknown-linux-gnu mips64-unknown-linux-gnuabi64 mips64el-unknown-linux-gnuabi64 mipsel-unknown-linux-gnu mipsel-unknown-linux-musl armv7-linux-androideabi armv7-unknown-linux-gnueabihf  aarch64-linux-android powerpc-unknown-linux-gnu powerpc64-unknown-linux-gnu powerpc64le-unknown-linux-gnu s390x-unknown-linux-gnu x86_64-unknown-linux-musl"
+        TARGETS="i686-unknown-linux-gnu x86_64-unknown-linux-gnu x86_64-unknown-freebsd x86_64-unknown-netbsd i586-unknown-linux-gnu i686-linux-android i686-unknown-freebsd mipsel-unknown-linux-gnu mips64-unknown-linux-gnuabi64 mips64el-unknown-linux-gnuabi64 mipsel-unknown-linux-gnu mipsel-unknown-linux-musl armv7-linux-androideabi armv7-unknown-linux-gnueabihf  aarch64-linux-android powerpc-unknown-linux-gnu powerpc64-unknown-linux-gnu powerpc64le-unknown-linux-gnu s390x-unknown-linux-gnu x86_64-unknown-linux-musl"
 
         if [ "${RUST}" != "1.13.0" ]; then
             TARGETS="${TARGETS} i586-unknown-linux-musl armv7-unknown-linux-musleabihf x86_64-sun-solaris x86_64-linux-android wasm32-unknown-emscripten sparc64-unknown-linux-gnu sparcv9-sun-solaris wasm32-unknown-unknown x86_64-rumprun-netbsd x86_64-unknown-cloudabi"
