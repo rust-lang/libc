1494a7e1a35277bcb7edf61ecf6a53c37d6139ef
gnzlbg <gonzalobg88@gmail.com>
2019-02-11 21:48:10 +0100

    Fix clippy and formatting

--- ctest/testcrate/tests/all.rs
+++ ctest/testcrate/tests/all.rs
@@ -112,7 +112,6 @@ fn t2_cxx() {
     }
 }
 
-
 fn output(cmd: &mut Command) -> (String, ExitStatus) {
     let output = cmd.output().unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
