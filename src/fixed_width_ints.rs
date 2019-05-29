08a4519714444e10d6f494b426805e265f981628
gnzlbg <gonzalobg88@gmail.com>
2019-05-29 13:05:49 +0200

    Refactor fixed-width integer types into its own module

---
+++ src/fixed_width_ints.rs
@@ -0,0 +1,12 @@
+//! This module contains type aliases for C's fixed-width integer types .
+//!
+//! These aliases are deprecated: use the Rust types instead.
+
+pub type int8_t = i8;
+pub type int16_t = i16;
+pub type int32_t = i32;
+pub type int64_t = i64;
+pub type uint8_t = u8;
+pub type uint16_t = u16;
+pub type uint32_t = u32;
+pub type uint64_t = u64;
