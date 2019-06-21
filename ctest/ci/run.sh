807dbec859278b414242a8e59a6537e5022dd6ed
gnzlbg <gonzalobg88@gmail.com>
2019-06-21 12:19:54 +0200

    Bump patch version

--- ctest/ci/run.sh
+++ ctest/ci/run.sh
@@ -10,6 +10,6 @@ set -ex
 mkdir -p target
 git clone https://github.com/rust-lang/libc target/libc
 mkdir -p target/libc/target/ctest
-sed -i 's@ctest = "0.2.3"@ctest = { path = "../../.." }@g' target/libc/libc-test/Cargo.toml
+sed -i 's@ctest = "0.2"@ctest = { path = "../../.." }@g' target/libc/libc-test/Cargo.toml
 
 cargo test --manifest-path target/libc/libc-test/Cargo.toml --target $TARGET
