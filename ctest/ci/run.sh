e17fec6418dbc7146552c12d262c1d8dfcc7f270
gnzlbg <gonzalobg88@gmail.com>
2019-07-03 13:50:05 +0200

    Fix alignment computation once and for all

--- ctest/ci/run.sh
+++ ctest/ci/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 # Builds and runs tests for a particular target passed as an argument to this
 # script.
@@ -10,6 +10,14 @@ set -ex
 mkdir -p target
 git clone https://github.com/rust-lang/libc target/libc
 mkdir -p target/libc/target/ctest
-sed -i 's@ctest = "0.2"@ctest = { path = "../../.." }@g' target/libc/libc-test/Cargo.toml
+
+case $TARGET in
+    *linux*)
+        sed -i 's@ctest = "0.2"@ctest = { path = "../../.." }@g' target/libc/libc-test/Cargo.toml
+        ;;
+    *apple*)
+        sed -i '' 's@ctest = "0.2"@ctest = { path = "../.." }@g' target/libc/libc-test/Cargo.toml
+        ;;
+esac
 
 cargo test --release --manifest-path target/libc/libc-test/Cargo.toml --target $TARGET
