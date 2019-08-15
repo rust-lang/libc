1a458390842538bf882ae9b5381b05c74a71ce83
Sébastien Marie <semarie@users.noreply.github.com>
2019-08-15 12:43:00 +0200

    openbsd: add sparc64 support

--- src/unix/bsd/netbsdlike/openbsd/mod.rs
+++ src/unix/bsd/netbsdlike/openbsd/mod.rs
@@ -1438,6 +1438,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub use self::aarch64::*;
+    } else if #[cfg(target_arch = "sparc64")] {
+        mod sparc64;
+        pub use self::sparc64::*;
     } else {
         // Unknown target_arch
     }
