bde74be6b20fe8dfc9191ccce29718a073166b81
gnzlbg <gonzalobg88@gmail.com>
2019-08-17 14:01:21 +0200

    Rename armv7-wrs-vxworks target

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,10 @@
 #![crate_name = "libc"]
 #![crate_type = "rlib"]
 #![cfg_attr(libc_deny_warnings, deny(warnings))]
-#![allow(bad_style, overflowing_literals, improper_ctypes, unknown_lints)]
-// FIXME: this is due to a rustc bug
-#![allow(redundant_semicolon)]
+#![allow(
+    bad_style, overflowing_literals, improper_ctypes, unknown_lints,
+    redundant_semicolon
+)]
 // Attributes needed when building as part of the standard library
 #![cfg_attr(
     feature = "rustc-dep-of-std",
