efaabd899012b6dc69be760649d7b44d53b865e1
gnzlbg <gonzalobg88@gmail.com>
2019-08-25 16:12:47 +0200

    WIP: use freebsd11 linknames for freebsd10

--- src/unix/bsd/freebsdlike/mod.rs
+++ src/unix/bsd/freebsdlike/mod.rs
@@ -1162,7 +1162,7 @@ extern {
     pub fn getutxline(ut: *const utmpx) -> *mut utmpx;
     pub fn initgroups(name: *const ::c_char, basegid: ::gid_t) -> ::c_int;
     #[cfg_attr(
-        all(target_os = "freebsd", freebsd11),
+        all(target_os = "freebsd", any(freebsd11, freebsd10)),
         link_name = "kevent@FBSD_1.0"
     )]
     pub fn kevent(kq: ::c_int,
@@ -1181,7 +1181,7 @@ extern {
     pub fn mkfifoat(dirfd: ::c_int, pathname: *const ::c_char,
                     mode: ::mode_t) -> ::c_int;
     #[cfg_attr(
-        all(target_os = "freebsd", freebsd11),
+        all(target_os = "freebsd", any(freebsd11, freebsd10)),
         link_name = "mknodat@FBSD_1.1"
     )]
     pub fn mknodat(dirfd: ::c_int, pathname: *const ::c_char,
