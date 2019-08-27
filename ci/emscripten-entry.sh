cd617d78819ca6a5d06a4d33f5db372994151ddb
Thomas Lively <tlively@google.com>
2019-08-27 10:12:14 -0700

    Revert "Add extra debug info in emscripten-entry.sh"
    This reverts commit 89e8ae6ab999460cdbb75e2472775b25166e4ce1.

--- ci/emscripten-entry.sh
+++ ci/emscripten-entry.sh
@@ -12,11 +12,9 @@
 set -ex
 
 # shellcheck disable=SC1091
-echo "IN EMSCRIPTEN ENTRY"
-source /emsdk-portable/emsdk_env.sh
+source /emsdk-portable/emsdk_env.sh &> /dev/null
 
 # emsdk-portable provides a node binary, but we need version 8 to run wasm
 export PATH="/node-v12.3.1-linux-x64/bin:$PATH"
 
-echo "RUNNING with PATH=$PATH"
 exec "$@"
