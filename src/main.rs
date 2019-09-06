3d42ff0872c7dd6f23d17de5cf611f13734a5162
gnzlbg <gonzalobg88@gmail.com>
2019-09-06 20:21:32 +0200

    fixup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 #![feature(unwind_attributes)]
-#[repr(C, align(64))] struct JmpBuf([i32; 512]);
+#[repr(C, align(16))] struct JmpBuf([u8; 2048]);
 
 use std::mem::MaybeUninit;
-static mut JMP_BUF: MaybeUninit<JmpBuf> = MaybeUninit::uninit();
+static mut JMP_BUF: MaybeUninit<JmpBuf> = MaybeUninit::zeroed();
 
 extern "C" {
-    fn longjmp(env: *mut JmpBuf, status: i32);
+    fn longjmp(env: *mut JmpBuf, status: i32) -> !;
     fn setjmp(env: *mut JmpBuf) -> i32;
 }
 
 unsafe extern "C" fn oh_noes() {
-    longjmp(JMP_BUF.as_mut_ptr(), 1)
+    longjmp(JMP_BUF.as_mut_ptr(), 1);
+    panic!("ohh noes");
 }
 
 unsafe fn jumps() {
