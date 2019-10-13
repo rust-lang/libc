f5ff9d9a718ea16a6293db4cccb3051b3324e697
GrayJack <gr41.j4ck@gmail.com>
2019-10-13 16:10:47 -0300

    Format settimeofday on openbsd

--- src/unix/bsd/netbsdlike/openbsd/mod.rs
+++ src/unix/bsd/netbsdlike/openbsd/mod.rs
@@ -1374,7 +1374,10 @@ f! {
 
 extern "C" {
     pub fn gettimeofday(tp: *mut ::timeval, tz: *mut ::timezone) -> ::c_int;
-    pub fn settimeofday(tp: *const ::timeval, tz: *const ::timezone) -> ::c_int;
+    pub fn settimeofday(
+        tp: *const ::timeval,
+        tz: *const ::timezone
+    ) -> ::c_int;
     pub fn accept4(
         s: ::c_int,
         addr: *mut ::sockaddr,
