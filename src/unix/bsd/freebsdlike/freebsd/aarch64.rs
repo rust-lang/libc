9340bb3516bc287794f7c44efd3ea01d2e48a2b4
Greg V <greg@unrelenting.technology>
2019-12-30 00:11:05 +0300

    FreeBSD: add mcontext_t/ucontext_t (only amd64 for now)

--- src/unix/bsd/freebsdlike/freebsd/aarch64.rs
+++ src/unix/bsd/freebsdlike/freebsd/aarch64.rs
@@ -4,6 +4,7 @@ pub type c_ulong = u64;
 pub type wchar_t = u32;
 pub type time_t = i64;
 pub type suseconds_t = i64;
+pub type register_t = i64;
 
 // should be pub(crate), but that requires Rust 1.18.0
 cfg_if! {
--- src/unix/bsd/freebsdlike/freebsd/powerpc64.rs
+++ src/unix/bsd/freebsdlike/freebsd/powerpc64.rs
@@ -4,6 +4,7 @@ pub type c_ulong = u64;
 pub type wchar_t = i32;
 pub type time_t = i64;
 pub type suseconds_t = i64;
+pub type register_t = i64;
 
 s! {
     pub struct stat {
