7f869d167f959c42804c40d461b6c68f133116f5
Isawan Millican <admin@isawan.net>
2020-02-02 13:34:07 +0000

    Added sys/capability.h header to test

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -2159,6 +2159,7 @@ fn test_linux(target: &str) {
                "stdio.h",
                "stdlib.h",
                "string.h",
+               "sys/capability.h",
                "sys/epoll.h",
                "sys/eventfd.h",
                "sys/file.h",
