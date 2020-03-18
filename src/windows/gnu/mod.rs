0327cc0b3cc2a5fb1ffc069fc845662b932eef40
Lzu Tao <taolzu@gmail.com>
2020-03-18 11:57:28 +0700

    Remove MSVC's wmemchr declaration

--- src/windows/gnu/mod.rs
+++ src/windows/gnu/mod.rs
@@ -13,6 +13,14 @@ extern "C" {
         s2: *const ::c_char,
         n: ::size_t,
     ) -> ::c_int;
+
+    // NOTE: For MSVC target, `wmemchr` is only a inline function in `<wchar.h>`
+    //      header file. We cannot find a way to link to that symbol from Rust.
+    pub fn wmemchr(
+        cx: *const ::wchar_t,
+        c: ::wchar_t,
+        n: ::size_t,
+    ) -> *mut ::wchar_t;
 }
 
 cfg_if! {
