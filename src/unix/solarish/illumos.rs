b53ccb8740491e315b98969e741c3e0be4e007b9
Patrick Mooney <pmooney@pfmooney.com>
2020-06-08 17:27:00 -0500

    illumos: Add eventfd support

--- src/unix/solarish/illumos.rs
+++ src/unix/solarish/illumos.rs
@@ -18,7 +18,13 @@ s! {
 pub const AF_LOCAL: ::c_int = 1; // AF_UNIX
 pub const AF_FILE: ::c_int = 1; // AF_UNIX
 
+pub const EFD_SEMAPHORE: ::c_int = 0x1;
+pub const EFD_NONBLOCK: ::c_int = 0x800;
+pub const EFD_CLOEXEC: ::c_int = 0x80000;
+
 extern "C" {
+    pub fn eventfd(init: ::c_uint, flags: ::c_int) -> ::c_int;
+
     pub fn mincore(
         addr: ::caddr_t,
         len: ::size_t,
