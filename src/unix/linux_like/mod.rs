57298d063710d10c66981c14a0a828879014cec7
Josh Triplett <josh@joshtriplett.org>
2020-08-10 20:23:03 -0700

    Add W_STOPCODE to construct a stop code
    On Linux, `sys/wait.h` defines a `W_STOPCODE` macro to construct a stop
    code from the signal number of a stopping signal. Provide an equivalent
    function.
    
    Suggested-by: Ivan Tham <pickfire@riseup.net>

--- src/unix/linux_like/mod.rs
+++ src/unix/linux_like/mod.rs
@@ -1258,6 +1258,10 @@ f! {
         (ret << 8) | sig
     }
 
+    pub fn W_STOPCODE(sig: ::c_int) -> ::c_int {
+        (sig << 8) | 0x7f
+    }
+
     pub fn QCMD(cmd: ::c_int, type_: ::c_int) -> ::c_int {
         (cmd << 8) | (type_ & 0x00ff)
     }
