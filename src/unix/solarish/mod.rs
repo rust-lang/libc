c35cc0a1886161d54ddbed094bffcaec1b20e5b0
Jason King <jason.brian.king@gmail.com>
2020-10-30 00:32:04 +0000

    Add additional solarish TCP options

--- src/unix/solarish/mod.rs
+++ src/unix/solarish/mod.rs
@@ -1387,6 +1387,9 @@ pub const IPV6_LEAVE_GROUP: ::c_int = 10;
 
 pub const TCP_NODELAY: ::c_int = 1;
 pub const TCP_KEEPIDLE: ::c_int = 34;
+pub const TCP_KEEPCNT: ::c_int = 35;
+pub const TCP_KEEPINTVL: ::c_int = 36;
+
 pub const SOL_SOCKET: ::c_int = 0xffff;
 pub const SO_DEBUG: ::c_int = 0x01;
 pub const SO_ACCEPTCONN: ::c_int = 0x0002;
