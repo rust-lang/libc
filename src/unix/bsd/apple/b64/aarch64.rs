10347e8604257f5396c568d8ec69b11d15f79f5a
Benedikt Steinbusch <ben@rockshrub.de>
2020-12-23 22:00:25 +0100

    provide complete definition of __darwin_mcontext64 on aarch64

--- src/unix/bsd/apple/b64/aarch64.rs
+++ src/unix/bsd/apple/b64/aarch64.rs
@@ -1,8 +1,14 @@
 s! {
     pub struct __darwin_mcontext64 {
-        pub __es: ::__darwin_x86_exception_state64,
+        pub __es: __darwin_arm_exception_state64,
         pub __ss: __darwin_arm_thread_state64,
-        pub __fs: ::__darwin_x86_float_state64,
+        pub __ns: __darwin_arm_neon_state64,
+    }
+
+    pub struct __darwin_arm_exception_state64 {
+        pub __far: u64,
+        pub __esr: u32,
+        pub __exception: u32,
     }
 
     pub struct __darwin_arm_thread_state64 {
@@ -13,5 +19,11 @@ s! {
         pub __pc: u64,
         pub __cpsr: u32,
         pub __pad: u32,
+    }
+
+    pub struct __darwin_arm_neon_state64 {
+        pub __v: [u128; 32],
+        pub __fpsr: u32,
+        pub __fpcr: u32,
     }
 }
