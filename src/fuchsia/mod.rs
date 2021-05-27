9458a0ef20c377d6c91acf36f79adf63d580cb1b
Aymerick Valette <valaymerick@gmail.com>
2021-05-27 14:14:49 +0000

    add IP_TOS and IP_RECVTOS fuschia APIs

--- src/fuchsia/mod.rs
+++ src/fuchsia/mod.rs
@@ -1768,8 +1768,10 @@ pub const SCM_TIMESTAMP: ::c_int = SO_TIMESTAMP;
 pub const SOCK_RAW: ::c_int = 3;
 pub const SOCK_RDM: ::c_int = 4;
 
+pub const IP_TOS: ::c_int = 1;
 pub const IP_TTL: ::c_int = 2;
 pub const IP_HDRINCL: ::c_int = 3;
+pub const IP_RECVTOS: ::c_int = 13;
 pub const IP_FREEBIND: ::c_int = 15;
 pub const IP_TRANSPARENT: ::c_int = 19;
 pub const IP_MULTICAST_IF: ::c_int = 32;
