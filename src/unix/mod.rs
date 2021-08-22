f0dd7a93e9b7f45382dc6d96e9308881a0874ff0
Hans Christian Schmitz <hcsch@hcsch.eu>
2021-08-22 17:55:13 +0200

    Add `strtof` for all platforms that have `strtod`
    All platforms that have `strtod` very likely also have `strtof`. Having
    `strtof` allows for fuzzing of `hexf-parse::parse_hexf32` comparing
    against `strtof`, which can't (easily) be done with `strtod`, due to
    float rounding and over-/underflow behavior.

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -486,6 +486,7 @@ extern "C" {
         link_name = "strtod$UNIX2003"
     )]
     pub fn strtod(s: *const c_char, endp: *mut *mut c_char) -> c_double;
+    pub fn strtof(s: *const c_char, endp: *mut *mut c_char) -> c_float;
     pub fn strtol(s: *const c_char, endp: *mut *mut c_char, base: c_int) -> c_long;
     pub fn strtoul(s: *const c_char, endp: *mut *mut c_char, base: c_int) -> c_ulong;
     pub fn calloc(nobj: size_t, size: size_t) -> *mut c_void;
