6924fee5617c96b88e3671cb251061fed82d4751
David Carlier <devnexen@gmail.com>
2021-10-16 00:17:54 +0100

    windows deprecate CONTEXT and related types.

--- src/windows/msvc/x86_64/align.rs
+++ src/windows/msvc/x86_64/align.rs
@@ -1,3 +1,4 @@
+#[allow(deprecated)]
 pub type XMM_SAVE_AREA32 = XSAVE_FORMAT;
 
 s_no_extra_traits! {
@@ -40,12 +41,18 @@ cfg_if! {
 }
 
 s! {
+    #[doc(hidden)]
+    #[deprecated(since = "0.2.104", note = "use the `winapi` crate instead; we're going to
+remove it in a future release")]
     #[repr(align(16))]
     pub struct M128A {
         pub Low: ::c_ulonglong,
         pub High: ::c_longlong,
     }
 
+    #[doc(hidden)]
+    #[deprecated(since = "0.2.104", note = "use the `winapi` crate instead; we're going to
+remove it in a future release")]
     #[repr(align(16))]
     pub struct XSAVE_FORMAT {
         pub ControlWord: ::c_ushort,
@@ -88,6 +95,9 @@ s! {
         pub Xmm15: M128A,
     }
 
+    #[doc(hidden)]
+    #[deprecated(since = "0.2.104", note = "use the `winapi` crate instead; we're going to
+remove it in a future release")]
     #[repr(align(16))]
     pub struct CONTEXT {
         pub P1Home: u64,
