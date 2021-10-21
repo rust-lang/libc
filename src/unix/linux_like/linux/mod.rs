157ca896b629f0d5b7bb196404b113916b95dcc8
Matthias Schiffer <mschiffer@universe-factory.net>
2021-10-21 15:55:17 +0200

    linux: Add CLOSE_RANGE_* flags
    Flags for the SYS_close_range syscall from <linux/close_range.h>.

--- src/unix/linux_like/linux/mod.rs
+++ src/unix/linux_like/linux/mod.rs
@@ -1818,6 +1818,10 @@ pub const MFD_CLOEXEC: ::c_uint = 0x0001;
 pub const MFD_ALLOW_SEALING: ::c_uint = 0x0002;
 pub const MFD_HUGETLB: ::c_uint = 0x0004;
 
+// linux/close_range.h
+pub const CLOSE_RANGE_UNSHARE: ::c_uint = 1 << 1;
+pub const CLOSE_RANGE_CLOEXEC: ::c_uint = 1 << 2;
+
 // these are used in the p_type field of Elf32_Phdr and Elf64_Phdr, which has
 // the type Elf32Word and Elf64Word respectively. Luckily, both of those are u32
 // so we can use that type here to avoid having to cast.
