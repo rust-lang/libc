3e65f555d7483549cdd99d60580141f9a843081b
Yuki Okushi <yuki.okushi@huawei.com>
2021-11-03 16:24:14 +0900

    sparc64: Update debian image to 11 (2021-10-20) to fix segfault on CI
    I'm not sure why, but this certainly fixes the segfault.

--- ci/linux-sparc64.sh
+++ ci/linux-sparc64.sh
@@ -5,11 +5,11 @@ set -ex
 mkdir -m 777 /qemu
 cd /qemu
 
-curl --retry 5 -LO https://cdimage.debian.org/cdimage/ports/snapshots/2020-10-13/debian-10.0.0-sparc64-NETINST-1.iso
-7z e debian-10.0.0-sparc64-NETINST-1.iso install/initrd.gz
-7z e debian-10.0.0-sparc64-NETINST-1.iso install/vmlinux
+curl --retry 5 -LO https://cdimage.debian.org/cdimage/ports/snapshots/2021-10-20/debian-11.0.0-sparc64-NETINST-1.iso
+7z e debian-11.0.0-sparc64-NETINST-1.iso install/initrd.gz
+7z e debian-11.0.0-sparc64-NETINST-1.iso install/vmlinux
 mv vmlinux kernel
-rm debian-10.0.0-sparc64-NETINST-1.iso
+rm debian-11.0.0-sparc64-NETINST-1.iso
 
 mkdir init
 cd init
