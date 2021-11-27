20f976ee4e3e435a5ff75f58fa2d8e4f6ad8e111
Tobias Kortkamp <t@tobik.me>
2021-11-27 07:31:00 +0100

    Add riscv64-unknown-freebsd support

--- src/unix/bsd/freebsdlike/freebsd/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/mod.rs
@@ -3845,6 +3845,9 @@ cfg_if! {
     } else if #[cfg(target_arch = "powerpc")] {
         mod powerpc;
         pub use self::powerpc::*;
+    } else if #[cfg(target_arch = "riscv64")] {
+        mod riscv64;
+        pub use self::riscv64::*;
     } else {
         // Unknown target_arch
     }
