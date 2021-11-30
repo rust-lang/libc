621a3c2a54bb6c03760bfb0c84e86129be83a315
David Carlier <devnexen@gmail.com>
2021-11-30 19:33:26 +0000

    linux seccomp extension for glibc

--- src/unix/linux_like/linux/gnu/b32/x86/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/x86/mod.rs
@@ -235,6 +235,12 @@ s! {
         pub ss_flags: ::c_int,
         pub ss_size: ::size_t
     }
+
+    pub struct seccomp_notif_sizes {
+        pub seccomp_notif: ::__u16,
+        pub seccomp_notif_resp: ::__u16,
+        pub seccomp_data: ::__u16,
+    }
 }
 
 s_no_extra_traits! {
@@ -1114,6 +1120,11 @@ pub const REG_CS: ::c_int = 15;
 pub const REG_EFL: ::c_int = 16;
 pub const REG_UESP: ::c_int = 17;
 pub const REG_SS: ::c_int = 18;
+
+pub const SECCOMP_SET_MODE_STRICT: ::c_uint = 0;
+pub const SECCOMP_SET_MODE_FILTER: ::c_uint = 1;
+pub const SECCOMP_GET_ACTION_AVAIL: ::c_uint = 2;
+pub const SECCOMP_GET_NOTIF_SIZES: ::c_uint = 3;
 
 extern "C" {
     pub fn getcontext(ucp: *mut ucontext_t) -> ::c_int;
--- src/unix/linux_like/linux/gnu/b64/aarch64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/aarch64/mod.rs
@@ -194,6 +194,12 @@ s! {
         pub imr_address: ::in_addr,
         pub imr_ifindex: ::c_int,
     }
+
+    pub struct seccomp_notif_sizes {
+        pub seccomp_notif: ::__u16,
+        pub seccomp_notif_resp: ::__u16,
+        pub seccomp_data: ::__u16,
+    }
 }
 
 pub const VEOF: usize = 4;
@@ -525,6 +531,11 @@ pub const B2500000: ::speed_t = 0o010014;
 pub const B3000000: ::speed_t = 0o010015;
 pub const B3500000: ::speed_t = 0o010016;
 pub const B4000000: ::speed_t = 0o010017;
+
+pub const SECCOMP_SET_MODE_STRICT: ::c_uint = 0;
+pub const SECCOMP_SET_MODE_FILTER: ::c_uint = 1;
+pub const SECCOMP_GET_ACTION_AVAIL: ::c_uint = 2;
+pub const SECCOMP_GET_NOTIF_SIZES: ::c_uint = 3;
 
 pub const VEOL: usize = 11;
 pub const VEOL2: usize = 16;
