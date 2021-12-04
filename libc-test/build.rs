75dad067bfd2cc06695047ce037cc53ca2e39cc0
David CARLIER <devnexen@gmail.com>
2021-12-04 08:59:30 +0000

    freebsd 14 adding linux sched api compatibility fn.

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -1767,6 +1767,8 @@ fn test_freebsd(target: &str) {
         _ => &mut cfg,
     };
 
+    // For sched linux compat fn
+    cfg.define("_WITH_CPU_SET_T", None);
     // Required for `getline`:
     cfg.define("_WITH_GETLINE", None);
     // Required for making freebsd11_stat available in the headers
@@ -2220,6 +2222,13 @@ fn test_freebsd(target: &str) {
             // We should probably be at least using `&`/`&mut` here, see:
             // https://github.com/gnzlbg/ctest/issues/68
             "lio_listio" => true,
+
+            // Those are introduced in FreeBSD 14.
+            "sched_getaffinity" | "sched_setaffinity" | "sched_getcpu"
+                if Some(14) > freebsd_ver =>
+            {
+                true
+            }
 
             _ => false,
         }
