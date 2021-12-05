b1a407a9933866a26f8d2b7b50c154fde5f981ad
Mek101 <mek101-dev.inv@slmail.me>
2021-12-05 10:40:28 +0100

    Fix ioctl constants on mips little-endian

--- src/unix/linux_like/linux/arch/mips/mod.rs
+++ src/unix/linux_like/linux/arch/mips/mod.rs
@@ -134,8 +134,8 @@ pub const IBSHIFT: ::tcflag_t = 16;
 
 cfg_if! {
     if #[cfg(target_endian = "little")] {
-        pub const BLKSSZGET: ::c_int = 0x1268;
-        pub const BLKPBSZGET: ::c_int = 0x127B;
+        pub const BLKSSZGET: ::c_int = 0x20001268;
+        pub const BLKPBSZGET: ::c_int = 0x2000127B;
     } else if #[cfg(target_endian = "big")] {
         pub const BLKSSZGET: ::c_int = 0x20001268;
         pub const BLKPBSZGET: ::c_int = 0x2000127B;
