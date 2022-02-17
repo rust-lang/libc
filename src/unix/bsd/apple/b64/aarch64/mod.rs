2db639b5d68979658048114bbe01afd2a5da433b
lyinch <backes.thierry@gmail.com>
2022-02-17 17:12:59 +0100

    Fix linter issue

--- src/unix/bsd/apple/b64/aarch64/mod.rs
+++ src/unix/bsd/apple/b64/aarch64/mod.rs
@@ -2,7 +2,6 @@ pub type boolean_t = ::c_int;
 
 pub const CLOCK_UPTIME_RAW: ::clockid_t = 8;
 
-
 s! {
     pub struct malloc_zone_t {
         __private: [::uintptr_t; 18], // FIXME: needs arm64 auth pointers support
