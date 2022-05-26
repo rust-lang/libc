bca896be1db2767a0b5d3cbc0f8d5e7df14238fe
David CARLIER <devnexen@gmail.com>
2022-05-26 18:15:42 +0100

    apple adding `IPV6_BOUND_IF` socket constant.
    closes #2793

--- src/unix/bsd/apple/mod.rs
+++ src/unix/bsd/apple/mod.rs
@@ -3561,6 +3561,7 @@ pub const IP_ADD_SOURCE_MEMBERSHIP: ::c_int = 70;
 pub const IP_DROP_SOURCE_MEMBERSHIP: ::c_int = 71;
 pub const IP_BLOCK_SOURCE: ::c_int = 72;
 pub const IP_UNBLOCK_SOURCE: ::c_int = 73;
+pub const IPV6_BOUND_IF: ::c_int = 125;
 
 pub const TCP_NOPUSH: ::c_int = 4;
 pub const TCP_NOOPT: ::c_int = 8;
