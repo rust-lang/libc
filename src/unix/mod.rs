d6d7bfdb055f4158fe41a1d5e1ee64de0d31840d
Bryanskiy <ivakin.kir@gmail.com>
2022-07-31 19:53:55 +0300

    getting back android traget in case without rustc-dep-of-std

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -357,6 +357,7 @@ cfg_if! {
     } else if #[cfg(any(target_os = "macos",
                         target_os = "ios",
                         target_os = "watchos",
+                        target_os = "android",
                         target_os = "openbsd"))] {
         #[link(name = "c")]
         #[link(name = "m")]
