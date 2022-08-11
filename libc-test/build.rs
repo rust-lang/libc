f49efe43ad6063ff2ef030ee8601cb8ec2c2852b
Christian Legnitto <christian@legnitto.com>
2022-08-11 10:35:17 +0200

    Limit `gnu/libc-version.h` header to glibc

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -2675,7 +2675,7 @@ fn test_linux(target: &str) {
                "elf.h",
                "fcntl.h",
                "glob.h",
-               "gnu/libc-version.h",
+               [gnu]: "gnu/libc-version.h",
                "grp.h",
                "iconv.h",
                "ifaddrs.h",
