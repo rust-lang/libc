8a045af255a7cb0d1516fe8c21fdf73f704d3548
Thom Chiovoloni <thom@shift.click>
2022-10-06 21:05:29 -0700

    Don't link against iconv on apple targets when used by `std`

--- src/unix/bsd/apple/mod.rs
+++ src/unix/bsd/apple/mod.rs
@@ -5873,7 +5873,10 @@ cfg_if! {
     }
 }
 
-#[link(name = "iconv")]
+// These require a dependency on `libiconv`, and including this when built as
+// part of `std` means every Rust program gets it. Ideally we would have a link
+// modifier to only include these if they are used, but we do not.
+#[cfg_attr(not(feature = "rustc-dep-of-std"), link(name = "iconv"))]
 extern "C" {
     pub fn iconv_open(tocode: *const ::c_char, fromcode: *const ::c_char) -> iconv_t;
     pub fn iconv(
