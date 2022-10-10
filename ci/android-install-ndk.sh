1f8c652935c3fe5e479fbc149bef53b1fb97883d
Yuki Okushi <jtitor@2k36.org>
2022-10-10 12:57:52 +0900

    Use Android NDK r25b
    Signed-off-by: Yuki Okushi <jtitor@2k36.org>

--- ci/android-install-ndk.sh
+++ ci/android-install-ndk.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-NDK=android-ndk-r21d
-wget --tries=20 -q https://dl.google.com/android/repository/${NDK}-linux-x86_64.zip
-unzip -q ${NDK}-linux-x86_64.zip
+NDK=android-ndk-r25b
+wget --tries=20 -q https://dl.google.com/android/repository/${NDK}-linux.zip
+unzip -q ${NDK}-linux.zip
 
 case "$1" in
   arm)
@@ -33,9 +33,14 @@ case "$1" in
     ;;
 esac;
 
+STANDALONE_NDK="/android/ndk-${1}"
 python3 ${NDK}/build/tools/make_standalone_toolchain.py \
-        --install-dir "/android/ndk-${1}" \
+        --install-dir "${STANDALONE_NDK}" \
         --arch "${arch}" \
         --api ${api}
 
-rm -rf ./${NDK}-linux-x86_64.zip ./${NDK}
+# FIXME: workaround of ndk23 https://github.com/rust-lang/rust/pull/85806#issuecomment-1096266946
+find "/android/ndk-${1}" -name "libunwind.a" -exec sh -c \
+        'printf "INPUT(-lunwind)" > $(dirname $1)/libgcc.a' shell {} \;
+
+rm -rf ./${NDK}-linux.zip
