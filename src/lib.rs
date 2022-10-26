7ccd2753aa1e26301c73bdb6f9e2df8085ac39e6
Nicole L <dlegare.1001@gmail.com>
2022-10-26 14:39:05 -0700

    Add Trusty platform support

--- src/lib.rs
+++ src/lib.rs
@@ -139,6 +139,12 @@ cfg_if! {
 
         mod hermit;
         pub use hermit::*;
+    } else if #[cfg(target_os = "trusty")] {
+        mod fixed_width_ints;
+        pub use fixed_width_ints::*;
+
+        mod trusty;
+        pub use trusty::*;
     } else if #[cfg(all(target_env = "sgx", target_vendor = "fortanix"))] {
         mod fixed_width_ints;
         pub use fixed_width_ints::*;
