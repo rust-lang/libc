11568d45cae32d8c41e3adc6e0be35361e80420d
David Tolnay <dtolnay@gmail.com>
2022-11-21 10:39:47 -0800

    Add kexec_file_load system call for arm64 linux

--- src/unix/linux_like/linux/gnu/b64/aarch64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/aarch64/mod.rs
@@ -866,6 +866,7 @@ pub const SYS_pkey_alloc: ::c_long = 289;
 pub const SYS_pkey_free: ::c_long = 290;
 pub const SYS_statx: ::c_long = 291;
 pub const SYS_rseq: ::c_long = 293;
+pub const SYS_kexec_file_load: ::c_long = 294;
 pub const SYS_pidfd_send_signal: ::c_long = 424;
 pub const SYS_io_uring_setup: ::c_long = 425;
 pub const SYS_io_uring_enter: ::c_long = 426;
