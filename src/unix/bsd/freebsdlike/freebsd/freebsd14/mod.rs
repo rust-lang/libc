cbd5ecc8ba0629cc9decc33002506fdf7c2bc020
Ihor Antonov <ihor@antonovs.family>
2023-01-14 10:34:46 -0800

    freebsd: add basic netlink definitions

--- src/unix/bsd/freebsdlike/freebsd/freebsd14/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/freebsd14/mod.rs
@@ -228,6 +228,35 @@ s! {
         /// kthread flag.
         pub ki_tdflags: ::c_long,
     }
+
+    /// Netlink Message Header
+    pub struct nlmsghdr {
+	    /// Length of message including header
+        pub nlmsg_len: u32,
+    	/// Message type identifier
+        pub nlmsg_type: u16,
+	    /// Flags (NLM_F_)
+        pub nlmsg_flags: u16,
+    	/// Sequence number
+        pub nlmsg_seq: u32,
+	    /// Sending process port ID
+        pub nlmsg_pid: u32,
+    }
+
+
+    /// Netlink ACK Message
+    pub struct nlmsgerr {
+        pub error: ::c_int,
+        pub msg: nlmsghdr,
+    }
+
+    /// Base netlink attribute TLV header
+    pub struct nlattr {
+        /// Total attribute length
+        pub nla_len: u16,
+        /// Attribute type
+        pub nla_type: u16,
+    }
 }
 
 s_no_extra_traits! {
@@ -276,6 +305,33 @@ s_no_extra_traits! {
         pub vn_type: ::c_int,
         pub vn_mode: u16,
         pub vn_devname: [::c_char; ::SPECNAMELEN as usize + 1],
+    }
+
+    pub struct sockaddr_nl {
+        /// sizeof (sockaddr_nl)
+        pub nl_len: u8,
+        /// netlink family
+        pub nl_family: ::sa_family_t,
+        /// reserved, set to 0
+        nl_pad: ::c_ushort,
+        /// desired port ID, 0 for auto-select
+        pub nl_pid: u32,
+        /// multicast groups mask to bind to
+        pub nl_groups: u32
+    }
+}
+
+e! {
+    #[repr(u32)]
+    pub enum nlmsgerr_attrs {
+        /// string, error message
+        NLMSGERR_ATTR_MSG = 1,
+        /// u32, offset of the invalid attr from nl header
+        NLMSGERR_ATTR_OFFS	= 2,
+        /// binary, data to pass to userland
+        NLMSGERR_ATTR_COOKIE = 3,
+        // not supported
+        NLMSGERR_ATTR_POLICY = 4,
     }
 }
 
@@ -449,6 +505,137 @@ cfg_if! {
         }
     }
 }
+// Netlink
+// sys/socket.h
+pub const AF_NETLINK: ::c_int = 38;
+pub const PF_NETLINK: ::c_int = AF_NETLINK;
+
+pub const SOL_NETLINK: ::c_int = 270;
+
+// RFC 3549, 2.3.2 standard flag bits (nlmsg_flags)
+
+/// Indicateds request to kernel
+pub const NLM_F_REQUEST: ::c_int = 1;
+/// Message is part of a group terminated by NLMSG_DONE msg
+pub const NLM_F_MULTI: ::c_int = 2;
+/// Reply with ack message containing resulting error code
+pub const NLM_F_ACK: ::c_int = 4;
+/// (not supported) Echo this request back
+pub const NLM_F_ECHO: ::c_int = 8;
+/// Dump was inconsistent due to sequence change
+pub const NLM_F_DUMP_INTR: ::c_int = 16;
+/// Dump was filtered as requested
+pub const NLM_F_DUMP_FILTERED: ::c_int = 32;
+
+// RFC 3549, 2.3.2 Additional flag bits for GET requests
+//
+/// Return the complete table
+pub const NLM_F_ROOT: ::c_int = 0x100;
+/// Return all entries matching criteria
+pub const NLM_F_MATCH: ::c_int = 0x200;
+/// Return an atomic snapshot (ignored)
+pub const NLM_F_ATOMIC: ::c_int = 0x400;
+pub const NLM_F_DUMP: ::c_int = NLM_F_ROOT | NLM_F_MATCH;
+
+// RFC 3549, 2.3.2 Additional flag bits for NEW requests
+
+/// Replace existing matching config object
+pub const NLM_F_REPLACE: ::c_int = 0x100;
+/// Don't replace the object if exists
+pub const NLM_F_EXCL: ::c_int = 0x200;
+/// Create if it does not exist
+pub const NLM_F_CREATE: ::c_int = 0x400;
+/// Add to end of list
+pub const NLM_F_APPEND: ::c_int = 0x800;
+
+// Modifiers to DELETE requests
+
+/// Do not delete recursively
+pub const NLM_F_NONREC: ::c_int = 0x100;
+
+// Flags for ACK Message
+/// request was capped
+pub const NLM_F_CAPPED: ::c_int = 0x100;
+/// extended ACK TVLs were included
+pub const NLM_F_ACK_TLVS: ::c_int = 0x200;
+
+// RFC 3549, 2.3.2 standard message types (nlmsg_type)
+
+/// Message is ignored
+pub const NLMSG_NOOP: ::c_int = 0x1;
+/// reply error code reporting
+pub const NLMSG_ERROR: ::c_int = 0x2;
+/// Message terminates a multipart message
+pub const NLMSG_DONE: ::c_int = 0x3;
+/// overrun detected, data is lost
+pub const NLMSG_OVERRUN: ::c_int = 0x4;
+/// < 0x10: reserved control messages
+pub const NLMSG_MIN_TYPE: ::c_int = 0x10;
+
+// Defition of numbers assigned to the netlink subsystems.
+
+/// Routing/device hook
+pub const NETLINK_ROUTE: ::c_int = 0;
+/// not supported
+pub const NETLINK_UNUSED: ::c_int = 1;
+/// not supported
+pub const NETLINK_USERSOCK: ::c_int = 2;
+/// not supported
+pub const NETLINK_FIREWALL: ::c_int = 3;
+/// not supported
+pub const NETLINK_SOCK_DIAG: ::c_int = 4;
+/// not supported
+pub const NETLINK_NFLOG: ::c_int = 5;
+/// not supported
+pub const NETLINK_XFRM: ::c_int = 6;
+/// not supported
+pub const NETLINK_SELINUX: ::c_int = 7;
+/// not supported
+pub const NETLINK_ISCSI: ::c_int = 8;
+/// not supported
+pub const NETLINK_AUDIT: ::c_int = 9;
+/// not supported
+pub const NETLINK_FIB_LOOKUP: ::c_int = 10;
+/// not supported
+pub const NETLINK_CONNECTOR: ::c_int = 11;
+/// not supported
+pub const NETLINK_NETFILTER: ::c_int = 12;
+/// not supported
+pub const NETLINK_IP6_FW: ::c_int = 13;
+/// not supported
+pub const NETLINK_DNRTMSG: ::c_int = 14;
+/// not supported
+pub const NETLINK_KOBJECT_UEVENT: ::c_int = 15;
+/// Generic netlink (dynamic families)
+pub const NETLINK_GENERIC: ::c_int = 16;
+
+// Netlink socket operations
+
+/// Subscribe for the specified group notifications
+pub const NETLINK_ADD_MEMBERSHIP: ::c_int = 1;
+/// Unsubscribe from the specified group
+pub const NETLINK_DROP_MEMBERSHIP: ::c_int = 2;
+/// XXX: not supported
+pub const NETLINK_PKTINFO: ::c_int = 3;
+/// XXX: not supported
+pub const NETLINK_BROADCAST_ERROR: ::c_int = 4;
+/// XXX: not supported
+pub const NETLINK_NO_ENOBUFS: ::c_int = 5;
+/// XXX: not supported
+pub const NETLINK_RX_RING: ::c_int = 6;
+/// XXX: not supported
+pub const NETLINK_TX_RING: ::c_int = 7;
+/// XXX: not supported
+pub const NETLINK_LISTEN_ALL_NSID: ::c_int = 8;
+
+pub const NETLINK_LIST_MEMBERSHIPS: ::c_int = 9;
+/// Send only original message header in the reply
+pub const NETLINK_CAP_ACK: ::c_int = 10;
+/// Ack support for receiving additional TLVs in ack
+pub const NETLINK_EXT_ACK: ::c_int = 11;
+/// Strick header checking
+pub const NETLINK_GET_STRICT_CHK: ::c_int = 12;
+
 
 pub const RAND_MAX: ::c_int = 0x7fff_ffff;
 pub const ELAST: ::c_int = 97;
@@ -551,3 +738,4 @@ cfg_if! {
         pub use self::x86_64::*;
     }
 }
+
