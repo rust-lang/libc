f1ede75a5ef5c6cc78b8967c750facf4f4fbe25f
Peter Zhang <mbyzhang@outlook.com>
2023-03-23 17:00:16 +0000

    linux: add PTRACE_GETSIGMASK and PTRACE_SETSIGMASK

--- src/unix/linux_like/linux/musl/mod.rs
+++ src/unix/linux_like/linux/musl/mod.rs
@@ -579,6 +579,8 @@ pub const PTRACE_SEIZE: ::c_int = 0x4206;
 pub const PTRACE_INTERRUPT: ::c_int = 0x4207;
 pub const PTRACE_LISTEN: ::c_int = 0x4208;
 pub const PTRACE_PEEKSIGINFO: ::c_int = 0x4209;
+pub const PTRACE_GETSIGMASK: ::c_uint = 0x420a;
+pub const PTRACE_SETSIGMASK: ::c_uint = 0x420b;
 
 pub const FAN_MARK_INODE: ::c_uint = 0x0000_0000;
 pub const FAN_MARK_MOUNT: ::c_uint = 0x0000_0010;
