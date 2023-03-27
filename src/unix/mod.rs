cbbeb497b63b36e2775b924e9b33eadcdc7af23c
Ola x Nilsson <olani@axis.com>
2023-03-27 14:17:19 +0200

    gnu: Update struct timespec for _TIME_BITS=64
    Big-endian platforms wants 32 bits of padding before tv_nsec,
    little-endian after.
    
    GNU libc always uses long for tv_nsec.

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -74,10 +74,14 @@ s! {
     // See https://sourceware.org/bugzilla/show_bug.cgi?id=16437
     pub struct timespec {
         pub tv_sec: time_t,
-        #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
+        #[cfg(all(gnu_time64_abi, target_endian = "big"))]
+        __pad: i32,
+        #[cfg(all(not(target_env = "gnu"), target_arch = "x86_64", target_pointer_width = "32"))]
         pub tv_nsec: i64,
-        #[cfg(not(all(target_arch = "x86_64", target_pointer_width = "32")))]
+        #[cfg(any(target_env = "gnu", not(all(target_arch = "x86_64", target_pointer_width = "32"))))]
         pub tv_nsec: ::c_long,
+        #[cfg(all(gnu_time64_abi, target_endian = "little"))]
+        __pad: i32,
     }
 
     pub struct rlimit {
