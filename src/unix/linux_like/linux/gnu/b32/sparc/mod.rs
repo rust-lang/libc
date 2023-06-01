b196045cc621a71c42ebbdcca95baa7e64f95854
Andy Caldwell <andycaldwell@microsoft.com>
2023-06-01 19:01:34 +0100

    musl: Remove redundant definitions
    These types are redundant as they are exported from higher-level
    modules.
    
    [ move some non-statfs fixups here from other commits in the series, add
      context to the commmit message - Trevor ]

--- src/unix/linux_like/linux/gnu/b32/sparc/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/sparc/mod.rs
@@ -58,27 +58,6 @@ s! {
         pub ss_size: ::size_t,
     }
 
-    pub struct stat {
-        pub st_dev: ::dev_t,
-        pub st_ino: ::ino64_t,
-        pub st_mode: ::mode_t,
-        pub st_nlink: ::nlink_t,
-        pub st_uid: ::uid_t,
-        pub st_gid: ::gid_t,
-        pub st_rdev: ::dev_t,
-        __pad2: ::c_ushort,
-        pub st_size: ::off64_t,
-        pub st_blksize: ::blksize_t,
-        pub st_blocks: ::blkcnt64_t,
-        pub st_atime: ::time_t,
-        pub st_atime_nsec: ::c_long,
-        pub st_mtime: ::time_t,
-        pub st_mtime_nsec: ::c_long,
-        pub st_ctime: ::time_t,
-        pub st_ctime_nsec: ::c_long,
-        __unused: [::c_long; 2],
-    }
-
     pub struct stat64 {
         pub st_dev: ::dev_t,
         pub st_ino: ::ino64_t,
