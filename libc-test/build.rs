839bfc2d53aab9f36af0fbf8fff66ea863fc757b
David Carlier <devnexen@gmail.com>
2024-02-17 16:52:40 +0000

    adding new syscall id fchmodat2 for glibc/musl x86 (kernel >= 6.6).
    (backport <https://github.com/rust-lang/libc/pull/3588>)
    (cherry picked from commit 14bc9fb8089ed3d8d91938b65cc5cd5a1ad7a013)

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -4210,6 +4210,9 @@ fn test_linux(target: &str) {
                 true
             }
 
+            // FIXME: Requires >= 6.6 kernel headers.
+            "SYS_fchmodat2" => true,
+
             // FIXME: seems to not be available all the time (from <include/linux/sched.h>:
             "PF_VCPU"
             | "PF_IDLE"
