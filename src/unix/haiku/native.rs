179acc2341aa6722faf94752124afb3a7012fe8e
whosehang <whosehang@outlook.com>
2024-04-24 13:54:05 +0800

    chore: fix some typos
    Signed-off-by: whosehang <whosehang@outlook.com>
    
    (backport <https://github.com/rust-lang/libc/pull/3664>)
    (cherry picked from commit e2e73d75fada8cfb606239b3967a66ef8f8eda09)

--- src/unix/haiku/native.rs
+++ src/unix/haiku/native.rs
@@ -1300,7 +1300,7 @@ extern "C" {
     pub fn find_path_for_path_etc(
         path: *const ::c_char,
         dependency: *const ::c_char,
-        architectur: *const ::c_char,
+        architecture: *const ::c_char,
         baseDirectory: path_base_directory,
         subPath: *const ::c_char,
         flags: u32,
