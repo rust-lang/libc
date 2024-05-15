4333c2f1de640379ec5ecbbc79219bef799b5bd0
Mads Marquart <mads@marquart.dk>
2024-05-15 11:58:30 +0200

    Add _NSGetArgv, _NSGetArgc and _NSGetProgname from crt_externs.h
    Available in the headers on all Apple platforms.

--- src/unix/bsd/apple/mod.rs
+++ src/unix/bsd/apple/mod.rs
@@ -6266,7 +6266,11 @@ extern "C" {
     pub fn CCRandomGenerateBytes(bytes: *mut ::c_void, size: ::size_t) -> ::CCRNGStatus;
     pub fn getentropy(buf: *mut ::c_void, buflen: ::size_t) -> ::c_int;
 
+    // crt_externs.h
+    pub fn _NSGetArgv() -> *mut *mut *mut ::c_char;
+    pub fn _NSGetArgc() -> *mut ::c_int;
     pub fn _NSGetEnviron() -> *mut *mut *mut ::c_char;
+    pub fn _NSGetProgname() -> *mut *mut ::c_char;
 
     pub fn vm_allocate(
         target_task: vm_map_t,
