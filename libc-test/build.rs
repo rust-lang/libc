61331df06f425934fa43a506e25217f49a039a7c
Josh Stone <cuviper@gmail.com>
2024-05-17 11:27:55 -0700

    Also skip `MFD_EXEC` and `MFD_NOEXEC_SEAL` on sparc64

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -3910,7 +3910,10 @@ fn test_linux(target: &str) {
                 if ppc64 || riscv64 => true,
 
             // FIXME: requires more recent kernel headers on CI
-            "SECCOMP_FILTER_FLAG_WAIT_KILLABLE_RECV" if sparc64 => true,
+            | "MFD_EXEC"
+            | "MFD_NOEXEC_SEAL"
+            | "SECCOMP_FILTER_FLAG_WAIT_KILLABLE_RECV"
+                if sparc64 => true,
 
             // FIXME: Not currently available in headers on ARM and musl.
             "NETLINK_GET_STRICT_CHK" if arm || musl => true,
