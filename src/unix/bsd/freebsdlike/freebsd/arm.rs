1c1cff2db4b80568b58454e4ea2f60d0ff808680
Nathaniel <me@nathanielbennett.com>
2024-08-17 16:51:44 -0400

    FreeBSD: Add ucontext_t, mcontext_t for all archs

--- src/unix/bsd/freebsdlike/freebsd/arm.rs
+++ src/unix/bsd/freebsdlike/freebsd/arm.rs
@@ -5,6 +5,8 @@ pub type wchar_t = u32;
 pub type time_t = i64;
 pub type suseconds_t = i32;
 pub type register_t = i32;
+pub type __greg_t = ::c_uint;
+pub type __gregset_t = [::__greg_t; 17];
 
 s! {
     pub struct stat {
@@ -33,6 +35,47 @@ s! {
         pub st_birthtime: ::time_t,
         pub st_birthtime_nsec: ::c_long,
         pub st_birthtime_pad: ::c_long,
+    }
+}
+
+s_no_extra_traits! {
+    pub struct mcontext_t {
+        pub __gregs: ::__gregset_t,
+        pub mc_vfp_size: ::__size_t,
+        pub mc_vfp_ptr: *mut ::c_void,
+        pub mc_spare: [::c_uint; 33],
+    }
+}
+
+cfg_if! {
+    if #[cfg(feature = "extra_traits")] {
+        impl PartialEq for mcontext_t {
+            fn eq(&self, other: &mcontext_t) -> bool {
+                self.__gregs == other.__gregs &&
+                self.mc_vfp_size == other.mc_vfp_size &&
+                self.mc_vfp_ptr == other.mc_vfp_ptr &&
+                self.mc_spare.iter().zip(other.mc_spare.iter()).all(|(a, b)| a == b)
+            }
+        }
+        impl Eq for mcontext_t {}
+        impl ::fmt::Debug for mcontext_t {
+            fn fmt(&self, f: &mut ::fmt::Formatter) -> ::fmt::Result {
+                f.debug_struct("mcontext_t")
+                    .field("__gregs", &self.__gregs)
+                    .field("mc_vfp_size", &self.mc_vfp_size)
+                    .field("mc_vfp_ptr", &self.mc_vfp_ptr)
+                    .field("mc_spare", &self.mc_spare)
+                    .finish()
+            }
+        }
+        impl ::hash::Hash for mcontext_t {
+            fn hash<H: ::hash::Hasher>(&self, state: &mut H) {
+                self.__gregs.hash(state);
+                self.mc_vfp_size.hash(state);
+                self.mc_vfp_ptr.hash(state);
+                self.mc_spare.hash(state);
+            }
+        }
     }
 }
 
