507833559bd0bfe04b5139c1607c3dc3208174f5
Trevor Gross <tmgross@umich.edu>
2024-08-31 11:01:53 -0400

    Fix rustc version when `clippy-driver` is used
    This is a combined cherry-pick of the following two commits:
    
    - 18b8da96 ("Handle rustc version output correctly...")
    - cdf12d2a ("Update `rustc_version_cmd`")
    
    These two commits are squashed for the backport to slightly reduce the
    amount of conflict resolution needed going forward (some small tweaks
    were still needed).
    
    This backports the following:
    
    - <https://github.com/rust-lang/libc/pull/3893>
    - <https://github.com/rust-lang/libc/pull/3903>
    
    Commit 1 original message:
    
        Handle rustc version output correctly when `clippy-driver` used
    
    Commit 2 original message:
    
        Update `rustc_version_cmd`
    
        Change `if let` to a `match` because it is about the same complexity but
        also works with our MSRV for 0.2. This should allow backporting [1]
        easier, as well as future backports that touch this code.
    
        Additionally, add some new documentation comments.
    
        [1]: https://github.com/rust-lang/libc/pull/3893
    
    Co-authored-by: Nathaniel Bennett <me@nathanielbennett.com>
    Co-authored-by: Trevor Gross <tmgross@umich.edu>

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::process::Command;
+use std::process::{Command, Output};
 use std::str;
 
 // List of cfgs this build script is allowed to set. The list is needed to support check-cfg, as we
@@ -194,6 +194,41 @@ fn main() {
     }
 }
 
+/// Run `rustc --version` and capture the output, adjusting arguments as needed if `clippy-driver`
+/// is used instead.
+fn rustc_version_cmd(is_clippy_driver: bool) -> Output {
+    let rustc_wrapper = env::var_os("RUSTC_WRAPPER").filter(|w| !w.is_empty());
+    let rustc = env::var_os("RUSTC").expect("Failed to get rustc version: missing RUSTC env");
+
+    let mut cmd = match rustc_wrapper {
+        Some(wrapper) => {
+            let mut cmd = Command::new(wrapper);
+            cmd.arg(rustc);
+            if is_clippy_driver {
+                cmd.arg("--rustc");
+            }
+
+            cmd
+        }
+        None => Command::new(rustc),
+    };
+
+    cmd.arg("--version");
+
+    let output = cmd.output().ok().expect("Failed to get rustc version");
+
+    if !output.status.success() {
+        panic!(
+            "failed to run rustc: {}",
+            String::from_utf8_lossy(output.stderr.as_slice())
+        );
+    }
+
+    output
+}
+
+/// Return the minor version of `rustc`, as well as a bool indicating whether or not the version
+/// is a nightly.
 fn rustc_minor_nightly() -> (u32, bool) {
     macro_rules! otry {
         ($e:expr) => {
@@ -204,29 +239,14 @@ fn rustc_minor_nightly() -> (u32, bool) {
         };
     }
 
-    let rustc = env::var_os("RUSTC").expect("Failed to get rustc version: missing RUSTC env");
-    let mut cmd = match env::var_os("RUSTC_WRAPPER").as_ref() {
-        Some(wrapper) if !wrapper.is_empty() => {
-            let mut cmd = Command::new(wrapper);
-            cmd.arg(rustc);
-            cmd
-        }
-        _ => Command::new(rustc),
-    };
+    let mut output = rustc_version_cmd(false);
 
-    let output = cmd
-        .arg("--version")
-        .output()
-        .ok()
-        .expect("Failed to get rustc version");
-    if !output.status.success() {
-        panic!(
-            "failed to run rustc: {}",
-            String::from_utf8_lossy(output.stderr.as_slice())
-        );
+    if otry!(str::from_utf8(&output.stdout).ok()).starts_with("clippy") {
+        output = rustc_version_cmd(true);
     }
 
     let version = otry!(str::from_utf8(&output.stdout).ok());
+
     let mut pieces = version.split('.');
 
     if pieces.next() != Some("rustc 1") {
