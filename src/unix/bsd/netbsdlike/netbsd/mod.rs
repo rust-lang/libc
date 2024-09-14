4dbb0a4ffa104093117f6f9aae1dd596a98694be
pin <voidpin@protonmail.com>
2024-09-14 06:29:46 +0200

    Add CLOCK_PROCESS_CPUTIME_ID and CLOCK_THREAD_CPUTIME_ID constants

--- src/unix/bsd/netbsdlike/netbsd/mod.rs
+++ src/unix/bsd/netbsdlike/netbsd/mod.rs
@@ -1873,6 +1873,8 @@ pub const MNT_NOWAIT: ::c_int = 2;
 pub const MNT_LAZY: ::c_int = 3;
 
 //<sys/timex.h>
+pub const CLOCK_PROCESS_CPUTIME_ID: ::clockid_t = 2;
+pub const CLOCK_THREAD_CPUTIME_ID: ::clockid_t = 4;
 pub const NTP_API: ::c_int = 4;
 pub const MAXPHASE: ::c_long = 500000000;
 pub const MAXFREQ: ::c_long = 500000;
