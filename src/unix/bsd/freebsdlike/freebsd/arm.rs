8286345bcd00769eac11ee41a0cf974ff0b4298c
Alan Somers <asomers@gmail.com>
2024-09-27 17:37:05 -0600

    Fix definition of clock_t on FreeBSD x86, powerpc, powerpc64, and arm.
    https://github.com/freebsd/freebsd-src/blob/main/sys/arm/include/_types.h
    https://github.com/freebsd/freebsd-src/blob/main/sys/arm64/include/_types.h
    https://github.com/freebsd/freebsd-src/blob/main/sys/powerpc/include/_types.h
    https://github.com/freebsd/freebsd-src/blob/main/sys/riscv/include/_types.h
    https://github.com/freebsd/freebsd-src/blob/main/sys/x86/include/_types.h
    
    (backport <https://github.com/rust-lang/libc/pull/3948>)
    (cherry picked from commit 6605f08aeade93fb6d01c91a13d84769735dc172)

--- src/unix/bsd/freebsdlike/freebsd/arm.rs
+++ src/unix/bsd/freebsdlike/freebsd/arm.rs
@@ -1,6 +1,7 @@
 pub type c_char = u8;
 pub type c_long = i32;
 pub type c_ulong = u32;
+pub type clock_t = u32;
 pub type wchar_t = u32;
 pub type time_t = i64;
 pub type suseconds_t = i32;
--- src/unix/bsd/freebsdlike/freebsd/x86_64/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/x86_64/mod.rs
@@ -1,6 +1,7 @@
 pub type c_char = i8;
 pub type c_long = i64;
 pub type c_ulong = u64;
+pub type clock_t = i32;
 pub type wchar_t = i32;
 pub type time_t = i64;
 pub type suseconds_t = i64;
