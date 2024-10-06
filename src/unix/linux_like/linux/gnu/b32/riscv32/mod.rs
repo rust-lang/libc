f5c431deb60375a7cf18f1b591aed89d54c3bb12
Andy Caldwell <andrew.caldwell@metaswitch.com>
2024-10-06 04:12:33 +0100

    glibc: Remove redundant definitions
    These are redundant as they are exported from higher-level modules.
    
    [ extract this commit to only cover glibc - Trevor ]

--- src/unix/linux_like/linux/gnu/b32/riscv32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/riscv32/mod.rs
@@ -70,21 +70,6 @@ s! {
         pub f_spare: [::c_long; 4],
     }
 
-    pub struct statvfs {
-        pub f_bsize: ::c_ulong,
-        pub f_frsize: ::c_ulong,
-        pub f_blocks: ::fsblkcnt_t,
-        pub f_bfree: ::fsblkcnt_t,
-        pub f_bavail: ::fsblkcnt_t,
-        pub f_files: ::fsfilcnt_t,
-        pub f_ffree: ::fsfilcnt_t,
-        pub f_favail: ::fsfilcnt_t,
-        pub f_fsid: ::c_ulong,
-        pub f_flag: ::c_ulong,
-        pub f_namemax: ::c_ulong,
-        pub __f_spare: [::c_int; 6],
-    }
-
     pub struct statvfs64 {
         pub f_bsize: ::c_ulong,
         pub f_frsize: ::c_ulong,
