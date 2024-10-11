fc93ac8ff0ec653655e27af35b503dc5d59f2626
wlynxg <liuauthor@foxmail.com>
2024-10-11 14:47:33 +0800

    feat: add `TUNSETCARRIER` and `TUNGETDEVNETNS` in `gnu`

--- src/unix/linux_like/linux/gnu/mod.rs
+++ src/unix/linux_like/linux/gnu/mod.rs
@@ -1602,5 +1602,28 @@ cfg_if! {
     }
 }
 
+cfg_if! {
+    if #[cfg(any(target_arch = "x86",
+                 target_arch = "x86_64",
+                 target_arch = "arm",
+                 target_arch = "aarch64",
+                 target_arch = "loongarch64",
+                 target_arch = "riscv64",
+                 target_arch = "s390x"))] {
+        pub const TUNSETCARRIER: ::Ioctl = 0x400454e2;
+        pub const TUNGETDEVNETNS: ::Ioctl = 0x54e3;
+    } else if #[cfg(any(target_arch = "mips",
+                        target_arch = "mips64",
+                        target_arch = "powerpc",
+                        target_arch = "powerpc64",
+                        target_arch = "sparc",
+                        target_arch = "sparc64"))] {
+        pub const TUNSETCARRIER: ::Ioctl = 0x800454e2;
+        pub const TUNGETDEVNETNS: ::Ioctl = 0x200054e3;
+    } else {
+        // Unknown target_arch
+    }
+}
+
 mod align;
 pub use self::align::*;
