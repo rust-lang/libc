58dcc8af13d9e89d6a02e88a924b7680903e2341
Kleis Auke Wolthuizen <github@kleisauke.nl>
2024-11-07 10:08:14 +0100

    emscripten: Remove `aio.h` usage
    See: emscripten-core/emscripten@6416c351c6d98b796dfefd8a8d00d71e83ca7fa5.
    
    (backport <https://github.com/rust-lang/libc/pull/3962>)
    (cherry picked from commit 01c72ee99a0c6ba5bbb393ab3ef486ba06ccb0e9)

--- src/unix/linux_like/emscripten/mod.rs
+++ src/unix/linux_like/emscripten/mod.rs
@@ -171,23 +171,6 @@ s! {
         pub sem_flg: ::c_short,
     }
 
-    pub struct aiocb {
-        pub aio_fildes: ::c_int,
-        pub aio_lio_opcode: ::c_int,
-        pub aio_reqprio: ::c_int,
-        pub aio_buf: *mut ::c_void,
-        pub aio_nbytes: ::size_t,
-        pub aio_sigevent: ::sigevent,
-        __td: *mut ::c_void,
-        __lock: [::c_int; 2],
-        __err: ::c_int,
-        __ret: ::ssize_t,
-        pub aio_offset: off_t,
-        __next: *mut ::c_void,
-        __prev: *mut ::c_void,
-        __dummy4: [::c_char; 24],
-    }
-
     pub struct sigaction {
         pub sa_sigaction: ::sighandler_t,
         pub sa_mask: ::sigset_t,
@@ -916,15 +899,6 @@ pub const SYNC_FILE_RANGE_WRITE: ::c_uint = 2;
 pub const SYNC_FILE_RANGE_WAIT_AFTER: ::c_uint = 4;
 
 pub const EAI_SYSTEM: ::c_int = -11;
-
-pub const AIO_CANCELED: ::c_int = 0;
-pub const AIO_NOTCANCELED: ::c_int = 1;
-pub const AIO_ALLDONE: ::c_int = 2;
-pub const LIO_READ: ::c_int = 0;
-pub const LIO_WRITE: ::c_int = 1;
-pub const LIO_NOP: ::c_int = 2;
-pub const LIO_WAIT: ::c_int = 0;
-pub const LIO_NOWAIT: ::c_int = 1;
 
 pub const MREMAP_MAYMOVE: ::c_int = 1;
 pub const MREMAP_FIXED: ::c_int = 2;
