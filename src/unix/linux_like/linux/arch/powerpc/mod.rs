b3884fb8bfdb17bd911de42ad46d7f46f8700c8c
Pedro Tammela <pctammela@gmail.com>
2024-11-21 13:06:04 -0300

    Add SO_PREFER_BUSY_POLL and SO_BUSY_POLL_BUDGET
    Remove the comment of these socket options.
    Reference: https://elixir.bootlin.com/linux/latest/source/include/uapi/asm-generic/socket.h
    
    Note, musl hardcodes 'SO_*' constants instead of inheriting them from the OS.
    
    Signed-off-by: Pedro Tammela <pctammela@gmail.com>

--- src/unix/linux_like/linux/arch/powerpc/mod.rs
+++ src/unix/linux_like/linux/arch/powerpc/mod.rs
@@ -84,8 +84,8 @@ pub const SO_BINDTOIFINDEX: c_int = 62;
 // pub const SO_RCVTIMEO_NEW: c_int = 66;
 // pub const SO_SNDTIMEO_NEW: c_int = 67;
 // pub const SO_DETACH_REUSEPORT_BPF: c_int = 68;
-// pub const SO_PREFER_BUSY_POLL: c_int = 69;
-// pub const SO_BUSY_POLL_BUDGET: c_int = 70;
+pub const SO_PREFER_BUSY_POLL: c_int = 69;
+pub const SO_BUSY_POLL_BUDGET: c_int = 70;
 
 pub const FICLONE: c_ulong = 0x80049409;
 pub const FICLONERANGE: c_ulong = 0x8020940D;
