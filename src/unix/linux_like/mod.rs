c082745f7c0b7860874fd838f83bb4e2e0f9e50e
xd009642 <danielmckenna93@gmail.com>
2025-01-14 01:18:49 +0900

    Move constants around

--- src/unix/linux_like/mod.rs
+++ src/unix/linux_like/mod.rs
@@ -1297,7 +1297,6 @@ pub const SI_MESGQ: c_int = -3;
 pub const SI_ASYNCIO: c_int = -4;
 pub const SI_SIGIO: c_int = -5;
 pub const SI_TKILL: c_int = -6;
-pub const SI_DETHREAD: c_int = -7;
 pub const SI_ASYNCNL: c_int = -60;
 
 // si_code values for SIGBUS signal
@@ -1314,7 +1313,6 @@ pub const TRAP_TRACE: c_int = 2;
 pub const TRAP_BRANCH: c_int = 3;
 pub const TRAP_HWBKPT: c_int = 4;
 pub const TRAP_UNK: c_int = 5;
-pub const TRAP_PERF: c_int = 6;
 
 // si_code values for SIGCHLD signal
 pub const CLD_EXITED: c_int = 1;
