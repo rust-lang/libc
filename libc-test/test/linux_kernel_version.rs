1779f14a0ea8bb663e1e220f006f88284fe5e1b3
Trevor Gross <tmgross@umich.edu>
2025-03-10 05:40:03 +0000

    Change the range syntax that is giving `ctest` problems
    `ctest` is iffy about whether or not it accepts `..=` syntax, and I
    can't figure out what makes it decide whether or not to accept it and
    sometimes random changes seem to make things fail, so just replace the
    syntax.
    
    This is simpler anyway, and closer matches the upstream definition [1].
    
    Link: https://github.com/torvalds/linux/blob/80e54e84911a923c40d7bee33a34c1b4be148d7a/Makefile#L1316 [1]

--- libc-test/test/linux_kernel_version.rs
+++ libc-test/test/linux_kernel_version.rs
@@ -1,15 +1,16 @@
 //! Compare libc's KERNEL_VERSION macro against a specific kernel version.
 
-#[cfg(
-    target_os = "linux",
-)]
+#[cfg(target_os = "linux")]
 mod t {
     use libc;
 
     #[test]
     fn test_kernel_version() {
-        unsafe {
-            assert_eq!(libc::KERNEL_VERSION(6, 0, 0), 393216);
-        }
+        assert_eq!(unsafe { libc::KERNEL_VERSION(6, 0, 0) }, 393216);
+        // Check that the patch level saturates
+        assert_eq!(unsafe { libc::KERNEL_VERSION(6, 0, 255) }, 393471);
+        assert_eq!(unsafe { libc::KERNEL_VERSION(6, 0, 256) }, 393471);
+        assert_eq!(unsafe { libc::KERNEL_VERSION(6, 0, 300) }, 393471);
+        assert_eq!(unsafe { libc::KERNEL_VERSION(6, 0, u32::MAX) }, 393471);
     }
 }
