0c6d56cfe115847344d62756e5d7334740011572
Ola x Nilsson <olani@axis.com>
2025-03-18 16:47:20 +0100

    gnu powerpc: Use a separate stat struct for powerpc
    Like mips, the stat struct will become different once support for
    gnu_file_offset_bits64 is added.

--- src/unix/linux_like/linux/gnu/b32/powerpc.rs
+++ src/unix/linux_like/linux/gnu/b32/powerpc.rs
@@ -57,6 +57,28 @@ s! {
         __glibc_reserved2: u64,
     }
 
+    pub struct stat {
+        pub st_dev: crate::dev_t,
+        pub st_ino: crate::ino_t,
+        pub st_mode: crate::mode_t,
+        pub st_nlink: crate::nlink_t,
+        pub st_uid: crate::uid_t,
+        pub st_gid: crate::gid_t,
+        pub st_rdev: crate::dev_t,
+        __pad2: c_ushort,
+        pub st_size: off_t,
+        pub st_blksize: crate::blksize_t,
+        pub st_blocks: crate::blkcnt_t,
+        pub st_atime: crate::time_t,
+        pub st_atime_nsec: c_long,
+        pub st_mtime: crate::time_t,
+        pub st_mtime_nsec: c_long,
+        pub st_ctime: crate::time_t,
+        pub st_ctime_nsec: c_long,
+        __glibc_reserved4: c_ulong,
+        __glibc_reserved5: c_ulong,
+    }
+
     pub struct stat64 {
         pub st_dev: crate::dev_t,
         pub st_ino: crate::ino64_t,
