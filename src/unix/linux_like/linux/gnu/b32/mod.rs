de7076eb4e71bd0ce0264bd63ec7b16cdaf0464a
Ola x Nilsson <olani@axis.com>
2025-03-25 17:09:13 +0100

    gnu: Handle basic time types for 32bit with _TIME_BITS=64
    Set the basic types correctly for
    gnu_time_bits64 (_TIME_BITS=64 and _FILE_OFFSET_BITS=64).

--- src/unix/linux_like/linux/gnu/b32/mod.rs
+++ src/unix/linux_like/linux/gnu/b32/mod.rs
@@ -29,6 +29,18 @@ cfg_if! {
         pub type fsfilcnt_t = u64;
         pub type rlim_t = u64;
         pub type blksize_t = i64;
+    } else if #[cfg(gnu_time_bits64)] {
+        pub type time_t = i64;
+        pub type suseconds_t = i32;
+        type __ino_t = c_ulong;
+        type __ino64_t = u64;
+        pub type ino_t = __ino64_t;
+        pub type off_t = i64;
+        pub type blkcnt_t = i64;
+        pub type fsblkcnt_t = u64;
+        pub type fsfilcnt_t = u64;
+        pub type rlim_t = u64;
+        pub type blksize_t = i32;
     } else if #[cfg(gnu_file_offset_bits64)] {
         pub type time_t = i32;
         pub type suseconds_t = i32;
