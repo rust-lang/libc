b328398c4a2b857feadba508121415f726f585c1
Trevor Gross <tmgross@umich.edu>
2025-07-08 04:04:58 +0000

    glibc: Initial setup of source reorganization
    Establish the basic source reorganization for glibc, and seed it with a
    `net/route.h` definition.
    
    Glibc has the same headers in various locations, then copies or (I
    believe) combines them in the install location based on the target
    platform. To mirror this, our source primarily mirrors the glibc source
    tree, then reexports things in `src/new/glibc.rs` in the same way that
    glibc does when it creates an install.
    
    (backport <https://github.com/rust-lang/libc/pull/4540>)
    (cherry picked from commit 0399c4ed2ae4d3f7c3c6728b894881cd338e9520)

--- src/new/glibc/mod.rs
+++ src/new/glibc/mod.rs
@@ -2,5 +2,24 @@
 //!
 //! * Headers: <https://sourceware.org/git/?p=glibc.git> (official)
 //! * Headers: <https://github.com/bminor/glibc> (mirror)
+//!
+//! This module structure is modeled after glibc's source tree. Its build system selects headers
+//! from different locations based on the platform, which we mimic here with reexports.
 
-pub(crate) mod unistd;
+/// Source directory: `posix/`
+///
+/// <https://github.com/bminor/glibc/tree/master/posix>
+mod posix {
+    pub(crate) mod unistd;
+}
+
+/// Source directory: `sysdeps/`
+///
+/// <https://github.com/bminor/glibc/tree/master/sysdeps>
+mod sysdeps {
+    pub(crate) mod unix;
+}
+
+pub(crate) use posix::*;
+#[cfg(target_os = "linux")]
+pub(crate) use sysdeps::unix::linux::*;
