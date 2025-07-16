rust-lang/libc

commit 2a68f7f9f6139f8930df345ae19697336908e940

Add musl_v1_2_3 feature
This feature, controlled by the environment variable
RUST_LIBC_UNSTABLE_MUSL_V1_2_3 will control whether breaking changes up
to musl v1.2.3 will be reflected

build.rs
@@ -21,6 +21,7 @@ const ALLOWED_CFGS: &[&str] = &[
     "libc_ctest",
     // Corresponds to `__USE_TIME_BITS64` in UAPI
     "linux_time_bits64",
+    "musl_v1_2_3"
 ];
 
 // Extra values to allow for check-cfg.
@@ -85,6 +86,13 @@ fn main() {
         _ => (),
     }
 
+    let musl_v1_2_3 = env::var("RUST_LIBC_UNSTABLE_MUSL_V1_2_3").is_ok();
+    println!("cargo:rerun-if-env-changed=RUST_LIBC_UNSTABLE_MUSL_V1_2_3");
+    // loongarch64 and ohos have already updated
+    if musl_v1_2_3 || target_os == "loongarch64" || target_env == "ohos" {
+        // FIXME(musl): enable time64 api as well
+        set_cfg("musl_v1_2_3");
+    }
     let linux_time_bits64 = env::var("RUST_LIBC_UNSTABLE_LINUX_TIME_BITS64").is_ok();
     println!("cargo:rerun-if-env-changed=RUST_LIBC_UNSTABLE_LINUX_TIME_BITS64");
     if linux_time_bits64 {

libc-test/build.rs
@@ -3660,6 +3660,9 @@ fn test_linux(target: &str) {
     let old_musl = musl && !musl_v1_2_3;
 
     let mut cfg = ctest_cfg();
+    if musl_v1_2_3 {
+        cfg.cfg("musl_v1_2_3", None);
+    }
     cfg.define("_GNU_SOURCE", None);
     // This macro re-defines fscanf,scanf,sscanf to link to the symbols that are
     // deprecated since glibc >= 2.29. This allows Rust binaries to link against

commit 4c52a681543d58fb8de37322f8b4bb86fbbd5fc4

build: Fix incorrect `target_os` -> `target_arch` check
This was introduced in 2a68f7f9f6 ("Add musl_v1_2_3 feature").

Fixes: https://github.com/rust-lang/libc/issues/4526

build.rs
@@ -91,7 +91,7 @@ fn main() {
     let musl_v1_2_3 = env::var("RUST_LIBC_UNSTABLE_MUSL_V1_2_3").is_ok();
     println!("cargo:rerun-if-env-changed=RUST_LIBC_UNSTABLE_MUSL_V1_2_3");
     // loongarch64 and ohos have already updated
-    if musl_v1_2_3 || target_os == "loongarch64" || target_env == "ohos" {
+    if musl_v1_2_3 || target_arch == "loongarch64" || target_env == "ohos" {
         // FIXME(musl): enable time64 api as well
         set_cfg("musl_v1_2_3");
     }