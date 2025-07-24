e3acb8ad609d072284a8557d582043e73d36bcb2
Trevor Gross <tmgross@umich.edu>
2025-07-23 22:25:11 -0500

    ci: Remove support for testing the Nintendo Switch
    This has been unused since 6ca5bfaea17f ("Setup Azure Pipelines"), and
    the docker image hasn't been updated for two years anyway.

--- ci/run-docker.sh
+++ ci/run-docker.sh
@@ -58,48 +58,11 @@ run() {
         sh -c "HOME=/tmp PATH=\$PATH:/rust/bin exec ci/run.sh $target"
 }
 
-build_switch() {
-    echo "Building docker container for target switch"
-
-    # use -f so we can use ci/ as build context
-    docker build -t libc-switch -f "ci/docker/switch/Dockerfile" ci/
-    mkdir -p target
-    if [ -w /dev/kvm ]; then
-        kvm="--volume /dev/kvm:/dev/kvm"
-    else
-        kvm=""
-    fi
-
-    cp "$(command -v rustup)" "$(rustc --print sysroot)/bin"
-
-    docker run \
-        --rm \
-        --user "$(id -u)":"$(id -g)" \
-        --env LIBC_CI \
-        --env CARGO_HOME=/cargo \
-        --env CARGO_TARGET_DIR=/checkout/target \
-        --volume "$CARGO_HOME":/cargo \
-        --volume "$(rustc --print sysroot)":/rust:ro \
-        --volume "$(pwd)":/checkout:ro \
-        --volume "$(pwd)"/target:/checkout/target \
-        --volume ~/.rustup:/.rustup:Z \
-        $kvm \
-        --init \
-        --workdir /checkout \
-        libc-switch \
-        sh -c "HOME=/tmp RUSTUP_HOME=/tmp PATH=\$PATH:/rust/bin rustup default nightly \
-            && rustup component add rust-src --target ci/switch.json \
-            && cargo build -Z build-std=core,alloc --target ci/switch.json"
-}
-
 if [ -z "$target" ]; then
+    # Run all docker targets
     for d in ci/docker/*; do
         run "${d}"
     done
 else
-    if [ "$target" != "switch" ]; then
-        run "$target"
-    else
-        build_switch
-    fi
+    run "$target"
 fi
