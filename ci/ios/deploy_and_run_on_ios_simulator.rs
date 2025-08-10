c0071cc9f9385da6352e40824cfd77334e85f72d
Trevor Gross <tmgross@umich.edu>
2025-08-09 21:11:26 -0500

    cleanup: Format a file that was missed
    Also start validating formatting in CI for files in `ci/`.

--- ci/ios/deploy_and_run_on_ios_simulator.rs
+++ ci/ios/deploy_and_run_on_ios_simulator.rs
@@ -6,18 +6,19 @@
 // (https://github.com/snipsco/dinghy): cargo dinghy install, then cargo dinghy
 // test.
 
-use std::env;
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::Path;
-use std::process;
 use std::process::Command;
+use std::{env, process};
 
 macro_rules! t {
-    ($e:expr) => (match $e {
-        Ok(e) => e,
-        Err(e) => panic!("{} failed with: {e}", stringify!($e)),
-    })
+    ($e:expr) => {
+        match $e {
+            Ok(e) => e,
+            Err(e) => panic!("{} failed with: {e}", stringify!($e)),
+        }
+    };
 }
 
 // Step one: Wrap as an app
@@ -25,11 +26,15 @@ fn package_as_simulator_app(crate_name: &str, test_binary_path: &Path) {
     println!("Packaging simulator app");
     drop(fs::remove_dir_all("ios_simulator_app"));
     t!(fs::create_dir("ios_simulator_app"));
-    t!(fs::copy(test_binary_path,
-                Path::new("ios_simulator_app").join(crate_name)));
+    t!(fs::copy(
+        test_binary_path,
+        Path::new("ios_simulator_app").join(crate_name)
+    ));
 
     let mut f = t!(File::create("ios_simulator_app/Info.plist"));
-    t!(f.write_all(format!(r#"
+    t!(f.write_all(
+        format!(
+            r#"
         <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE plist PUBLIC
                 "-//Apple//DTD PLIST 1.0//EN"
@@ -42,7 +47,11 @@ fn package_as_simulator_app(crate_name: &str, test_binary_path: &Path) {
                 <string>com.rust.unittests</string>
             </dict>
         </plist>
-    "#, crate_name).as_bytes()));
+    "#,
+            crate_name
+        )
+        .as_bytes()
+    ));
 }
 
 // Step two: Start the iOS simulator
@@ -57,8 +66,10 @@ fn start_simulator() {
     for line in stdout.lines() {
         if line.contains("rust_ios") {
             if found_rust_sim {
-                panic!("Duplicate rust_ios simulators found. Please \
-                        double-check xcrun simctl list.");
+                panic!(
+                    "Duplicate rust_ios simulators found. Please \
+                        double-check xcrun simctl list."
+                );
             }
             simulator_exists = true;
             simulator_booted = line.contains("(Booted)");
@@ -69,62 +80,67 @@ fn start_simulator() {
     if simulator_exists == false {
         println!("Creating iOS simulator");
         Command::new("xcrun")
-                .arg("simctl")
-                .arg("create")
-                .arg("rust_ios")
-                .arg("com.apple.CoreSimulator.SimDeviceType.iPhone-SE")
-                .arg("com.apple.CoreSimulator.SimRuntime.iOS-10-2")
-                .check_status();
+            .arg("simctl")
+            .arg("create")
+            .arg("rust_ios")
+            .arg("com.apple.CoreSimulator.SimDeviceType.iPhone-SE")
+            .arg("com.apple.CoreSimulator.SimRuntime.iOS-10-2")
+            .check_status();
     } else if simulator_booted == true {
         println!("Shutting down already-booted simulator");
         Command::new("xcrun")
-                .arg("simctl")
-                .arg("shutdown")
-                .arg("rust_ios")
-                .check_status();
+            .arg("simctl")
+            .arg("shutdown")
+            .arg("rust_ios")
+            .check_status();
     }
 
     println!("Starting iOS simulator");
     // We can't uninstall the app (if present) as that will hang if the
     // simulator isn't completely booted; just erase the simulator instead.
-    Command::new("xcrun").arg("simctl").arg("erase").arg("rust_ios").check_status();
-    Command::new("xcrun").arg("simctl").arg("boot").arg("rust_ios").check_status();
+    Command::new("xcrun")
+        .arg("simctl")
+        .arg("erase")
+        .arg("rust_ios")
+        .check_status();
+    Command::new("xcrun")
+        .arg("simctl")
+        .arg("boot")
+        .arg("rust_ios")
+        .check_status();
 }
 
 // Step three: Install the app
 fn install_app_to_simulator() {
     println!("Installing app to simulator");
     Command::new("xcrun")
-            .arg("simctl")
-            .arg("install")
-            .arg("booted")
-            .arg("ios_simulator_app/")
-            .check_status();
+        .arg("simctl")
+        .arg("install")
+        .arg("booted")
+        .arg("ios_simulator_app/")
+        .check_status();
 }
 
 // Step four: Run the app
 fn run_app_on_simulator() {
     println!("Running app");
     let output = t!(Command::new("xcrun")
-                    .arg("simctl")
-                    .arg("launch")
-                    .arg("--console")
-                    .arg("booted")
-                    .arg("com.rust.unittests")
-                    .output());
+        .arg("simctl")
+        .arg("launch")
+        .arg("--console")
+        .arg("booted")
+        .arg("com.rust.unittests")
+        .output());
 
     println!("status: {}", output.status);
     println!("stdout --\n{}\n", String::from_utf8_lossy(&output.stdout));
     println!("stderr --\n{}\n", String::from_utf8_lossy(&output.stderr));
 
     let stdout = String::from_utf8_lossy(&output.stdout);
-    let passed = stdout.lines()
-                       .find(|l|
-                             (l.contains("PASSED") &&
-                              l.contains("tests")) ||
-                             l.contains("test result: ok")
-                        )
-                       .unwrap_or(false);
+    let passed = stdout
+        .lines()
+        .find(|l| (l.contains("PASSED") && l.contains("tests")) || l.contains("test result: ok"))
+        .unwrap_or(false);
 
     println!("Shutting down simulator");
     Command::new("xcrun")
