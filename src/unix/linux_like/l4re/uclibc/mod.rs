e0fc9a2af61052038272182f83d9a571faf09dfb
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2025-08-11 10:59:55 +0200

    linux_like: unify SIGEV_THREAD_ID support
    This effectively adds the constant for musl too, as the last platform to
    support this symbol.
    
    It has been supported in musl since
    
      7c71792e ("add support for SIGEV_THREAD_ID timers")
    
    and was released with v1.2.2 in January 2021.
    
    The libc-test build script needed some tweaking to skip the symbol when
    building against an older musl.
    
    Also double-checked that the `sigevent` struct is defined in the exact
    same way.
    
    Signed-off-by: Christoph Heiss <christoph@c8h4.io>
    
    Co-authored-by: Christoph Heiss <christoph@c8h4.io>
    
    (backport <https://github.com/rust-lang/libc/pull/5375>)
    (cherry picked from commit 74e58cc7294fa594b205a33f8235a2b264455b13)

--- src/unix/linux_like/l4re/uclibc/mod.rs
+++ src/unix/linux_like/l4re/uclibc/mod.rs
@@ -386,8 +386,6 @@ pub const MCL_CURRENT: c_int = 0x0001;
 pub const MCL_FUTURE: c_int = 0x0002;
 pub const MCL_ONFAULT: c_int = 0x0004;
 
-pub const SIGEV_THREAD_ID: c_int = 4;
-
 pub const AF_VSOCK: c_int = 40;
 
 pub const POSIX_FADV_DONTNEED: c_int = 4;
