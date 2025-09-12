025b71af86dcc3626654b3acffc47878017ef8c7
David Carlier <devnexen@gmail.com>
2025-09-12 19:11:39 +0100

    Fix gid_t/uid_t types for redox.
    ref #4678

--- src/unix/redox/mod.rs
+++ src/unix/redox/mod.rs
@@ -29,8 +29,8 @@ pub type tcflag_t = u32;
 pub type time_t = c_longlong;
 pub type id_t = c_uint;
 pub type pid_t = usize;
-pub type uid_t = u32;
-pub type gid_t = u32;
+pub type uid_t = c_int;
+pub type gid_t = c_int;
 
 #[cfg_attr(feature = "extra_traits", derive(Debug))]
 pub enum timezone {}
