9044bfcff242703e3252543831ddc9a1a6ba1dfb
Xing Xue <xingxue@outlook.com>
2025-09-15 16:58:44 -0400

    Add 'ucontext_t' and related structures and functions.
    (backport <https://github.com/rust-lang/libc/pull/4696>)
    (cherry picked from commit 5ab43ebf0fc1c2b13d594e51d084b08e7760cd7a)

--- src/unix/linux_like/linux/gnu/b64/powerpc64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/powerpc64/mod.rs
@@ -9,6 +9,8 @@ pub type blksize_t = i64;
 pub type suseconds_t = i64;
 pub type __u64 = c_ulong;
 pub type __s64 = c_long;
+pub type gregset_t = [c_ulong; __NGREG];
+pub type fpregset_t = [c_ulong; __NFPREG];
 
 s! {
     // FIXME(1.0): This should not implement `PartialEq`
@@ -191,12 +193,85 @@ s! {
         pub ss_flags: c_int,
         pub ss_size: size_t,
     }
+
+    #[repr(align(8))]
+    pub struct clone_args {
+        pub flags: c_ulonglong,
+        pub pidfd: c_ulonglong,
+        pub child_tid: c_ulonglong,
+        pub parent_tid: c_ulonglong,
+        pub exit_signal: c_ulonglong,
+        pub stack: c_ulonglong,
+        pub stack_size: c_ulonglong,
+        pub tls: c_ulonglong,
+        pub set_tid: c_ulonglong,
+        pub set_tid_size: c_ulonglong,
+        pub cgroup: c_ulonglong,
+    }
 }
 
 s_no_extra_traits! {
     #[repr(align(16))]
     pub struct max_align_t {
         priv_: [i64; 4],
+    }
+
+    pub struct ucontext_t {
+        pub uc_flags: c_ulong,
+        pub uc_link: *mut ucontext_t,
+        pub uc_stack: crate::stack_t,
+        pub uc_sigmask: crate::sigset_t,
+        pub uc_mcontext: mcontext_t,
+    }
+
+    pub struct pt_regs {
+        pub gpr: [c_ulong; 32],
+        pub nip: c_ulong,
+        pub msr: c_ulong,
+        pub orig_gpr3: c_ulong,
+        pub ctr: c_ulong,
+        pub link: c_ulong,
+        pub xer: c_ulong,
+        pub ccr: c_ulong,
+        pub softe: c_ulong,
+        pub trap: c_ulong,
+        pub dar: c_ulong,
+        pub dsisr: c_ulong,
+        pub result: c_ulong,
+    }
+
+    pub struct mcontext_t {
+        __glibc_reserved: [c_ulong; 4],
+        pub signal: c_int,
+        __pad0: c_int,
+        pub handler: c_ulong,
+        pub oldmask: c_ulong,
+        pub regs: *mut pt_regs,
+        pub gp_regs: crate::gregset_t,
+        pub fp_regs: crate::fpregset_t,
+        pub v_regs: *mut vrregset_t,
+        pub vmx_reserve: [c_long; __NVRREG + __NVRREG + 1],
+    }
+
+    #[repr(align(16))]
+    pub struct vrregset_t {
+        pub vrregs: [[c_uint; 4]; 32],
+        pub vscr: vscr_t,
+        pub vrsave: c_uint,
+        __pad: [c_uint; 3],
+    }
+
+    #[repr(align(4))]
+    pub struct vscr_t {
+        #[cfg(target_endian = "big")]
+        __pad: [c_uint; 3],
+        #[cfg(target_endian = "big")]
+        pub vscr_word: c_uint,
+
+        #[cfg(target_endian = "little")]
+        pub vscr_word: c_uint,
+        #[cfg(target_endian = "little")]
+        __pad: [c_uint; 3],
     }
 }
 
@@ -209,6 +284,10 @@ pub const RTLD_NOLOAD: c_int = 0x4;
 pub const VEOF: usize = 4;
 pub const __SIZEOF_PTHREAD_RWLOCK_T: usize = 56;
 pub const __SIZEOF_PTHREAD_BARRIER_T: usize = 32;
+
+pub const __NGREG: usize = 48;
+pub const __NFPREG: usize = 33;
+pub const __NVRREG: usize = 34;
 
 pub const O_APPEND: c_int = 1024;
 pub const O_CREAT: c_int = 64;
@@ -971,4 +1050,9 @@ extern "C" {
         newp: *mut c_void,
         newlen: size_t,
     ) -> c_int;
+
+    pub fn getcontext(ucp: *mut ucontext_t) -> c_int;
+    pub fn setcontext(ucp: *const ucontext_t) -> c_int;
+    pub fn swapcontext(oucp: *mut ucontext_t, ucp: *const ucontext_t) -> c_int;
+    pub fn makecontext(ucp: *mut ucontext_t, func: extern "C" fn(), argc: c_int, ...);
 }
