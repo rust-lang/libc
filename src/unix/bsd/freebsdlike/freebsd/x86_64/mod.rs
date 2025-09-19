rust-lang/libc

commit 3d93bf5894136d6baf66ccecf6ecf5275908f082

freebsd: Limit mcontext_t::mc_tlsbase to FreeBSD 15
mcontext_t::mc_tlsbase was introduced in FreeBSD 15, in commit
eea3e4dd9703 ("amd64: add mc_tlsbase member to mcontext").

https://github.com/freebsd/freebsd-src/commit/eea3e4dd9703a252509d75814049aa8da5007ebb

src/unix/bsd/freebsdlike/freebsd/x86_64/mod.rs
@@ -98,6 +98,7 @@ s_no_extra_traits! {
     }
 
     #[repr(align(16))]
+    #[non_exhaustive]
     pub struct mcontext_t {
         pub mc_onstack: register_t,
         pub mc_rdi: register_t,
@@ -136,7 +137,11 @@ s_no_extra_traits! {
         pub mc_gsbase: register_t,
         pub mc_xfpustate: register_t,
         pub mc_xfpustate_len: register_t,
+        #[cfg(any(freebsd12, freebsd13, freebsd14))]
+        pub mc_spare: [c_long; 4],
+        #[cfg(freebsd15)]
         pub mc_tlsbase: register_t,
+        #[cfg(freebsd15)]
         pub mc_spare: [c_long; 3],
     }
 }

commit abbde1ddf01950dea4df8f68e9c49a0e7288de0b

FreeBSD: update gating of `mcontext_t.mc_tlsbase`
freebsd11 wasn't covered, meaning test failures on the 0.2 branch. Add
freebsd11 to the pattern, and use `cfg(not(...))` rather than
`cfg(freebsd15)`. (Ideally this would instead be encoded as something
like `cfg(freebsd_least_15)`.)

Fixes: 3d93bf589413 ("freebsd: Limit mcontext_t::mc_tlsbase to FreeBSD 15")

src/unix/bsd/freebsdlike/freebsd/x86_64/mod.rs
@@ -98,7 +98,7 @@ s_no_extra_traits! {
     }
 
     #[repr(align(16))]
-    #[non_exhaustive]
+    #[cfg_attr(not(any(freebsd11, freebsd12, freebsd13, freebsd14)), non_exhaustive)]
     pub struct mcontext_t {
         pub mc_onstack: register_t,
         pub mc_rdi: register_t,
@@ -137,11 +137,13 @@ s_no_extra_traits! {
         pub mc_gsbase: register_t,
         pub mc_xfpustate: register_t,
         pub mc_xfpustate_len: register_t,
-        #[cfg(any(freebsd12, freebsd13, freebsd14))]
+        // freebsd < 15
+        #[cfg(any(freebsd11, freebsd12, freebsd13, freebsd14))]
         pub mc_spare: [c_long; 4],
-        #[cfg(freebsd15)]
+        // freebsd >= 15
+        #[cfg(not(any(freebsd11, freebsd12, freebsd13, freebsd14)))]
         pub mc_tlsbase: register_t,
-        #[cfg(freebsd15)]
+        #[cfg(not(any(freebsd11, freebsd12, freebsd13, freebsd14)))]
         pub mc_spare: [c_long; 3],
     }
 }