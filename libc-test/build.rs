3eec53940ff9382bfeef7f3f36528c82742a4799
Trevor Gross <tmgross@umich.edu>
2025-09-20 19:24:11 -0500

    test: Set required config in `ctest_next_cfg`
    This is done in `ctest_cfg` on `libc-0.2` to account for thread local
    use on Dragonfly. I should have done this in the cherry pick that added
    the function (the "Fixes" commit below) but missed it. Thus, add it now.
    
    Fixes: 3a650dfdcb08 ("libc: port freebsd to use ctest-next")

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -87,6 +87,10 @@ fn ctest_cfg() -> ctest::TestGenerator {
 fn ctest_next_cfg() -> ctest_next::TestGenerator {
     let mut cfg = ctest_next::TestGenerator::new();
     cfg.skip_private(true);
+    let libc_cfgs = ["libc_thread_local"];
+    for f in &libc_cfgs {
+        cfg.cfg(f, None);
+    }
     cfg
 }
 
