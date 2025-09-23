d22f27a6d6c8096d164fd150013315cf6742b91e
Trevor Gross <tmgross@umich.edu>
2025-09-22 21:42:25 -0500

    Implement `Debug` for `Padding`
    Use the `MaybeUninit` formatting so this prints as `Padding<u32>` or
    similar.

--- src/types.rs
+++ src/types.rs
@@ -17,6 +17,16 @@ impl<T: Copy> Default for Padding<T> {
     }
 }
 
+impl<T: Copy> fmt::Debug for Padding<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // Taken frmo `MaybeUninit`'s debug implementation
+        // NB: there is no `.pad_fmt` so we can't use a simpler `format_args!("Padding<{..}>").
+        let full_name = core::any::type_name::<Self>();
+        let prefix_len = full_name.find("Padding").unwrap();
+        f.pad(&full_name[prefix_len..])
+    }
+}
+
 /// The default repr type used for C style enums in Rust.
 #[cfg(target_env = "msvc")]
 #[allow(unused)]
