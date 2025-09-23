rust-lang/libc

commit a6e75638ce4ea3faa003082fc91226a77336de07

Always implement `Debug`
Currently `Debug` implementations are gated behind the `extra-traits`
feature. My understanding is that historically, this was for two
reasons:

1. `Debug` implementations for unions were unsound
2. Concerns about compile time

The first was resolved a while ago by adding a "manual derive" opaque
implementation, in 6faa521f32fc ("fix: make Debug impl for unions
opaque"). Regarding the second reason, compile times for the current
`main` on my machine are (cleaning in between, with the 2025-08-06
nightly):

    $ cargo build -p libc
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.79s
    $ cargo build -p libc --release
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `release` profile [optimized] target(s) in 0.64s
    $ cargo build -p libc --features extra_traits
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `dev` profile [unoptimized + debuginfo] target(s) in 1.08s
    $ cargo build -p libc --features extra_traits --release
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `release` profile [optimized] target(s) in 0.85s

And with this patch applied:

    $ cargo build -p libc
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.89s
    $ cargo build -p libc --release
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `release` profile [optimized] target(s) in 0.70s
    $ cargo build -p libc --features extra_traits
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `dev` profile [unoptimized + debuginfo] target(s) in 1.15s
    $ cargo build -p libc --features extra_traits --release
     Compiling libc v1.0.0-alpha.1 (~/rust-libc)
      Finished `release` profile [optimized] target(s) in 0.86s

That is a microbenchmark but it shows that there probably isn't anything
to worry about.

src/lib.rs
@@ -22,8 +22,6 @@
 // Attributes needed when building as part of the standard library
 #![cfg_attr(feature = "rustc-dep-of-std", feature(link_cfg, no_core))]
 #![cfg_attr(feature = "rustc-dep-of-std", allow(internal_features))]
-// Enable extra lints:
-#![cfg_attr(feature = "extra_traits", warn(missing_debug_implementations))]
 #![warn(missing_copy_implementations, safe_packed_borrows)]
 #![cfg_attr(not(feature = "rustc-dep-of-std"), no_std)]
 #![cfg_attr(feature = "rustc-dep-of-std", no_core)]

src/fuchsia/mod.rs
@@ -80,7 +80,7 @@ pub type rlim_t = c_ulonglong;
 
 // FIXME(fuchsia): why are these uninhabited types? that seems... wrong?
 // Presumably these should be `()` or an `extern type` (when that stabilizes).
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum timezone {}
 impl Copy for timezone {}
 impl Clone for timezone {
@@ -88,7 +88,7 @@ impl Clone for timezone {
         *self
     }
 }
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum DIR {}
 impl Copy for DIR {}
 impl Clone for DIR {
@@ -97,7 +97,7 @@ impl Clone for DIR {
     }
 }
 
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum fpos64_t {} // FIXME(fuchsia): fill this out with a struct
 impl Copy for fpos64_t {}
 impl Clone for fpos64_t {
@@ -3469,7 +3469,7 @@ fn __MHDR_END(mhdr: *const msghdr) -> *mut c_uchar {
 #[link(name = "fdio")]
 extern "C" {}
 
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum FILE {}
 impl Copy for FILE {}
 impl Clone for FILE {
@@ -3477,7 +3477,7 @@ impl Clone for FILE {
         *self
     }
 }
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum fpos_t {} // FIXME(fuchsia): fill this out with a struct
 impl Copy for fpos_t {}
 impl Clone for fpos_t {

src/macros.rs
@@ -72,17 +72,20 @@ macro_rules! prelude {
         mod prelude {
             // Exports from `core`
             #[allow(unused_imports)]
-            pub(crate) use ::core::clone::Clone;
+            pub(crate) use core::clone::Clone;
             #[allow(unused_imports)]
-            pub(crate) use ::core::default::Default;
+            pub(crate) use core::default::Default;
             #[allow(unused_imports)]
-            pub(crate) use ::core::marker::{Copy, Send, Sync};
+            pub(crate) use core::marker::{Copy, Send, Sync};
             #[allow(unused_imports)]
-            pub(crate) use ::core::option::Option;
+            pub(crate) use core::option::Option;
             #[allow(unused_imports)]
-            pub(crate) use ::core::prelude::v1::derive;
+            pub(crate) use core::prelude::v1::derive;
             #[allow(unused_imports)]
-            pub(crate) use ::core::{fmt, hash, iter, mem, ptr};
+            pub(crate) use core::{fmt, hash, iter, mem, ptr};
+
+            #[allow(unused_imports)]
+            pub(crate) use fmt::Debug;
             #[allow(unused_imports)]
             pub(crate) use mem::{align_of, align_of_val, size_of, size_of_val};
 
@@ -120,9 +123,13 @@ macro_rules! s {
             #[repr(C)]
             #[cfg_attr(
                 feature = "extra_traits",
-                ::core::prelude::v1::derive(Debug, Eq, Hash, PartialEq)
+                ::core::prelude::v1::derive(Eq, Hash, PartialEq)
             )]
-            #[::core::prelude::v1::derive(::core::clone::Clone, ::core::marker::Copy)]
+            #[::core::prelude::v1::derive(
+                ::core::clone::Clone,
+                ::core::marker::Copy,
+                ::core::fmt::Debug,
+            )]
             #[allow(deprecated)]
             $(#[$attr])*
             pub struct $i { $($field)* }
@@ -142,17 +149,21 @@ macro_rules! s_paren {
         __item! {
             #[cfg_attr(
                 feature = "extra_traits",
-                ::core::prelude::v1::derive(Debug, Eq, Hash, PartialEq)
+                ::core::prelude::v1::derive(Eq, Hash, PartialEq)
             )]
-            #[::core::prelude::v1::derive(::core::clone::Clone, ::core::marker::Copy)]
+            #[::core::prelude::v1::derive(
+                ::core::clone::Clone,
+                ::core::marker::Copy,
+                ::core::fmt::Debug,
+            )]
             $(#[$attr])*
             pub struct $i ( $($field)* );
         }
     )*);
 }
 
-/// Implement `Clone` and `Copy` for a struct with no `extra_traits` feature, as well as `Debug`
-/// with `extra_traits` since that can always be derived.
+/// Implement `Clone`, `Copy`, and `Debug` since those can be derived, but exclude `PartialEq`,
+/// `Eq`, and `Hash`.
 ///
 /// Most items will prefer to use [`s`].
 macro_rules! s_no_extra_traits {
@@ -171,7 +182,6 @@ macro_rules! s_no_extra_traits {
             pub union $i { $($field)* }
         }
 
-        #[cfg(feature = "extra_traits")]
         impl ::core::fmt::Debug for $i {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
                 f.debug_struct(::core::stringify!($i)).finish_non_exhaustive()
@@ -182,8 +192,11 @@ macro_rules! s_no_extra_traits {
     (it: $(#[$attr:meta])* pub struct $i:ident { $($field:tt)* }) => (
         __item! {
             #[repr(C)]
-            #[::core::prelude::v1::derive(::core::clone::Clone, ::core::marker::Copy)]
-            #[cfg_attr(feature = "extra_traits", ::core::prelude::v1::derive(Debug))]
+            #[::core::prelude::v1::derive(
+                ::core::clone::Clone,
+                ::core::marker::Copy,
+                ::core::fmt::Debug,
+            )]
             $(#[$attr])*
             pub struct $i { $($field)* }
         }

src/solid/mod.rs
@@ -395,7 +395,7 @@ pub const SIGUSR1: c_int = 30;
 pub const SIGUSR2: c_int = 31;
 pub const SIGPWR: c_int = 32;
 
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum FILE {}
 impl Copy for FILE {}
 impl Clone for FILE {
@@ -403,7 +403,7 @@ impl Clone for FILE {
         *self
     }
 }
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum fpos_t {}
 impl Copy for fpos_t {}
 impl Clone for fpos_t {

src/unix/aix/powerpc64.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 
 // Define lock_data_instrumented as an empty enum
 missing! {
-    #[cfg_attr(feature = "extra_traits", derive(Debug))]
+    #[derive(Debug)]
     pub enum lock_data_instrumented {}
 }
 

src/unix/bsd/apple/mod.rs
@@ -175,7 +175,7 @@ pub type copyfile_callback_t = Option<
 pub type attrgroup_t = u32;
 pub type vol_capabilities_set_t = [u32; 4];
 
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum timezone {}
 impl Copy for timezone {}
 impl Clone for timezone {

src/unix/bsd/freebsdlike/dragonfly/mod.rs
@@ -45,7 +45,7 @@ pub type vm_map_entry_t = *mut vm_map_entry;
 
 pub type pmap = __c_anonymous_pmap;
 
-#[cfg_attr(feature = "extra_traits", derive(Debug))]
+#[derive(Debug)]
 pub enum sem {}
 impl Copy for sem {}
 impl Clone for sem {

src/unix/bsd/freebsdlike/freebsd/freebsd11/b32.rs
@@ -2,7 +2,8 @@ use crate::off_t;
 use crate::prelude::*;
 
 #[repr(C)]
-#[cfg_attr(feature = "extra_traits", derive(Debug, Eq, Hash, PartialEq))]
+#[derive(Debug)]
+#[cfg_attr(feature = "extra_traits", derive(Eq, Hash, PartialEq))]
 pub struct stat {
     pub st_dev: crate::dev_t,
     pub st_ino: crate::ino_t,

commit fc9d8fd843aa8aa4847f34c1c37c7328b7f13507

Warn on missing debug implementations
This was removed by accident; only the `feature = "rustc-dep-of-std"`
gate should have been removed.

Fixes: a6e75638ce4e ("Always implement `Debug`")

src/lib.rs
@@ -9,6 +9,11 @@
     unused_macros,
     unused_macro_rules,
 )]
+#![warn(
+    missing_copy_implementations,
+    missing_debug_implementations,
+    safe_packed_borrows
+)]
 // Prepare for a future upgrade
 #![warn(rust_2024_compatibility)]
 // Things missing for 2024 that are blocked on MSRV or breakage
@@ -22,7 +27,6 @@
 // Attributes needed when building as part of the standard library
 #![cfg_attr(feature = "rustc-dep-of-std", feature(link_cfg, no_core))]
 #![cfg_attr(feature = "rustc-dep-of-std", allow(internal_features))]
-#![warn(missing_copy_implementations, safe_packed_borrows)]
 #![cfg_attr(not(feature = "rustc-dep-of-std"), no_std)]
 #![cfg_attr(feature = "rustc-dep-of-std", no_core)]
 