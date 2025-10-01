e5b77756f0a66d784144ebf8ff6f41c619fd19cc
Brad Smith <brad@comstyle.com>
2025-09-30 22:24:40 -0400

    add HWCAP consts

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -2130,6 +2130,9 @@ fn test_android(target: &str) {
             // FIXME(android): Requires >= 6.12 kernel headers.
             "SOF_TIMESTAMPING_OPT_RX_FILTER" => true,
 
+            // FIXME(android): Requires >= 6.9 kernel headers.
+            "AT_HWCAP3" | "AT_HWCAP4" => true,
+
             _ => false,
         }
     });
@@ -2702,6 +2705,9 @@ fn test_freebsd(target: &str) {
 
             // Added in FreeBSD 14.2
             "SO_SPLICE" if Some(14) > freebsd_ver => true,
+
+            // Added in FreeBSD 15
+            "AT_HWCAP3" | "AT_HWCAP4" if Some(15) > freebsd_ver => true,
 
             _ => false,
         }
@@ -4585,6 +4591,9 @@ fn test_linux(target: &str) {
 
             // FIXME(linux): Value changed in 6.14
             "SECURE_ALL_BITS" | "SECURE_ALL_LOCKS" => true,
+
+            // FIXME(linux): Requires >= 6.9 kernel headers.
+            "AT_HWCAP3" | "AT_HWCAP4" => true,
 
             _ => false,
         }
