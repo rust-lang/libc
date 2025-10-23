a3b1bc644f2c641a02493c5c992b7cda09799218
Trevor Gross <tmgross@umich.edu>
2025-10-23 05:58:28 -0400

    apple: Port mcontext and ucontext to the new structure
    This includes `signal.h` stubs since that is a common header to import
    these types.

--- src/new/mod.rs
+++ src/new/mod.rs
@@ -3,6 +3,16 @@
 //!
 //! Eventually everything should be moved over, and we will move this directory to the top
 //! level in `src`.
+//!
+//! # Basic structure
+//!
+//! Each child module here represents a library or group of libraries that we are binding. Each of
+//! these has several submodules, representing either a directory or a header file in that library.
+//!
+//! `#include`s turn into `pub use ...*;` statements. Then at the root level (here), we choose
+//! which top-level headers we want to reexport the definitions for.
+//!
+//! All modules are only crate-public since we don't reexport this structure.
 
 cfg_if! {
     if #[cfg(target_os = "linux")] {
@@ -11,5 +21,9 @@ cfg_if! {
     } else if #[cfg(target_os = "android")] {
         mod bionic;
         pub use bionic::*;
+    } else if #[cfg(target_vendor = "apple")] {
+        mod apple;
+        pub(crate) use apple::*;
+        pub use signal::*;
     }
 }
