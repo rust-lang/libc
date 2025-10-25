91fc98caa6ac1120db4bd82469398995a7775c33
Trevor Gross <tmgross@umich.edu>
2025-10-25 17:54:47 -0400

    doc: Fix rustdoc `bare_urls` warnings

--- src/new/apple_xnu/mach/machine/_structs.rs
+++ src/new/apple_xnu/mach/machine/_structs.rs
@@ -1,6 +1,6 @@
 //! Header: `mach/machine/_structs.h`
 //!
-//! https://github.com/apple-oss-distributions/xnu/blob/main/osfmk/mach/machine/_structs.h
+//! <https://github.com/apple-oss-distributions/xnu/blob/main/osfmk/mach/machine/_structs.h>
 
 cfg_if! {
     if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
