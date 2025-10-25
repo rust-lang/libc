73d83da313fb4357701cf93521c99dc745473a3f
Trevor Gross <tmgross@umich.edu>
2025-10-25 15:23:40 -0400

    espidf: Fix the duplicate definition of gethostname
    (backport <https://github.com/rust-lang/libc/pull/4773>)
    (cherry picked from commit f6388933578ef33b7b830eacae8d293b816b14c0)

--- src/unix/mod.rs
+++ src/unix/mod.rs
@@ -1466,6 +1466,7 @@ extern "C" {
         link_name = "mknod@FBSD_1.0"
     )]
     pub fn mknod(pathname: *const c_char, mode: mode_t, dev: crate::dev_t) -> c_int;
+    #[cfg(not(target_os = "espidf"))]
     pub fn gethostname(name: *mut c_char, len: size_t) -> c_int;
     pub fn endservent();
     pub fn getservbyname(name: *const c_char, proto: *const c_char) -> *mut servent;
