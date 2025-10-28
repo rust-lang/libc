f8d1d9d9e28fd70809ec95bea23bba10783d1abc
Trevor Gross <tmgross@umich.edu>
2025-10-27 22:40:25 -0400

    NetBSD: Remove `vm_size_t`
    As noted, this has been deprecated a long time and is no longer present
    on x86_64. Remove the typedef.
    
    (backport <https://github.com/rust-lang/libc/pull/4782>)
    (cherry picked from commit 11d36ae0efd967e52c9346778ae6ab70b505beae)

--- src/unix/bsd/netbsdlike/netbsd/mod.rs
+++ src/unix/bsd/netbsdlike/netbsd/mod.rs
@@ -13,7 +13,6 @@ pub type fsfilcnt_t = u64;
 pub type idtype_t = c_int;
 pub type mqd_t = c_int;
 type __pthread_spin_t = __cpu_simple_lock_nv_t;
-pub type vm_size_t = crate::uintptr_t; // FIXME(deprecated): deprecated since long time
 pub type lwpid_t = c_uint;
 pub type shmatt_t = c_uint;
 pub type cpuid_t = c_ulong;
