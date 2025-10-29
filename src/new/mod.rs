00a0290f1472b74b83ec08aa2dd904db4dad6241
Trevor Gross <tmgross@umich.edu>
2025-10-28 21:44:59 -0400

    NetBSD: Introduce `timex.rs`
    Move `timex.h` types to the `new/` module.
    
    (backport <https://github.com/rust-lang/libc/pull/4782>)
    (cherry picked from commit fb74b6f9cafdc60afebcbf7016e34daf839d99c4)

--- src/new/mod.rs
+++ src/new/mod.rs
@@ -185,6 +185,7 @@ cfg_if! {
     } else if #[cfg(target_os = "netbsd")] {
         pub use sys::ipc::*;
         pub use sys::statvfs::*;
+        pub use sys::timex::*;
         pub use sys::types::*;
         pub use utmp_::*;
         pub use utmpx_::*;
