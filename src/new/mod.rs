rust-lang/libc

commit 42289eb64f63fff85a08dffcfb30b1e6b167d20a

Implement utmp for NetBSD

src/unix/bsd/netbsdlike/netbsd/mod.rs
@@ -299,6 +299,19 @@ s! {
         pub shm_ctime: ::time_t,
         _shm_internal: *mut ::c_void,
     }
+
+    pub struct utmp {
+        pub ut_line: [::c_char; UT_LINESIZE],
+        pub ut_name: [::c_char; UT_NAMESIZE],
+        pub ut_host: [::c_char; UT_HOSTSIZE],
+        pub ut_time: ::time_t
+    }
+
+    pub struct lastlog {
+        pub ll_line: [::c_char; UT_LINESIZE],
+        pub ll_host: [::c_char; UT_HOSTSIZE],
+        pub ll_time: ::time_t
+    }
 }
 
 s_no_extra_traits! {
@@ -1517,6 +1530,9 @@ pub const CHWFLOW: ::tcflag_t = ::MDMBUF | ::CRTSCTS | ::CDTRCTS;
 // pub const _PATH_WTMPX: &[::c_char; 14] = b"/var/log/wtmpx";
 // pub const _PATH_LASTLOGX: &[::c_char; 17] = b"/var/log/lastlogx";
 // pub const _PATH_UTMP_UPDATE: &[::c_char; 24] = b"/usr/libexec/utmp_update";
+pub const UT_NAMESIZE: usize = 8;
+pub const UT_LINESIZE: usize = 8;
+pub const UT_HOSTSIZE: usize = 16;
 pub const _UTX_USERSIZE: usize = 32;
 pub const _UTX_LINESIZE: usize = 32;
 pub const _UTX_PADSIZE: usize = 40;
@@ -1914,9 +1930,14 @@ extern "C" {
     pub fn pututxline(ut: *const utmpx) -> *mut utmpx;
     pub fn setutxent();
     pub fn endutxent();
-// TODO: uncomment after utmp implementation
-// pub fn getutmp(ux: *const utmpx, u: *mut utmp);
-// pub fn getutmpx(u: *const utmp, ux: *mut utmpx);
+
+    pub fn getutmp(ux: *const utmpx, u: *mut utmp);
+    pub fn getutmpx(u: *const utmp, ux: *mut utmpx);
+
+    pub fn utpname(file: *const ::c_char) -> ::c_int;
+    pub fn setutent();
+    pub fn endutent();
+    pub fn getutent() -> *mut utmp;
 }
 
 cfg_if! {

commit 03cba082a4633ac880af06c0a692b15c360f0c8e

NetBSD: Introduce `utmp_.rs`, correct the definition of `lastlog`
The field order was incorrect. Update this and move it to `new`, along
with the rest of `utmp`.

As part of this, correct an incorrectly spelled `utpname` to `utmpname`.

Fixes: 42289eb64f63 "Implement utmp for NetBSD"

src/new/mod.rs
@@ -182,6 +182,7 @@ cfg_if! {
     } else if #[cfg(target_os = "netbsd")] {
         pub use sys::ipc::*;
         pub use sys::statvfs::*;
+        pub use utmp_::*;
     } else if #[cfg(target_os = "openbsd")] {
         pub use sys::ipc::*;
     }

src/new/netbsd/mod.rs
@@ -6,3 +6,4 @@
 
 pub(crate) mod sys;
 pub(crate) mod unistd;
+pub(crate) mod utmp_;

src/new/netbsd/utmp_.rs
@@ -1,0 +1,32 @@
+//! Header: `utmp.h`
+//!
+//! <https://github.com/NetBSD/src/blob/master/include/utmp.h>
+
+use crate::prelude::*;
+
+pub const UT_NAMESIZE: usize = 8;
+pub const UT_LINESIZE: usize = 8;
+pub const UT_HOSTSIZE: usize = 16;
+
+s! {
+    pub struct lastlog {
+        pub ll_time: crate::time_t,
+        pub ll_line: [c_char; UT_LINESIZE],
+        pub ll_host: [c_char; UT_HOSTSIZE],
+    }
+
+    pub struct utmp {
+        pub ut_line: [c_char; UT_LINESIZE],
+        pub ut_name: [c_char; UT_NAMESIZE],
+        pub ut_host: [c_char; UT_HOSTSIZE],
+        pub ut_time: crate::time_t,
+    }
+}
+
+#[link(name = "util")]
+extern "C" {
+    pub fn utmpname(file: *const c_char) -> c_int;
+    pub fn setutent();
+    pub fn getutent() -> *mut utmp;
+    pub fn endutent();
+}

src/unix/bsd/netbsdlike/netbsd/mod.rs
@@ -435,19 +435,6 @@ s! {
         _shm_internal: *mut c_void,
     }
 
-    pub struct utmp {
-        pub ut_line: [c_char; UT_LINESIZE],
-        pub ut_name: [c_char; UT_NAMESIZE],
-        pub ut_host: [c_char; UT_HOSTSIZE],
-        pub ut_time: crate::time_t,
-    }
-
-    pub struct lastlog {
-        pub ll_line: [c_char; UT_LINESIZE],
-        pub ll_host: [c_char; UT_HOSTSIZE],
-        pub ll_time: crate::time_t,
-    }
-
     pub struct timex {
         pub modes: c_uint,
         pub offset: c_long,
@@ -2044,9 +2031,6 @@ pub const CHWFLOW: crate::tcflag_t = crate::MDMBUF | crate::CRTSCTS | crate::CDT
 // pub const _PATH_WTMPX: &[c_char; 14] = b"/var/log/wtmpx";
 // pub const _PATH_LASTLOGX: &[c_char; 17] = b"/var/log/lastlogx";
 // pub const _PATH_UTMP_UPDATE: &[c_char; 24] = b"/usr/libexec/utmp_update";
-pub const UT_NAMESIZE: usize = 8;
-pub const UT_LINESIZE: usize = 8;
-pub const UT_HOSTSIZE: usize = 16;
 pub const _UTX_USERSIZE: usize = 32;
 pub const _UTX_LINESIZE: usize = 32;
 pub const _UTX_PADSIZE: usize = 40;
@@ -2727,13 +2711,8 @@ extern "C" {
     pub fn setutxent();
     pub fn endutxent();
 
-    pub fn getutmp(ux: *const utmpx, u: *mut utmp);
-    pub fn getutmpx(u: *const utmp, ux: *mut utmpx);
-
-    pub fn utpname(file: *const c_char) -> c_int;
-    pub fn setutent();
-    pub fn endutent();
-    pub fn getutent() -> *mut utmp;
+    pub fn getutmp(ux: *const utmpx, u: *mut crate::utmp);
+    pub fn getutmpx(u: *const crate::utmp, ux: *mut utmpx);
 
     pub fn efopen(p: *const c_char, m: *const c_char) -> crate::FILE;
     pub fn emalloc(n: size_t) -> *mut c_void;
@@ -2796,7 +2775,7 @@ extern "C" {
         precision: size_t,
     ) -> *mut c_char;
     #[link_name = "__login50"]
-    pub fn login(ut: *const utmp);
+    pub fn login(ut: *const crate::utmp);
     #[link_name = "__loginx50"]
     pub fn loginx(ut: *const utmpx);
     pub fn logout(line: *const c_char);