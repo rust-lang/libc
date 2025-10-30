a1c27ef30edcb8df6ad2543ded262c8160f2d534
Reagan Bohan <xbjfk.github@gmail.com>
2025-10-30 09:27:09 +0000

    musl: Merge stat64 and stat with `stat`
    The structures are the same - and this follows the upstream definition
    On musl at least, blkcnt64_t == blkcnt_t == i64 and
    ino_t == ino64_t == u64

--- src/unix/linux_like/linux/musl/b64/s390x.rs
+++ src/unix/linux_like/linux/musl/b64/s390x.rs
@@ -8,6 +8,7 @@ pub type greg_t = u64;
 pub type __u64 = u64;
 pub type __s64 = i64;
 pub type statfs64 = statfs;
+pub type stat64 = stat;
 
 s! {
     pub struct ipc_perm {
@@ -47,26 +48,6 @@ s! {
         pub st_ctime_nsec: c_long,
         pub st_blksize: crate::blksize_t,
         pub st_blocks: crate::blkcnt_t,
-        __unused: Padding<[c_long; 3]>,
-    }
-
-    pub struct stat64 {
-        pub st_dev: crate::dev_t,
-        pub st_ino: crate::ino64_t,
-        pub st_nlink: crate::nlink_t,
-        pub st_mode: crate::mode_t,
-        pub st_uid: crate::uid_t,
-        pub st_gid: crate::gid_t,
-        pub st_rdev: crate::dev_t,
-        pub st_size: off_t,
-        pub st_atime: crate::time_t,
-        pub st_atime_nsec: c_long,
-        pub st_mtime: crate::time_t,
-        pub st_mtime_nsec: c_long,
-        pub st_ctime: crate::time_t,
-        pub st_ctime_nsec: c_long,
-        pub st_blksize: crate::blksize_t,
-        pub st_blocks: crate::blkcnt64_t,
         __unused: Padding<[c_long; 3]>,
     }
 
