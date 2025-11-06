555224d7a1b04b3da1c1b85174d3bf83e8b201a1
Trevor Gross <tmgross@umich.edu>
2025-11-05 18:59:43 -0600

    can't seem to repro without the rust build, add it back

--- ci/run.sh
+++ ci/run.sh
@@ -12,18 +12,18 @@ export RUST_BACKTRACE="${RUST_BACKTRACE:-1}"
 # For logging
 uname -a
 
-# cmd="cargo test --target $target ${LIBC_CI_ZBUILD_STD+"-Zbuild-std"}"
-# test_flags="--skip check_style"
+cmd="cargo test --target $target ${LIBC_CI_ZBUILD_STD+"-Zbuild-std"}"
+test_flags="--skip check_style"
 
-# # Run tests in the `libc` crate
-# case "$target" in
-#     # Only run `libc-test`
-#     # FIXME(android): unit tests fail to start on Android
-#     *android*) cmd="$cmd --manifest-path libc-test/Cargo.toml" ;;
-#     *s390x*) cmd="$cmd --manifest-path libc-test/Cargo.toml" ;;
-#     # For all other platforms, test everything in the workspace
-#     *) cmd="$cmd --workspace" ;;
-# esac
+# Run tests in the `libc` crate
+case "$target" in
+    # Only run `libc-test`
+    # FIXME(android): unit tests fail to start on Android
+    *android*) cmd="$cmd --manifest-path libc-test/Cargo.toml" ;;
+    *s390x*) cmd="$cmd --manifest-path libc-test/Cargo.toml" ;;
+    # For all other platforms, test everything in the workspace
+    *) cmd="$cmd --workspace" ;;
+esac
 
 # case "$target" in
 #     # crash in std::env::tmp_dir (no filesystem on wasm).
@@ -33,13 +33,13 @@ uname -a
 #     powerpc64le*musl) cmd="$cmd --exclude ctest --exclude ctest-test --exclude ctest-next" ;;
 # esac
 
-# env="$(rustc --print cfg --target "$target" | sed -n 's/target_env="\(.*\)"/\1/p')"
-# bits="$(rustc --print cfg --target "$target" | sed -n 's/target_pointer_width="\(.*\)"/\1/p')"
+env="$(rustc --print cfg --target "$target" | sed -n 's/target_env="\(.*\)"/\1/p')"
+bits="$(rustc --print cfg --target "$target" | sed -n 's/target_pointer_width="\(.*\)"/\1/p')"
 
-# # shellcheck disable=SC2086
-# $cmd --no-default-features -- $test_flags
+# shellcheck disable=SC2086
+$cmd --no-default-features -- $test_flags
 
-# Dummy build dir
-mkdir -p target/foo
-echo "hi" >> target/foo/ctest_output.c
-echo "hi" >> target/foo/hi.rs
+# # Dummy build dir
+# mkdir -p target/foo
+# echo "hi" >> target/foo/ctest_output.c
+# echo "hi" >> target/foo/hi.rs
