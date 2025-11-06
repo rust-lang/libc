a6d12264e8cd5e90b944f81b340e94e3f3a68d91
John Starks <jostarks@microsoft.com>
2025-11-06 05:52:29 +0000

    linux/aarch64: make mcontext_t.__reserved pub
    The `mcontext_t.__reserved` field is documented to contain extra
    context, such as FP state and the ESR register. This field is 16-byte
    aligned, so for simplicity expose it as an array of `u128` elements
    (similar to how `musl` defines it as an array of `long double`s in
    C).

--- src/unix/linux_like/linux/musl/b64/aarch64/mod.rs
+++ src/unix/linux_like/linux/musl/b64/aarch64/mod.rs
@@ -96,14 +96,13 @@ s! {
         pub uc_mcontext: mcontext_t,
     }
 
-    #[repr(align(16))]
     pub struct mcontext_t {
         pub fault_address: c_ulong,
         pub regs: [c_ulong; 31],
         pub sp: c_ulong,
         pub pc: c_ulong,
         pub pstate: c_ulong,
-        __reserved: [u64; 512],
+        pub __reserved: [u128; 256],
     }
 
     #[repr(align(8))]
