214a161e38c212d64ef3bab15e9a6a5f2fd6cfa7
Marius Melzer <marius.melzer@kernkonzept.com>
2025-11-19 15:38:15 +0100

    Add uclibc aarch64 support for L4Re
    (backport <https://github.com/rust-lang/libc/pull/4479>)
    (cherry picked from commit 980c9230631ce75f90812a0432609c3d2d0974db)

--- src/unix/linux_like/l4re/uclibc/mod.rs
+++ src/unix/linux_like/l4re/uclibc/mod.rs
@@ -529,6 +529,9 @@ cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
+    } else if #[cfg(target_arch = "aarch64")] {
+        mod aarch64;
+        pub use self::aarch64::*;
     } else {
         // unsupported target
     }
