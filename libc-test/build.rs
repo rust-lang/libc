9ba7c90662818c9bd6733899e4718816fe8b6394
500-internal-server-error <76838083+500-internal-server-error@users.noreply.github.com>
2025-11-24 21:11:41 +0700

    cygwin: Add missing utmp/x.h, grp.h, and stdio.h interfaces
    These constants and functions are required for building uutils-coreutils for Cygwin.
    
    Sources:
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/winsup/cygwin/include/cygwin/config.h#L50
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/newlib/libc/include/stdio.h#L138-L139
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/winsup/cygwin/include/cygwin/utmp.h#L20-L33
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/winsup/cygwin/include/utmpx.h#L21-L32
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/newlib/libc/include/stdio.h#L120-L122
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/newlib/libc/include/grp.h#L64-L65
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/winsup/cygwin/include/utmpx.h#L21-L32
    
    https://github.com/msys2/msys2-runtime/blob/msys2-3.6.5/winsup/cygwin/include/utmpx.h#L42-L49
    
    (backport <https://github.com/rust-lang/libc/pull/4827>)
    (cherry picked from commit a3bb40e18a207d53b7eb02fdd21cf97c360aaa37)

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -667,6 +667,7 @@ fn test_cygwin(target: &str) {
         "spawn.h",
         "stddef.h",
         "stdlib.h",
+        "stdio.h",
         "string.h",
         "sys/cpuset.h",
         "sys/ioctl.h",
@@ -689,6 +690,7 @@ fn test_cygwin(target: &str) {
         "termios.h",
         "unistd.h",
         "utime.h",
+        "utmpx.h",
         "wait.h",
         "wchar.h",
     );
