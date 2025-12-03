c244d167616a27d40cacdeaa0de139e2b787ad57
Rafael RL <rafael.ruiz-lucena@de.bosch.com>
2025-12-03 13:09:32 +0100

    Move structs into no_extra_traits macro

--- src/new/nto/net/if_.rs
+++ src/new/nto/net/if_.rs
@@ -1,17 +1,5 @@
 use crate::prelude::*;
 
-s! {
-    pub struct ifreq {
-        /// if name, e.g. "en0"
-        pub ifr_name: [c_char; crate::IFNAMSIZ],
-        pub ifr_ifru: __c_anonymous_ifr_ifru,
-    }
-    pub struct ifreq_buffer {
-        pub length: size_t,
-        pub buffer: *mut c_void,
-    }
-}
-
 s_no_extra_traits! {
     pub union __c_anonymous_ifr_ifru {
         pub ifru_addr: crate::sockaddr,
@@ -29,5 +17,16 @@ s_no_extra_traits! {
         pub ifru_cap: [c_int; 2],
         pub ifru_fib: c_uint,
         pub ifru_vlan_pcp: c_uchar,
+    }
+
+    pub struct ifreq {
+        /// if name, e.g. "en0"
+        pub ifr_name: [c_char; crate::IFNAMSIZ],
+        pub ifr_ifru: __c_anonymous_ifr_ifru,
+    }
+
+    pub struct ifreq_buffer {
+        pub length: size_t,
+        pub buffer: *mut c_void,
     }
 }
