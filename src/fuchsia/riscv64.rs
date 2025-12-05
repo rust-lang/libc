8341f11a836dc8ef5d7859ae20b78135f7d43be5
Reagan Bohan <xbjfk.github@gmail.com>
2025-12-05 12:12:00 +0000

    Fuchsia: change struct stat/stat64 times to timespec
    Source: https://cs.opensource.google/fuchsia/fuchsia/+/main:zircon/third_party/ulib/musl/include/bits/stat.h;bpv=1;bpt=1

--- src/fuchsia/riscv64.rs
+++ src/fuchsia/riscv64.rs
@@ -22,12 +22,9 @@ s! {
         pub st_size: off_t,
         pub st_blksize: crate::blksize_t,
         pub st_blocks: crate::blkcnt_t,
-        pub st_atime: crate::time_t,
-        pub st_atime_nsec: c_long,
-        pub st_mtime: crate::time_t,
-        pub st_mtime_nsec: c_long,
-        pub st_ctime: crate::time_t,
-        pub st_ctime_nsec: c_long,
+        pub st_atim: crate::timespec,
+        pub st_mtim: crate::timepsec,
+        pub st_ctim: crate::timespec,
         __unused: Padding<[c_long; 3]>,
     }
 
