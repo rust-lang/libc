2b98692fea39ad03d3ea8c8aa4f8cb9da0c27df7
Havard Eidnes <he@NetBSD.org>
2025-12-19 17:07:45 +0000

    netbsd/riscv64.rs: make changes so that this builds again.

--- src/unix/bsd/netbsdlike/netbsd/riscv64.rs
+++ src/unix/bsd/netbsdlike/netbsd/riscv64.rs
@@ -1,37 +1,52 @@
-use crate::prelude::*;
 use crate::PT_FIRSTMACH;
+
+use crate::prelude::*;
 
 pub type __greg_t = u64;
 pub type __cpu_simple_lock_nv_t = c_int;
 pub type __gregset = [__greg_t; _NGREG];
-pub type __fregset = [__fpreg; _NFREG];
+pub type __fpregset = [__fpreg; _NFREG];
 
 s! {
     pub struct mcontext_t {
         pub __gregs: __gregset,
-        pub __fregs: __fregset,
+        pub __fpregs: __fpregset,
         __spare: [crate::__greg_t; 7],
     }
 }
 
-s_no_extra_traits! {
-    pub union __fpreg {
-        pub u_u64: u64,
-        pub u_d: c_double,
-    }
+#[derive(Clone, Copy)]
+pub union __fpreg {
+    pub u_u64: u64,
+    pub u_d: c_double,
 }
 
 cfg_if! {
     if #[cfg(feature = "extra_traits")] {
         impl PartialEq for __fpreg {
-            fn eq(&self, other: &Self) -> bool {
-                unsafe { self.u_u64 == other.u_u64 }
+            fn eq(&self, other: &__fpreg) -> bool {
+                unsafe {
+                    self.u_u64 == other.u_u64
+                        || self.u_d == other.u_d
+                }
             }
         }
         impl Eq for __fpreg {}
         impl hash::Hash for __fpreg {
             fn hash<H: hash::Hasher>(&self, state: &mut H) {
-                unsafe { self.u_u64.hash(state) };
+                unsafe {
+                    self.u_u64.hash(state);
+                }
+            }
+        }
+        impl fmt::Debug for __fpreg {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                unsafe {
+                    f.debug_struct("__fpreg")
+                        .field("u_u64", &self.u_u64)
+                        .field("u_d", &self.u_d)
+                        .finish()
+                }
             }
         }
     }
