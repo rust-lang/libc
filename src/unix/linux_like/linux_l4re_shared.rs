cdc2077f76b91a89b47aea132209e2ad20fe94a7
gibbz00 <gabrielhansson00@gmail.com>
2025-12-26 12:30:32 +0100

    linux, l4re: address soundness issues of `CMSG_NXTHDR`
    This change makes sure that the header of `next` is within max,
    returning null if not. This is similar to how `glibc` does it.
    
    No checks were previously being done to assert that `next as usize +
    size_of::<cmsghdr>() < max`. Wrapping offset calculations could then
    lead to buffer over-reads in the following `(*next).cmsg_len`.
    
    [glibc ref](https://github.com/bminor/glibc/blob/b71d59074b98ad4abd23c136ec9ad4c26e29ee6d/sysdeps/unix/sysv/linux/cmsg_nxthdr.c#L49-L51)

--- src/unix/linux_like/linux_l4re_shared.rs
+++ src/unix/linux_like/linux_l4re_shared.rs
@@ -1493,15 +1493,25 @@ f! {
         if ((*cmsg).cmsg_len as usize) < size_of::<crate::cmsghdr>() {
             return core::ptr::null_mut::<crate::cmsghdr>();
         }
-        let next =
-            (cmsg as usize + super::CMSG_ALIGN((*cmsg).cmsg_len as usize)) as *mut crate::cmsghdr;
-        let max = (*mhdr).msg_control as usize + (*mhdr).msg_controllen as usize;
-        if (next.wrapping_offset(1)) as usize > max
-            || next as usize + super::CMSG_ALIGN((*next).cmsg_len as usize) > max
-        {
+
+        // FIXME(msrv): `.wrapping_byte_add()` stabilized in 1.75
+        let next_cmsg = cmsg
+            .cast::<u8>()
+            .wrapping_add(super::CMSG_ALIGN((*cmsg).cmsg_len as usize))
+            .cast::<crate::cmsghdr>();
+
+        // In case the addition wrapped. `next_addr > max_addr`
+        // would otherwise not work as intended.
+        if (next_cmsg as usize) < (cmsg as usize) {
+            return core::ptr::null_mut();
+        }
+
+        let max_addr = (*mhdr).msg_control as usize + (*mhdr).msg_controllen as usize;
+
+        if next_cmsg as usize + size_of::<crate::cmsghdr>() > max_addr {
             core::ptr::null_mut::<crate::cmsghdr>()
         } else {
-            next
+            next_cmsg as *mut crate::cmsghdr
         }
     }
 
