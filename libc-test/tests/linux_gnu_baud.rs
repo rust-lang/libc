52fcf32016d649fd97a28be48c837d5bf445a2c5
usamoi <usamoi@outlook.com>
2026-01-28 21:05:47 +0800

    checks termios size in the baud test
    (backport <https://github.com/rust-lang/libc/pull/4938>)
    (cherry picked from commit 44b709c241d5b007927cb3daecb7a19d7a4f53cc)

--- libc-test/tests/linux_gnu_baud.rs
+++ libc-test/tests/linux_gnu_baud.rs
@@ -13,21 +13,24 @@ fn baud() {
     assert!(ret >= 0);
     let terminal_fd = unsafe { open(buffer.as_ptr(), O_RDWR | O_NOCTTY) };
     assert!(terminal_fd >= 0);
-    let mut tio: termios = unsafe { std::mem::zeroed() };
-    let ret = unsafe { tcgetattr(terminal_fd, &mut tio) };
+    #[repr(C)]
+    struct Protector(termios, [u8; 512]);
+    let mut tio: Protector = unsafe { Protector(std::mem::zeroed(), [0xcc; _]) };
+    let ret = unsafe { tcgetattr(terminal_fd, &raw mut tio.0) };
     assert!(ret >= 0);
-    assert_eq!(unsafe { cfgetispeed(&tio) }, B38400);
-    assert_eq!(unsafe { cfgetospeed(&tio) }, B38400);
-    let ret = unsafe { cfsetspeed(&mut tio, B1000000) };
+    assert_eq!(tio.1, [0xcc; _]);
+    assert_eq!(unsafe { cfgetispeed(&tio.0) }, B38400);
+    assert_eq!(unsafe { cfgetospeed(&tio.0) }, B38400);
+    let ret = unsafe { cfsetspeed(&mut tio.0, B1000000) };
     assert!(ret >= 0);
-    assert_eq!(unsafe { cfgetispeed(&tio) }, B1000000);
-    assert_eq!(unsafe { cfgetospeed(&tio) }, B1000000);
-    let ret = unsafe { cfsetispeed(&mut tio, B9600) };
+    assert_eq!(unsafe { cfgetispeed(&tio.0) }, B1000000);
+    assert_eq!(unsafe { cfgetospeed(&tio.0) }, B1000000);
+    let ret = unsafe { cfsetispeed(&mut tio.0, B9600) };
     assert!(ret >= 0);
-    assert_eq!(unsafe { cfgetispeed(&tio) }, B9600);
-    assert!(matches!(unsafe { cfgetospeed(&tio) }, B9600 | B1000000));
-    let ret = unsafe { cfsetospeed(&mut tio, B19200) };
+    assert_eq!(unsafe { cfgetispeed(&tio.0) }, B9600);
+    assert!(matches!(unsafe { cfgetospeed(&tio.0) }, B9600 | B1000000));
+    let ret = unsafe { cfsetospeed(&mut tio.0, B19200) };
     assert!(ret >= 0);
-    assert!(matches!(unsafe { cfgetispeed(&tio) }, B9600 | B19200));
-    assert_eq!(unsafe { cfgetospeed(&tio) }, B19200);
+    assert!(matches!(unsafe { cfgetispeed(&tio.0) }, B9600 | B19200));
+    assert_eq!(unsafe { cfgetospeed(&tio.0) }, B19200);
 }
