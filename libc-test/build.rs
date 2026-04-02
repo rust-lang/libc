7a0c4350bf9565e6d1e1e9a5aea4a1c20f7244bc
Clayton Slaughter <clay.slaughter@ibm.com>
2026-04-02 14:08:43 -0500

    linux: add `sockaddr_iucv`
    This adds the sockaddr_iucv define in netiucv/iucv. It is primarily used
    on the s390x platform when linux is running under z/VM, but is present
    on all the linux systems I've been able to test on.
    
    [ cleaned up commit message - Trevor ]

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -3783,6 +3783,7 @@ fn test_linux(target: &str) {
         "netinet/ip.h",
         "netinet/tcp.h",
         "netinet/udp.h",
+        (gnu, "netiucv/iucv.h"),
         (l4re, "netpacket/packet.h"),
         "poll.h",
         "pthread.h",
