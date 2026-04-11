a59a9cd583a46b7b5d72d0b9bd0d744f68c70acb
Jeong, YunWon <jeong@youknowone.org>
2026-04-12 05:35:31 +0900

    [ctest] support r#keyword

--- ctest/src/ast/field.rs
+++ ctest/src/ast/field.rs
@@ -13,4 +13,69 @@ impl Field {
     pub fn ident(&self) -> &str {
         &self.ident
     }
+
+    /// Return the identifier escaped for Rust source output when needed.
+    pub fn rust_ident(&self) -> String {
+        if is_rust_keyword(&self.ident) {
+            format!("r#{}", self.ident)
+        } else {
+            self.ident.to_string()
+        }
+    }
+}
+
+fn is_rust_keyword(ident: &str) -> bool {
+    matches!(
+        ident,
+        "as" | "break"
+            | "const"
+            | "continue"
+            | "crate"
+            | "else"
+            | "enum"
+            | "extern"
+            | "false"
+            | "fn"
+            | "for"
+            | "if"
+            | "impl"
+            | "in"
+            | "let"
+            | "loop"
+            | "match"
+            | "mod"
+            | "move"
+            | "mut"
+            | "pub"
+            | "ref"
+            | "return"
+            | "self"
+            | "Self"
+            | "static"
+            | "struct"
+            | "super"
+            | "trait"
+            | "true"
+            | "type"
+            | "unsafe"
+            | "use"
+            | "where"
+            | "while"
+            | "async"
+            | "await"
+            | "dyn"
+            | "abstract"
+            | "become"
+            | "box"
+            | "do"
+            | "final"
+            | "macro"
+            | "override"
+            | "priv"
+            | "typeof"
+            | "unsized"
+            | "virtual"
+            | "yield"
+            | "try"
+    )
 }
