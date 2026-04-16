cd3d9008e3f96593044d681fb9f317750b31ca62
R4 Cheng <karaburi2023@gmail.com>
2026-04-15 21:42:11 -0700

    fix(newlib): update stat struct to match official headers
        - Source: https://sourceware.org/git/?p=newlib-cygwin.git;a=blob;f=newlib/libc/include/sys/stat.h;h=6525272dd40123d6ea4e7355a8e6f3a3f3d0dbb8;hb=HEAD
        - Note: If any platforms that meet the above criteria are added, they would need to create their own definition and use that (maybe a cfg_if!) - by @ian-h-chamberlain
    
        ps. thank @zetanumbers for checking targets

--- src/unix/newlib/vita/mod.rs
+++ src/unix/newlib/vita/mod.rs
@@ -1,4 +1,3 @@
-use crate::off_t;
 use crate::prelude::*;
 
 pub type clock_t = c_long;
@@ -59,23 +58,6 @@ s! {
 
     pub struct sched_param {
         pub sched_priority: c_int,
-    }
-
-    pub struct stat {
-        pub st_dev: crate::dev_t,
-        pub st_ino: crate::ino_t,
-        pub st_mode: crate::mode_t,
-        pub st_nlink: crate::nlink_t,
-        pub st_uid: crate::uid_t,
-        pub st_gid: crate::gid_t,
-        pub st_rdev: crate::dev_t,
-        pub st_size: off_t,
-        pub st_atime: crate::time_t,
-        pub st_mtime: crate::time_t,
-        pub st_ctime: crate::time_t,
-        pub st_blksize: crate::blksize_t,
-        pub st_blocks: crate::blkcnt_t,
-        pub st_spare4: [c_long; 2usize],
     }
 
     #[repr(align(8))]
