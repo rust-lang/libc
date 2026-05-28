dacedfbccf022f872d3ebb815452d6baf1bf8b53
dybucc <149513579+dybucc@users.noreply.github.com>
2026-05-28 09:35:15 +0200

    freebsd: annotate unstable `RAND_MAX`
    This symbol has changed recently. It needs usage advice. This patch
    links to that.
    
    (backport <https://github.com/rust-lang/libc/pull/5119>)
    (cherry picked from commit 8ece5e22b784c8612669565efdccee879d55c183)

--- src/unix/bsd/freebsdlike/freebsd/freebsd12/mod.rs
+++ src/unix/bsd/freebsdlike/freebsd/freebsd12/mod.rs
@@ -303,9 +303,11 @@ s! {
     }
 }
 
+/// This symbol is prone to change across releases upstream.
+/// See the [usage guidelines](crate::#usage-guidelines) for details and use.
 pub const RAND_MAX: c_int = 0x7fff_fffd;
 
-/// This symbols is prone to change across releases upstream.
+/// This symbol is prone to change across releases upstream.
 /// See the [usage guidelines](crate::#usage-guidelines) for details and use.
 pub const ELAST: c_int = 97;
 
