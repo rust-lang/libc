3c66425b08487a65c0e6fecb38541367a43d3650
Yuki Sireneva <sylfn@disroot.org>
2026-06-10 07:17:25 +0000

    android: Add getpwent/setpwent/endpwent
    Closes rust-lang/libc#3014
    Supersedes rust-lang/libc#3015

--- src/unix/linux_like/android/mod.rs
+++ src/unix/linux_like/android/mod.rs
@@ -3371,6 +3371,9 @@ safe_f! {
 }
 
 extern "C" {
+    pub fn setpwent();
+    pub fn endpwent();
+    pub fn getpwent() -> *mut passwd;
     pub fn setgrent();
     pub fn endgrent();
     pub fn getgrent() -> *mut crate::group;
