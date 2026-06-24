5e86813a416264f9f822db2a2f4ded44dbb744c3
Jonah Petri <jonah@sense.com>
2026-06-24 12:58:37 -0400

    use `cargo rustc` for macro expansion so -Zbuild-std can be supported

--- ctest/src/macro_expansion.rs
+++ ctest/src/macro_expansion.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs::canonicalize;
+use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 
@@ -14,18 +15,49 @@ pub fn expand<P: AsRef<Path>>(
     cfg: &[(String, Option<String>)],
     target: String,
 ) -> Result<String> {
-    let rustc = env::var("RUSTC").unwrap_or_else(|_| String::from("rustc"));
+    let dir = tempfile::tempdir()?;
 
-    let mut cmd = Command::new(rustc);
+    // make a Cargo.toml pointing to the crate path
+    let cargo_toml_path = dir.path().join("Cargo.toml");
+    let mut cargo_toml = std::fs::File::create(&cargo_toml_path)?;
+    writeln!(
+        &mut cargo_toml,
+        r#"
+[package]
+name = "libc"
+edition = "{EDITION}"
+[lib]
+path = '{}'
+[lints.rust]
+unexpected_cfgs = "allow"
+"#,
+        canonicalize(crate_path)?.display()
+    )?;
+    drop(cargo_toml);
+
+    let mut cmd = Command::new("cargo");
+
     cmd.env("RUSTC_BOOTSTRAP", "1")
-        .arg("-Zunpretty=expanded")
-        .arg("--edition")
-        .arg(EDITION) // By default, -Zunpretty=expanded uses 2015 edition.
-        .arg(canonicalize(crate_path)?);
+        .env("RUSTFLAGS", "") // ignore RUSTFLAGS so we don't get -Dwarnings
+        .current_dir(dir.path())
+        .arg("rustc")
+        .arg("--lib")
+        .arg("--profile")
+        .arg("check");
+
+    // set an independent target dir so we don't deadlock on the cargo build lock.
+    cmd.arg("--target-dir").arg(dir.path().join("target"));
+
+    if env::var("LIBC_CI_ZBUILD_STD").is_ok() {
+        cmd.arg("-Zbuild-std=core,std");
+    }
 
     if !target.is_empty() {
         cmd.arg("--target").arg(target);
     }
+
+    cmd.arg("--");
+    cmd.arg("-Zunpretty=expanded");
 
     // `libc` uses non standard cfg flags as well, which have to be manually expanded.
     for (k, v) in cfg {
@@ -39,7 +71,11 @@ pub fn expand<P: AsRef<Path>>(
 
     if !output.status.success() {
         let stderr = std::str::from_utf8(&output.stderr)?;
-        return Err(format!("macro expansion failed with {}: {}", output.status, stderr).into());
+        return Err(format!(
+            "macro expansion failed with {}: {}, {:?}",
+            output.status, stderr, cmd
+        )
+        .into());
     }
 
     let expanded = std::str::from_utf8(&output.stdout)?.to_string();
