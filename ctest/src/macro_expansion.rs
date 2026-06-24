111d3286d24dda0f9067687dcf413ce733982f04
Jonah Petri <jonah@sense.com>
2026-06-24 12:58:37 -0400

    use `cargo rustc` for macro expansion so -Zbuild-std can be supported

--- ctest/src/macro_expansion.rs
+++ ctest/src/macro_expansion.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs::canonicalize;
 use std::path::Path;
 use std::process::Command;
@@ -14,18 +13,62 @@ pub fn expand<P: AsRef<Path>>(
     cfg: &[(String, Option<String>)],
     target: String,
 ) -> Result<String> {
-    let rustc = env::var("RUSTC").unwrap_or_else(|_| String::from("rustc"));
+    expand_with_args(crate_path, cfg, target, None, &[])
+}
 
-    let mut cmd = Command::new(rustc);
+/// Extension of `expand` which allows a crate_name override for
+/// libc and other clients which require crate_name to match the
+/// original crate's name.
+pub(crate) fn expand_with_args<P: AsRef<Path>>(
+    crate_path: P,
+    cfg: &[(String, Option<String>)],
+    target: String,
+    crate_name: Option<&str>,
+    extra_cargo_args: &[String],
+) -> Result<String> {
+    let dir = tempfile::tempdir()?;
+
+    // make a Cargo.toml pointing to the crate path
+    let cargo_toml_path = dir.path().join("Cargo.toml");
+    let crate_name = crate_name.unwrap_or("ctest-expansion-tmp");
+    // FIXME(#5238): allow building using an existing manifest
+    let cargo_toml = format!(
+        r#"
+[package]
+name = '''{crate_name}'''  # Needs to match #![crate_name = "..."] if it exists.
+edition = '''{EDITION}'''
+[lib]
+path = '''{}'''
+[lints.rust]
+unexpected_cfgs = "allow"
+"#,
+        canonicalize(crate_path)?.display()
+    );
+    std::fs::write(cargo_toml_path, cargo_toml)?;
+
+    let mut cmd = Command::new(std::env::var("CARGO").unwrap_or("cargo".into()));
     cmd.env("RUSTC_BOOTSTRAP", "1")
-        .arg("-Zunpretty=expanded")
-        .arg("--edition")
-        .arg(EDITION) // By default, -Zunpretty=expanded uses 2015 edition.
-        .arg(canonicalize(crate_path)?);
+        .current_dir(dir.path())
+        .arg("rustc")
+        .arg("--lib")
+        .arg("--profile")
+        .arg("check")
+        .args(extra_cargo_args);
+
+    // In order to avoid warnings causing failures here when the
+    // environment's RUSTFLAGS contains -Dwarnings (or similar),
+    // we will clear RUSTFLAGS.
+    cmd.env_remove("RUSTFLAGS");
+
+    // set an independent target dir so we don't deadlock on the cargo build lock.
+    cmd.arg("--target-dir").arg(dir.path().join("target"));
 
     if !target.is_empty() {
         cmd.arg("--target").arg(target);
     }
+
+    cmd.arg("--");
+    cmd.arg("-Zunpretty=expanded");
 
     // `libc` uses non standard cfg flags as well, which have to be manually expanded.
     for (k, v) in cfg {
@@ -39,7 +82,11 @@ pub fn expand<P: AsRef<Path>>(
 
     if !output.status.success() {
         let stderr = std::str::from_utf8(&output.stderr)?;
-        return Err(format!("macro expansion failed with {}: {}", output.status, stderr).into());
+        return Err(format!(
+            "macro expansion failed with {}: {}, {:?}",
+            output.status, stderr, cmd
+        )
+        .into());
     }
 
     let expanded = std::str::from_utf8(&output.stdout)?.to_string();
