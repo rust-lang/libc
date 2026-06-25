bc62b095e9eaa657a4f5d44ff100804b34f80c40
dybucc <149513579+dybucc@users.noreply.github.com>
2026-06-25 11:39:43 +0200

    linux(uclibc): remove explicit linking to libutil
    Removes linking directory to libutil in MIPS32. This did not seem
    necessary. Testing is pending.
    
    (backport <https://github.com/rust-lang/libc/pull/5165>)
    (cherry picked from commit 8b7c4068fb4297f38cb59a700f16563785ae07f0)

--- src/unix/linux_like/linux/uclibc/mips/mips32/mod.rs
+++ src/unix/linux_like/linux/uclibc/mips/mips32/mod.rs
@@ -661,7 +661,6 @@ pub const SYS_process_mrelease: c_long = 4000 + 448;
 pub const SYS_futex_waitv: c_long = 4000 + 449;
 pub const SYS_set_mempolicy_home_node: c_long = 4000 + 450;
 
-#[link(name = "util")]
 extern "C" {
     pub fn sysctl(
         name: *mut c_int,
