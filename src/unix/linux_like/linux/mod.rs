rust-lang/libc

commit fb58c011af41a5a3d3ad3082c9263be5ae395751

epoll: add busy polling parameters
In Linux 6.9 a new ioctl for epoll was added:
https://man.archlinux.org/man/ioctl_eventpoll.2.en

Add support for it. The ioctls constants are padded to
64 bits alignment even on 32 bits machines.

Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

src/unix/linux_like/linux/mod.rs
@@ -1036,6 +1036,15 @@ s! {
         pub get_args: __u16,
         pub name: [c_char; ::IFNAMSIZ],
     }
+
+    // #include <linux/eventpoll.h>
+
+    pub struct epoll_params {
+        pub busy_poll_usecs: u32,
+        pub busy_poll_budget: u16,
+        pub prefer_busy_poll: u8,
+        pub __pad: u8, // Must be zero
+    }
 }
 
 cfg_if! {
@@ -5171,6 +5180,10 @@ pub const SCHED_FLAG_ALL: ::c_int = SCHED_FLAG_RESET_ON_FORK
     | SCHED_FLAG_DL_OVERRUN
     | SCHED_FLAG_KEEP_ALL
     | SCHED_FLAG_UTIL_CLAMP;
+
+// ioctl_eventpoll: added in Linux 6.9
+pub const EPIOCSPARAMS: ::Ioctl = 0x40088a01;
+pub const EPIOCGPARAMS: ::Ioctl = 0x80088a02;
 
 f! {
     pub fn NLA_ALIGN(len: ::c_int) -> ::c_int {

libc-test/build.rs
@@ -3839,6 +3839,9 @@ fn test_linux(target: &str) {
             // kernel so we can drop this and test the type once this new version is used in CI.
             "sched_attr" => true,
 
+            // FIXME: Requires >= 6.9 kernel headers.
+            "epoll_params" => true,
+
             _ => false,
         }
     });
@@ -4286,6 +4289,10 @@ fn test_linux(target: &str) {
             | "SCHED_FLAG_KEEP_ALL"
             | "SCHED_FLAG_UTIL_CLAMP"
             | "SCHED_FLAG_ALL" if musl => true, // Needs more recent linux headers.
+
+            // FIXME: Requires >= 6.9 kernel headers.
+            "EPIOCSPARAMS"
+            | "EPIOCGPARAMS" => true,
 
             _ => false,
         }

commit 515564da70aa697633644a2452c558a4753bab01

linux: Correct the value of `EPIOC[GS]PARAMS` with nonstandard _IOC
PowerPC, Sparc, and MIPS (32- and 64-bit for all) have different `_IOC`
numbers, meaning the constants were inaccurate. Example error on
PowerPC64:

    bad `EPIOCSPARAMS` value at byte 4: rust: 64 (0x40) != c 128 (0x80)
        rust bytes: 00 00 00 00 40 08 8a 01
        c bytes:    00 00 00 00 80 08 8a 01
    bad `EPIOCGPARAMS` value at byte 4: rust: 128 (0x80) != c 64 (0x40)
        rust bytes: 00 00 00 00 80 08 8a 02
        c bytes:    00 00 00 00 40 08 8a 02

Resolve this by using `_IOW` and `_IOR`.

Source: https://github.com/torvalds/linux/blob/502d801f0ab03e4f32f9a33d203154ce84887921/include/uapi/linux/eventpoll.h#L97-L99
Fixes: fb58c011af41 ("epoll: add busy polling parameters")

src/unix/linux_like/linux/mod.rs
@@ -3649,8 +3649,9 @@ pub const SCHED_FLAG_ALL: c_int = SCHED_FLAG_RESET_ON_FORK
     | SCHED_FLAG_UTIL_CLAMP;
 
 // ioctl_eventpoll: added in Linux 6.9
-pub const EPIOCSPARAMS: Ioctl = 0x40088a01;
-pub const EPIOCGPARAMS: Ioctl = u32_cast_ioctl(0x80088a02);
+const EPOLL_IOC_TYPE: u32 = 0x8A;
+pub const EPIOCSPARAMS: Ioctl = _IOW::<epoll_params>(EPOLL_IOC_TYPE, 0x01);
+pub const EPIOCGPARAMS: Ioctl = _IOR::<epoll_params>(EPOLL_IOC_TYPE, 0x02);
 
 // siginfo.h
 pub const SI_DETHREAD: c_int = -7;

libc-test/build.rs
@@ -3823,7 +3823,7 @@ fn test_linux(target: &str) {
     let mips64 = target.contains("mips64");
     let mips32 = mips && !mips64;
     let versions = &*VERSIONS;
-    let kernel = versions.linux.unwrap_or((0, 0));
+    let kernel = versions.linux.unwrap();
 
     let musl_v1_2_3 = env::var("CARGO_CFG_LIBC_UNSTABLE_MUSL_V1_2_3").is_ok();
     if musl_v1_2_3 {