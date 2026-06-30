4af5c76aa6cbfc9f09516984b04333d4c27f98a7
Sergii Bogomolov <sergii@bogomolov.io>
2026-06-30 21:53:40 +0200

    bsd: add `bpf_program`/`bpf_insn` on apple and `DLT_*` on freebsd
    apple was missing the two bpf structs; freebsd was missing the DLT_*
    link types.

--- src/new/freebsd/mod.rs
+++ src/new/freebsd/mod.rs
@@ -3,5 +3,6 @@
 //! * Headers: <https://github.com/freebsd/freebsd-src/blob/main/sys/riscv/include/ucontext.h>
 //! * Symbol map: <https://github.com/freebsd/freebsd-src/blob/main/lib/libc/gen/Symbol.map>
 
+pub(crate) mod net;
 pub(crate) mod sys;
 pub(crate) mod unistd;
