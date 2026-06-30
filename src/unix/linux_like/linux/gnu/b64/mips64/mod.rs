1237ac5c002b158513cde850075040a70c8be3e3
Trevor Gross <tg@trevorgross.com>
2026-06-30 07:48:39 +0000

    linux: Move `types.h` API to `src/new`
    Link: https://github.com/torvalds/linux/blob/dc59e4fea9d83f03bad6bddf3fa2e52491777482/include/uapi/linux/types.h
    Link: https://github.com/torvalds/linux/blob/dc59e4fea9d83f03bad6bddf3fa2e52491777482/include/uapi/asm-generic/types.h
    Link: https://github.com/torvalds/linux/blob/dc59e4fea9d83f03bad6bddf3fa2e52491777482/include/uapi/asm-generic/int-ll64.h
    Link: https://cs.android.com/android/platform/superproject/main/+/main:bionic/libc/kernel/uapi/linux/types.h;drc=180edefbd287c39caeb9d48784a9a10ac35f3636
    Link: https://cs.android.com/android/platform/superproject/main/+/main:bionic/libc/kernel/uapi/asm-generic/types.h;drc=180edefbd287c39caeb9d48784a9a10ac35f3636
    Link: https://cs.android.com/android/platform/superproject/main/+/main:bionic/libc/kernel/uapi/asm-generic/int-ll64.h;drc=180edefbd287c39caeb9d48784a9a10ac35f3636
    
    (backport <https://github.com/rust-lang/libc/pull/5233>)
    (cherry picked from commit 483795464d6fbbd5fe07d59013f7c9e2bb46ec82)

--- src/unix/linux_like/linux/gnu/b64/mips64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/mips64/mod.rs
@@ -9,8 +9,6 @@ pub type blksize_t = i64;
 pub type nlink_t = u64;
 pub type suseconds_t = i64;
 pub type wchar_t = i32;
-pub type __u64 = c_ulong;
-pub type __s64 = c_long;
 
 s! {
     pub struct stat {
--- src/unix/linux_like/linux/musl/b64/aarch64/mod.rs
+++ src/unix/linux_like/linux/musl/b64/aarch64/mod.rs
@@ -1,8 +1,6 @@
 use crate::off_t;
 use crate::prelude::*;
 
-pub type __u64 = c_ulonglong;
-pub type __s64 = c_longlong;
 pub type wchar_t = u32;
 pub type nlink_t = u32;
 pub type blksize_t = c_int;
