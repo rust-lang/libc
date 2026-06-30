af0a57048a0fa7dcf933085f007ba5dfacf1bcc5
Sergii Bogomolov <sergii@bogomolov.io>
2026-07-01 01:37:06 +0200

    linux: move if_addr.h constants to src/new

--- src/unix/linux_like/linux/mod.rs
+++ src/unix/linux_like/linux/mod.rs
@@ -1136,31 +1136,6 @@ pub const AT_HANDLE_FID: c_int = 0x200;
 pub const AT_HANDLE_MNT_ID_UNIQUE: c_int = 0x001;
 pub const AT_HANDLE_CONNECTABLE: c_int = 0x002;
 
-// linux/if_addr.h
-pub const IFA_UNSPEC: c_ushort = 0;
-pub const IFA_ADDRESS: c_ushort = 1;
-pub const IFA_LOCAL: c_ushort = 2;
-pub const IFA_LABEL: c_ushort = 3;
-pub const IFA_BROADCAST: c_ushort = 4;
-pub const IFA_ANYCAST: c_ushort = 5;
-pub const IFA_CACHEINFO: c_ushort = 6;
-pub const IFA_MULTICAST: c_ushort = 7;
-pub const IFA_FLAGS: c_ushort = 8;
-
-pub const IFA_F_SECONDARY: u32 = 0x01;
-pub const IFA_F_TEMPORARY: u32 = 0x01;
-pub const IFA_F_NODAD: u32 = 0x02;
-pub const IFA_F_OPTIMISTIC: u32 = 0x04;
-pub const IFA_F_DADFAILED: u32 = 0x08;
-pub const IFA_F_HOMEADDRESS: u32 = 0x10;
-pub const IFA_F_DEPRECATED: u32 = 0x20;
-pub const IFA_F_TENTATIVE: u32 = 0x40;
-pub const IFA_F_PERMANENT: u32 = 0x80;
-pub const IFA_F_MANAGETEMPADDR: u32 = 0x100;
-pub const IFA_F_NOPREFIXROUTE: u32 = 0x200;
-pub const IFA_F_MCAUTOJOIN: u32 = 0x400;
-pub const IFA_F_STABLE_PRIVACY: u32 = 0x800;
-
 // linux/fs.h
 
 // Flags for preadv2/pwritev2
