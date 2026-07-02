01c422d3dc29707bb7009cbeddcc51e7fe2d4dc7
dybucc <149513579+dybucc@users.noreply.github.com>
2026-07-02 18:44:58 +0200

    hurd: remove duplicate routines
    Remove duplicate bindings already present in the top-level `unix`
    module. These have equivalent definitions. The `unix` module definitions
    implement support for link name redirection under the
    `gnu_file_offset_bits64` `cfg`. These have been preferred.

--- src/unix/hurd/mod.rs
+++ src/unix/hurd/mod.rs
@@ -3841,8 +3841,6 @@ extern "C" {
     )]
     pub fn lseek64(__fd: c_int, __offset: off64_t, __whence: c_int) -> off64_t;
 
-    pub fn lseek(__fd: c_int, __offset: __off_t, __whence: c_int) -> __off_t;
-
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4259,7 +4257,6 @@ extern "C" {
         old_value: *mut crate::itimerspec,
     ) -> c_int;
 
-    pub fn fstat(__fd: c_int, __buf: *mut stat) -> c_int;
     #[cfg_attr(
         gnu_file_offset_bits64,
         deprecated(
@@ -4270,7 +4267,6 @@ extern "C" {
     )]
     pub fn fstat64(__fd: c_int, __buf: *mut stat64) -> c_int;
 
-    pub fn fstatat(__fd: c_int, __file: *const c_char, __buf: *mut stat, __flag: c_int) -> c_int;
     #[cfg_attr(
         gnu_file_offset_bits64,
         deprecated(
@@ -4294,7 +4290,6 @@ extern "C" {
         statxbuf: *mut statx,
     ) -> c_int;
 
-    pub fn ftruncate(__fd: c_int, __length: __off_t) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4314,7 +4309,6 @@ extern "C" {
     )]
     pub fn truncate64(__file: *const c_char, __length: off64_t) -> c_int;
 
-    pub fn lstat(__file: *const c_char, __buf: *mut stat) -> c_int;
     #[cfg_attr(
         gnu_file_offset_bits64,
         deprecated(
@@ -4348,7 +4342,6 @@ extern "C" {
     )]
     pub fn fstatfs64(__fildes: c_int, __buf: *mut statfs64) -> c_int;
 
-    pub fn statvfs(__file: *const c_char, __buf: *mut statvfs) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4358,7 +4351,6 @@ extern "C" {
         allow(deprecated)
     )]
     pub fn statvfs64(__file: *const c_char, __buf: *mut statvfs64) -> c_int;
-    pub fn fstatvfs(__fildes: c_int, __buf: *mut statvfs) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4369,7 +4361,6 @@ extern "C" {
     )]
     pub fn fstatvfs64(__fildes: c_int, __buf: *mut statvfs64) -> c_int;
 
-    pub fn open(__file: *const c_char, __oflag: c_int, ...) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4379,7 +4370,6 @@ extern "C" {
     )]
     pub fn open64(__file: *const c_char, __oflag: c_int, ...) -> c_int;
 
-    pub fn openat(__fd: c_int, __file: *const c_char, __oflag: c_int, ...) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4581,7 +4571,6 @@ extern "C" {
 
     pub fn faccessat(dirfd: c_int, pathname: *const c_char, mode: c_int, flags: c_int) -> c_int;
 
-    pub fn stat(__file: *const c_char, __buf: *mut stat) -> c_int;
     #[cfg_attr(
         gnu_file_offset_bits64,
         deprecated(
@@ -4592,12 +4581,6 @@ extern "C" {
     )]
     pub fn stat64(__file: *const c_char, __buf: *mut stat64) -> c_int;
 
-    pub fn readdir(dirp: *mut crate::DIR) -> *mut crate::dirent;
-    pub fn readdir_r(
-        dirp: *mut crate::DIR,
-        entry: *mut crate::dirent,
-        result: *mut *mut crate::dirent,
-    ) -> c_int;
     #[cfg_attr(
         any(target_pointer_width = "64", gnu_file_offset_bits64),
         deprecated(
@@ -4625,7 +4608,6 @@ extern "C" {
 
     pub fn dirfd(dirp: *mut crate::DIR) -> c_int;
 
-    #[link_name = "__xpg_strerror_r"]
     pub fn strerror_r(__errnum: c_int, __buf: *mut c_char, __buflen: size_t) -> c_int;
 
     pub fn __errno_location() -> *mut c_int;
