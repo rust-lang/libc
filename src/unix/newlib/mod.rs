abeef87f640a0d7e6281972c1ee0dd992744f0ad
Sang Woo Kim <sngwkim915@gmail.com>
2026-07-22 05:02:00 +0000

    newlib: fix RTEMS socket address definitions and constants
    RTEMS was using the generic ARM socket address layout, which does not
    match its FreeBSD-derived socket API. As a result, std could not parse
    addresses returned by local_addr, accept, and recv_from.
    
    Use the correct RTEMS socket address definitions and update 27 constants
    to their FreeBSD values.
    
    Header sources (newlib as built by the RTEMS 6 RSB,
    gitlab.rtems.org/contrib/newlib-cygwin @ 1b3dcfdc6f1f):
    
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/socket.h#L322
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/netinet/in.h#L98
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/netinet6/in6.h#L121
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/un.h#L58
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/_sockaddr_storage.h#L41-53
    
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/socket.h#L246
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/netinet/in.h#L424
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/netinet/tcp.h#L168
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/poll.h#L65
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/filio.h#L50
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/sys/_termios.h#L78
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/sys/rtems/include/netdb.h#L156
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/include/sys/_default_fcntl.h#L63
    https://gitlab.rtems.org/contrib/newlib-cygwin/-/blob/1b3dcfdc6f1fd2bd3e1ef2f8b7df736c076c6042/newlib/libc/include/sys/select.h#L34

--- src/unix/newlib/mod.rs
+++ src/unix/newlib/mod.rs
@@ -368,6 +368,8 @@ pub const PTHREAD_RWLOCK_INITIALIZER: pthread_rwlock_t = pthread_rwlock_t {
 cfg_if! {
     if #[cfg(target_os = "espidf")] {
         pub const NCCS: usize = 11;
+    } else if #[cfg(target_os = "rtems")] {
+        pub const NCCS: usize = 20;
     } else {
         pub const NCCS: usize = 32;
     }
@@ -427,7 +429,7 @@ pub const PTHREAD_MUTEX_ERRORCHECK: c_int = 2;
 cfg_if! {
     if #[cfg(any(target_os = "horizon", target_os = "espidf"))] {
         pub const FD_SETSIZE: c_int = 64;
-    } else if #[cfg(target_os = "vita")] {
+    } else if #[cfg(any(target_os = "vita", target_os = "rtems"))] {
         pub const FD_SETSIZE: c_int = 256;
     } else {
         pub const FD_SETSIZE: c_int = 1024;
@@ -559,7 +561,7 @@ pub const O_NONBLOCK: c_int = 16384;
 
 pub const O_ACCMODE: c_int = 3;
 cfg_if! {
-    if #[cfg(target_os = "espidf")] {
+    if #[cfg(any(target_os = "espidf", target_os = "rtems"))] {
         pub const O_CLOEXEC: c_int = 0x40000;
     } else {
         pub const O_CLOEXEC: c_int = 0x80000;
@@ -605,6 +607,8 @@ pub const PF_INET: c_int = 2;
 cfg_if! {
     if #[cfg(target_os = "espidf")] {
         pub const PF_INET6: c_int = 10;
+    } else if #[cfg(target_os = "rtems")] {
+        pub const PF_INET6: c_int = 28;
     } else {
         pub const PF_INET6: c_int = 23;
     }
@@ -666,7 +670,13 @@ cfg_if! {
 }
 pub const SO_TYPE: c_int = 0x1008;
 
-pub const SOCK_CLOEXEC: c_int = O_CLOEXEC;
+cfg_if! {
+    if #[cfg(target_os = "rtems")] {
+        pub const SOCK_CLOEXEC: c_int = 0x10000000;
+    } else {
+        pub const SOCK_CLOEXEC: c_int = O_CLOEXEC;
+    }
+}
 
 pub const INET_ADDRSTRLEN: c_int = 16;
 
@@ -690,7 +700,7 @@ pub const IFF_ALTPHYS: c_int = IFF_LINK2; // use alternate physical connection
 pub const IFF_MULTICAST: c_int = 0x8000; // supports multicast
 
 cfg_if! {
-    if #[cfg(target_os = "vita")] {
+    if #[cfg(any(target_os = "vita", target_os = "rtems"))] {
         pub const TCP_NODELAY: c_int = 1;
         pub const TCP_MAXSEG: c_int = 2;
     } else if #[cfg(target_os = "espidf")] {
@@ -726,7 +736,7 @@ cfg_if! {
     }
 }
 cfg_if! {
-    if #[cfg(target_os = "vita")] {
+    if #[cfg(any(target_os = "vita", target_os = "rtems"))] {
         pub const IP_TTL: c_int = 4;
     } else if #[cfg(target_os = "espidf")] {
         pub const IP_TTL: c_int = 2;
@@ -748,7 +758,7 @@ cfg_if! {
 }
 
 cfg_if! {
-    if #[cfg(target_os = "vita")] {
+    if #[cfg(any(target_os = "vita", target_os = "rtems"))] {
         pub const IP_ADD_MEMBERSHIP: c_int = 12;
         pub const IP_DROP_MEMBERSHIP: c_int = 13;
     } else if #[cfg(target_os = "espidf")] {
@@ -783,6 +793,11 @@ cfg_if! {
         pub const NO_DATA: c_int = 211;
         pub const NO_RECOVERY: c_int = 212;
         pub const TRY_AGAIN: c_int = 213;
+    } else if #[cfg(target_os = "rtems")] {
+        pub const HOST_NOT_FOUND: c_int = 1;
+        pub const TRY_AGAIN: c_int = 2;
+        pub const NO_RECOVERY: c_int = 3;
+        pub const NO_DATA: c_int = 4;
     } else {
         pub const HOST_NOT_FOUND: c_int = 1;
         pub const NO_DATA: c_int = 2;
@@ -790,7 +805,13 @@ cfg_if! {
         pub const TRY_AGAIN: c_int = 4;
     }
 }
-pub const NO_ADDRESS: c_int = 2;
+cfg_if! {
+    if #[cfg(target_os = "rtems")] {
+        pub const NO_ADDRESS: c_int = NO_DATA;
+    } else {
+        pub const NO_ADDRESS: c_int = 2;
+    }
+}
 
 pub const AI_PASSIVE: c_int = 1;
 pub const AI_CANONNAME: c_int = 2;
@@ -799,6 +820,9 @@ cfg_if! {
     if #[cfg(target_os = "espidf")] {
         pub const AI_NUMERICSERV: c_int = 8;
         pub const AI_ADDRCONFIG: c_int = 64;
+    } else if #[cfg(target_os = "rtems")] {
+        pub const AI_NUMERICSERV: c_int = 0x00000008;
+        pub const AI_ADDRCONFIG: c_int = 0x00000400;
     } else {
         pub const AI_NUMERICSERV: c_int = 0;
         pub const AI_ADDRCONFIG: c_int = 0;
@@ -811,7 +835,7 @@ pub const NI_NOFQDN: c_int = 1;
 pub const NI_NUMERICHOST: c_int = 2;
 pub const NI_NAMEREQD: c_int = 4;
 cfg_if! {
-    if #[cfg(target_os = "espidf")] {
+    if #[cfg(any(target_os = "espidf", target_os = "rtems"))] {
         pub const NI_NUMERICSERV: c_int = 8;
         pub const NI_DGRAM: c_int = 16;
     } else {
@@ -826,6 +850,11 @@ cfg_if! {
         pub const EAI_FAMILY: c_int = 204;
         pub const EAI_MEMORY: c_int = 203;
         pub const EAI_NONAME: c_int = 200;
+        pub const EAI_SOCKTYPE: c_int = 10;
+    } else if #[cfg(target_os = "rtems")] {
+        pub const EAI_FAMILY: c_int = 5;
+        pub const EAI_MEMORY: c_int = 6;
+        pub const EAI_NONAME: c_int = 8;
         pub const EAI_SOCKTYPE: c_int = 10;
     } else if #[cfg(not(target_os = "vita"))] {
         pub const EAI_FAMILY: c_int = -303;
@@ -974,17 +1003,10 @@ cfg_if! {
     } else if #[cfg(target_os = "vita")] {
         mod vita;
         pub use self::vita::*;
-    } else if #[cfg(target_arch = "arm")] {
-        mod arm;
-        pub use self::arm::*;
-    } else {
-        core::compile_error!("unsupported target");
-    }
-}
-
-cfg_if! {
-    if #[cfg(target_os = "rtems")] {
+    } else if #[cfg(target_os = "rtems")] {
         mod rtems;
         pub use self::rtems::*;
+    } else {
+        core::compile_error!("unsupported target");
     }
 }
