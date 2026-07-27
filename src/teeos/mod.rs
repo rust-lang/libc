16f80e7a5f524d831e4573f8c5f4bb8e2d498355
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-07-27 23:35:00 +0100

    clippy: fix explicit_iter_loop lint
    https://rust-lang.github.io/rust-clippy/master/index.html#explicit_iter_loop

--- src/teeos/mod.rs
+++ src/teeos/mod.rs
@@ -1337,8 +1337,7 @@ pub fn errno() -> c_int {
 pub fn CPU_COUNT_S(size: usize, cpuset: &cpu_set_t) -> c_int {
     let mut s: u32 = 0;
     let size_of_mask = size_of_val(&cpuset.bits[0]);
-
-    for i in cpuset.bits[..(size / size_of_mask)].iter() {
+    for i in &cpuset.bits[..(size / size_of_mask)] {
         s += i.count_ones();
     }
     s as c_int
