554945e3b47b065f1f28892d4287c76f71bf42dc
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-08-01 18:55:32 +0200

    hexagon: remove possibly redundant skip on `sem_t`
    Remove skip over `sem_t` in Hexagon QuRT as it alleged to this being a
    `typedef` of an anonymous `struct`, but that also happens in other
    targets we test against without there being skips for their records. See
    header file `posix/semaphore.h` under `rtos/qurt/computev81` in the
    Hexagon SDK for details.
    
    (backport <https://github.com/rust-lang/libc/pull/5351>)
    (cherry picked from commit 471be3d9247ddfa17e5c2139397db10b181f73a7)

--- libc-test/build.rs
+++ libc-test/build.rs
@@ -6363,8 +6363,6 @@ fn test_qurt(target: &str) {
             // These are compatibility stubs in libc, not from QuRT headers
             "stat" | "tm" | "timespec" | "timeval" | "itimerspec" | "dirent" | "DIR"
             | "termios" | "rlimit" | "rusage" | "flock" | "div_t" | "ldiv_t" | "lldiv_t" => true,
-            // sem_t is typedef of anonymous struct in C (no struct tag)
-            "sem_t" => true,
             _ => false,
         }
     });
