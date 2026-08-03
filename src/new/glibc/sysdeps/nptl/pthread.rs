aeb1f3522794c88b364f6ab4228cf044956ed716
Valentyn Kit <valentynkit.work@pm.me>
2026-08-03 12:09:02 +0300

    Add pthread_gettid_np for Android and glibc
    Returns the Linux TID for a given thread, unlike gettid, which only
    returns the id of the calling thread.
    
    This is useful for std in the thread spawn path: the parent can set the
    TID of the child it just created, whereas today the TID can only be read
    once the child itself starts running.
    
    The declaration is limited to Android and linux-gnu. musl and ohos do
    not re-export it.
    
    ctest is gated on the glibc version. The function was added in 2.42, so older
    glibc has no declaration to check against.
    
    Link: https://github.com/bminor/glibc/blob/d2097651cc57834dbfcaa102ddfacae0d86cfb66/sysdeps/nptl/pthread.h#L1320-L1322
    Link: https://android.googlesource.com/platform/bionic/+/731631f300090436d7f5df80d50b6275c8c60a93/libc/include/pthread.h#173
    
    (backport <https://github.com/rust-lang/libc/pull/5359>)
    (cherry picked from commit 87e552e35ff732b62c732082588076f609e589be)

--- src/new/glibc/sysdeps/nptl/pthread.rs
+++ src/new/glibc/sysdeps/nptl/pthread.rs
@@ -28,6 +28,7 @@ pub use crate::new::common::linux_like::pthread::{
     pthread_getaffinity_np,
     pthread_getattr_np,
     pthread_getname_np,
+    pthread_gettid_np,
     pthread_setaffinity_np,
     pthread_setname_np,
 };
