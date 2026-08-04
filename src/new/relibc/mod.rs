baeefcda7f28fcb7717bb7d48665e9af4ef69fee
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-08-04 03:06:52 +0100

    docs: add mirror repo for relibc

--- src/new/relibc/mod.rs
+++ src/new/relibc/mod.rs
@@ -1,5 +1,6 @@
 //! Redox OS libc.
 //!
-//! * Headers: <https://gitlab.redox-os.org/redox-os/relibc>
+//! * Headers: <https://gitlab.redox-os.org/redox-os/relibc> (official)
+//! * Headers: <https://github.com/redox-os/relibc> (mirror)
 
 pub(crate) mod unistd;
