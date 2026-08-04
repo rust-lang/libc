98e9e3918d4f1b6123eb13151a5429a9027e170e
Sergii Bogomolov <sergii@bogomolov.io>
2026-08-04 11:56:38 +0200

    freebsd: add `rt_msghdr` and `rt_metrics`
    (backport <https://github.com/rust-lang/libc/pull/5367>)
    (cherry picked from commit 9e26f038c7484363744a9ac3f49b4eea3c4643c3)

---
+++ src/new/freebsd/net/route.rs
@@ -0,0 +1,39 @@
+//! Header: `net/route.h`
+//!
+//! <https://github.com/freebsd/freebsd-src/blob/main/sys/net/route.h>
+
+use crate::prelude::*;
+
+s_with_default! {
+    pub struct rt_metrics {
+        pub rmx_locks: c_ulong,
+        pub rmx_mtu: c_ulong,
+        pub rmx_hopcount: c_ulong,
+        pub rmx_expire: c_ulong,
+        pub rmx_recvpipe: c_ulong,
+        pub rmx_sendpipe: c_ulong,
+        pub rmx_ssthresh: c_ulong,
+        pub rmx_rtt: c_ulong,
+        pub rmx_rttvar: c_ulong,
+        pub rmx_pksent: c_ulong,
+        pub rmx_weight: c_ulong,
+        pub rmx_nhidx: c_ulong,
+        rmx_filler: Padding<[c_ulong; 2]>,
+    }
+
+    pub struct rt_msghdr {
+        pub rtm_msglen: c_ushort,
+        pub rtm_version: c_uchar,
+        pub rtm_type: c_uchar,
+        pub rtm_index: c_ushort,
+        _rtm_spare1: Padding<c_ushort>,
+        pub rtm_flags: c_int,
+        pub rtm_addrs: c_int,
+        pub rtm_pid: crate::pid_t,
+        pub rtm_seq: c_int,
+        pub rtm_errno: c_int,
+        pub rtm_fmask: c_int,
+        pub rtm_inits: c_ulong,
+        pub rtm_rmx: rt_metrics,
+    }
+}
