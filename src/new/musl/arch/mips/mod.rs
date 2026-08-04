2a2e8ef233f4f8f0e9225ea2a6d571f575ee2245
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-08-04 04:01:32 +0100

    docs: add official repo for musl

--- src/new/musl/arch/mips/mod.rs
+++ src/new/musl/arch/mips/mod.rs
@@ -1,6 +1,7 @@
 //! Source directory: `arch/mips/`
 //!
-//! <https://github.com/kraj/musl/tree/master/arch/mips>
+//! * Headers: <https://git.musl-libc.org/cgit/musl/tree/arch/mips> (official)
+//! * Headers: <https://github.com/kraj/musl/tree/master/arch/mips> (mirror)
 
 pub(crate) mod bits {
     pub(crate) mod socket;
