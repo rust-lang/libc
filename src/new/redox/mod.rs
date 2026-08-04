1cfed554e1fd770d7be239054a2747cd764bf39e
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-08-04 03:06:52 +0100

    docs: add mirror repo for relibc
    (backport <https://github.com/rust-lang/libc/pull/5364>)
    (cherry picked from commit f31ee02cca4431b7e4269fd79d069ebe8bb171bf)

--- src/new/redox/mod.rs
+++ src/new/redox/mod.rs
@@ -1,3 +1,4 @@
 //! Redox libc.
 //!
-//! * Headers: <https://gitlab.redox-os.org/redox-os/relibc>
+//! * Headers: <https://gitlab.redox-os.org/redox-os/relibc> (official)
+//! * Headers: <https://github.com/redox-os/relibc> (mirror)
