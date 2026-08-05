4869ff8ada70236a1335da9c4d23b9dd3fa7afaa
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-08-05 18:37:47 +0200

    linux(uclibc): complete `siginfo_t` definition
    Replace alignment/padding dummy fields with a mirrored definition
    fitting that of upstream's `union`. uClibc supports targets for which we
    have no support in Rust. No architecture-specific definition has been
    provided for those. See [^1] for details on the generic definition and
    [^2] for details on the MIPS definition.
    
    [^1]: <https://github.com/wbx-github/uclibc-ng/blob/60d8e8c0cb9be8a241f6f2645daba260c8aec33c/libc/sysdeps/linux/common/bits/siginfo.h>
    [^2]: <https://github.com/wbx-github/uclibc-ng/blob/60d8e8c0cb9be8a241f6f2645daba260c8aec33c/libc/sysdeps/linux/mips/bits/siginfo.h>
    
    (backport <https://github.com/rust-lang/libc/pull/5345>)
    (cherry picked from commit 11a1a6407de254f2f61eed76ee9913ad483d7665)
    [ Needed to adjust the body of si_int for the cherry pick since on
      libc-0.2, sigval is still a struct - Trevor ]

---
+++ src/new/uclibc/sysdeps/linux/common/bits/mod.rs
@@ -0,0 +1,6 @@
+//! Directory source: `libc/sysdeps/linux/common/bits`
+//!
+//! * Headers: <https://gogs.waldemar-brodkorb.de/oss/uclibc-ng/src/master/libc/sysdeps/linux/common/bits> (official)
+//! * Headers: <https://github.com/wbx-github/uclibc-ng/tree/master/libc/sysdeps/linux/common/bits> (mirror)
+
+pub(crate) mod siginfo;
