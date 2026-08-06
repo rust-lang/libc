1565a052a92c69796e10cdd461ad29e64c202822
subotac <73706465+subotac@users.noreply.github.com>
2026-08-06 14:37:00 +0300

    wasi: add locale category constants
    Source: https://github.com/WebAssembly/wasi-libc/blob/6d8745c8cec1aaa82c24f2e3d7a544f7ea9c2089/libc-top-half/musl/include/locale.h#L23-L29
    
    (backport <https://github.com/rust-lang/libc/pull/5378>)
    (cherry picked from commit 364e4b60d8c300e97fb428d0389d00a5b03df2f4)

--- src/wasi/mod.rs
+++ src/wasi/mod.rs
@@ -554,6 +554,14 @@ pub static CLOCK_MONOTONIC: clockid_t = unsafe { clockid_t(core::ptr::addr_of!(_
 #[allow(unused_unsafe)]
 pub static CLOCK_REALTIME: clockid_t = unsafe { clockid_t(core::ptr::addr_of!(_CLOCK_REALTIME)) };
 
+pub const LC_CTYPE: c_int = 0;
+pub const LC_NUMERIC: c_int = 1;
+pub const LC_TIME: c_int = 2;
+pub const LC_COLLATE: c_int = 3;
+pub const LC_MONETARY: c_int = 4;
+pub const LC_MESSAGES: c_int = 5;
+pub const LC_ALL: c_int = 6;
+
 pub const ABDAY_1: crate::nl_item = 0x20000;
 pub const ABDAY_2: crate::nl_item = 0x20001;
 pub const ABDAY_3: crate::nl_item = 0x20002;
