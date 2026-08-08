d22b942f3709ec49990b7dd2bcf2c1c9175f8c4e
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-08-08 13:57:42 +0200

    freebsd: add `netlink/netlink.h` tests
    Add specific test for `netlink/netlink.h` bindings. This is necessary to
    avoid conflicts with the bindings for `net/if_mib.h`. libc-test now
    builds two different `TestGenerator` instances.
    
    One of the instances builds tests for the same set of bindings as before
    this patchset, while the other builds tests only for the
    `netlink/netlink.h` bindings.

---
+++ libc-test/tests/freebsd_netlink.rs
@@ -0,0 +1,17 @@
+#![allow(deprecated)]
+#![allow(unused)]
+
+#[cfg(target_os = "freebsd")]
+#[allow(unused_imports)]
+use libc::{
+    netlink::*,
+    netlink_generic::*,
+};
+
+#[cfg(target_os = "freebsd")]
+include!(concat!(env!("OUT_DIR"), "/netlink_ctest_output.rs"));
+
+#[cfg(not(target_os = "freebsd"))]
+fn main() {
+    println!("PASSED 0 tests");
+}
