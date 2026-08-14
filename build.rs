98e48b9655207c40e511c394f9827a6be1f6e688
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-08-14 08:35:51 +0200

    crate: expose `cfg` for general time64
    Add `cfg` enabling `time64` functionality across all supported targets.
    This ensures users have a simple entry point to the crate functionality
    gated behind one of `linux_time_bits64`, `uclibc_time64` and
    `gnu_time_bits64`.

--- build.rs
+++ build.rs
@@ -40,6 +40,9 @@ const ALLOWED_CFGS: &[&str] = &[
     "musl_redir_time64",
     "vxworks_lt_25_09",
     "libc_pauthtest",
+    // Global flag to enable one of `linux_time_bits64` or `gnu_time_bits64`.
+    // The musl flags are enabled by default on supported platforms.
+    "time64",
 ];
 
 // Extra values to allow for check-cfg.
@@ -169,6 +172,9 @@ fn main() {
     // OpenHarmony uses a fork of the musl libc
     let musl = target_env == "musl" || target_env == "ohos";
 
+    // Single entry-point to enable `time64` on supported platforms.
+    let time64 = env_flag("CARGO_CFG_LIBC_UNSTABLE_TIME64");
+
     // loongarch64, hexagon, ohos and pauthtest only exist with recent musl
     if target_arch == "loongarch64"
         || target_arch == "hexagon"
@@ -190,7 +196,7 @@ fn main() {
     }
 
     let uclibc_use_time64 = env_flag("CARGO_CFG_LIBC_UNSTABLE_UCLIBC_TIME64");
-    if target_env == "uclibc" && uclibc_use_time64 {
+    if target_env == "uclibc" && (uclibc_use_time64 || time64) {
         set_cfg("linux_time_bits64");
     }
 
@@ -230,7 +236,7 @@ fn main() {
             }
         };
 
-        if timebits == "64" {
+        if timebits == "64" || time64 {
             set_cfg("linux_time_bits64");
             set_cfg("gnu_file_offset_bits64");
             set_cfg("gnu_time_bits64");
