6ba7448cd5f190e4b89ef1a0c9fef43a8aeb66f7
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-08-28 18:26:04 +0200

    crate: use `Padding` for deprecated padding fields
    Replace types of padding fields that were deprecated for the `Padding`
    type. The deprecation warnings have been there for more than 6 years, so
    it's fine to act on them now.
    
    Make afore-mentioned padding fields private.
    
    (backport <https://github.com/rust-lang/libc/pull/5435>)
    (cherry picked from commit 31e8e55023b6f047a48151d0f8af56a85a8d5517)

--- src/unix/linux_like/linux/gnu/b64/sparc64/mod.rs
+++ src/unix/linux_like/linux/gnu/b64/sparc64/mod.rs
@@ -32,14 +32,7 @@ s! {
         pub si_signo: c_int,
         pub si_errno: c_int,
         pub si_code: c_int,
-        #[doc(hidden)]
-        #[deprecated(
-            since = "0.2.54",
-            note = "Please leave a comment on \
-                  https://github.com/rust-lang/libc/pull/1316 if you're using \
-                  this field"
-        )]
-        pub _pad: [c_int; 29],
+        _pad: Padding<[c_int; 29]>,
         _align: [usize; 0],
     }
 
