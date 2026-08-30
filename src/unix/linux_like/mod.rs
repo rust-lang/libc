f0b2b950795d05b3b36fb3ec93a9be5864e4c29e
Aelin Reidel <aelin@travitia.xyz>
2026-08-30 22:25:39 +0200

    linux_like: Expose statx on all musl versions
    Exposing the constants and the method in itself is not a breaking
    change, users are responsible for knowing whether they can call the
    method without running into linker errors, just like on glibc.
    
    (backport <https://github.com/rust-lang/libc/pull/5448>)
    (cherry picked from commit 823b37fd515dc5fa5f8f56abe4127e776fe80804)

--- src/unix/linux_like/mod.rs
+++ src/unix/linux_like/mod.rs
@@ -269,8 +269,8 @@ cfg_if! {
 cfg_if! {
     if #[cfg(any(
         target_env = "gnu",
+        target_env = "musl",
         target_os = "android",
-        all(target_env = "musl", musl_v1_2)
     ))] {
         s! {
             pub struct statx {
@@ -1656,8 +1656,8 @@ cfg_if! {
 cfg_if! {
     if #[cfg(any(
         target_env = "gnu",
+        target_env = "musl",
         target_os = "android",
-        all(target_env = "musl", musl_v1_2),
         target_os = "l4re"
     ))] {
         pub const AT_STATX_SYNC_TYPE: c_int = 0x6000;
@@ -2271,8 +2271,8 @@ cfg_if! {
 cfg_if! {
     if #[cfg(any(
         target_env = "gnu",
+        target_env = "musl",
         target_os = "android",
-        all(target_env = "musl", musl_v1_2)
     ))] {
         extern "C" {
             pub fn statx(
