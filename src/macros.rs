ec167aecb1bf80f4cc9efc419ac4180a1dcde986
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2026-09-06 13:40:23 +0100

    libc: Make `rustc-dep-of-std` work in tests

--- src/macros.rs
+++ src/macros.rs
@@ -854,6 +854,7 @@ macro_rules! offset_of {
 #[cfg(test)]
 mod tests {
     use core::any::TypeId;
+    use core::prelude::v1::*;
 
     use crate::types::CEnumRepr;
 
@@ -1171,6 +1172,8 @@ mod tests {
 #[cfg(test)]
 #[allow(unused)]
 mod macro_checks {
+    use core::prelude::v1::*;
+
     s! {
         pub struct S1 {
             pub a: u32,
