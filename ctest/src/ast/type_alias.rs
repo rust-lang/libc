d27b42c5006dad382c6cd780cca00cbc38892c64
Adam Martinez <149513579+dybucc@users.noreply.github.com>
2026-09-06 17:44:41 +0200

    ctest: differentiate between `ident` and `path`
    - Rename `ident` function on all parsed items to `path`. After the
      changes in the last few patches, this function was returning the
      cached string we keep in each item representative of the stringified
      token stream from each item's `path` field. Thus, speaking of an
      identifier here is a bit misleading, and considering it a path to the
      item is more correct.
    
    - Add `ident` function to return the last segment of each parsed item's
      `path` field. This corresponds now with the previous semantics of
      `ident`, now `path`, but breaks the API because it returns a
      fully-owned `String` and not a `&str`. It also incurrs an additional
      allocation on each call, but that can be easily fixed by also
      "caching" this last segment of the item's path in one of the item's
      fields.

--- ctest/src/ast/type_alias.rs
+++ ctest/src/ast/type_alias.rs
@@ -10,8 +10,22 @@ pub struct Type {
 }
 
 impl Type {
-    /// Return the identifier of the type alias as a string.
-    pub fn ident(&self) -> &str {
+    /// Return the full path to the type alias as a string.
+    ///
+    /// If inside a nested module, this will return a top-level-relative path,
+    /// but not a crate-relative path. For some item `foo` in module
+    /// `crate::bar`, the returned string will be `bar::foo`, and not
+    /// `crate::bar::foo`.
+    pub fn path(&self) -> &str {
         &self.ident
+    }
+
+    /// Returns the last path of the identifier, from the absolute path returned
+    /// by [`Type::path`].
+    pub fn ident(&self) -> String {
+        let Some(syn::PathSegment { ident, .. }) = self.path.segments.last() else {
+            unreachable!("all parsed items have at least one element in their path")
+        };
+        ident.to_string()
     }
 }
