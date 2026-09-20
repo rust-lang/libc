3de6a1a1e6d29536f785bc8991db8269eb29c88b
Trevor Gross <tg@trevorgross.com>
2026-09-20 16:26:51 -0500

    uclibc: Replace `linux_time_bits64` with a new `uclibc32_time64` cfg
    `linux_time_bits64` should correlate to the kernel's
    `__USE_TIME_BITS64`, which is set on both 32- and 64-bit platforms where
    `time_t` is 64 bits. The uclibc version of this config is
    `UCLIBC_USE_TIME64` which is only set on 32-bit platforms.
    
    Split this up by introducing `uclibc32_time64` to closer match
    `UCLIBC_USE_TIME64`. As an effect, setting `libc_unstable_uclibc_time64`
    no longer crashes builds for 64-bit uclibc targets. The setup also
    closer matches what is done for GNU targets.
    
    Note that `linux_time_bits64` is not actually the "64-bit time_t on any
    platform" yet that the kernel's version is, so it still isn't set on
    64-bit platforms after this change.
    
    * `UCLIBC_USE_TIME64` definition:
      https://github.com/wbx-github/uclibc-ng/blob/3f2ef085d8455a13fcdc4d4d473c0385dd65d55e/extra/Configs/Config.in#L1036-L1055
    * Implication of `__USE_TIME_BITS64`: 
      https://github.com/wbx-github/uclibc-ng/blob/3f2ef085d8455a13fcdc4d4d473c0385dd65d55e/include/features.h#L458-L460
    * Entrypoint for 64-bit `time_t`:
      https://github.com/wbx-github/uclibc-ng/blob/3f2ef085d8455a13fcdc4d4d473c0385dd65d55e/libc/sysdeps/linux/common/bits/typesizes.h#L50-L54
    * Arm `struct shimd_ds`:
      https://github.com/wbx-github/uclibc-ng/blob/3f2ef085d8455a13fcdc4d4d473c0385dd65d55e/libc/sysdeps/linux/arm/bits/shm.h#L48-L69

--- src/unix/linux_like/linux/uclibc/mod.rs
+++ src/unix/linux_like/linux/uclibc/mod.rs
@@ -14,7 +14,7 @@ pub type __priority_which_t = c_uint;
 
 cfg_if! {
     // Set `--cfg=libc_unstable_uclibc_time64` in RUSTFLAGS if your uClibc has 64-bit `time_t`.
-    if #[cfg(linux_time_bits64)] {
+    if #[cfg(uclibc32_time64)] {
         pub type time_t = i64;
         pub type suseconds_t = i64;
     } else {
